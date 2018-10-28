c9d6f93720d03256f831e3cf6cb2073a281bf04a
support <support@webeweb.fr>
2018-10-28 14:16:09 +0100

    Add main bundle class

---
+++ CoreBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Core bundle.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle
+ */
+class BootstrapBundle extends Bundle {
+
+}
