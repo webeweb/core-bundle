d0435cff15549fb2e6da67c45a71e9d7b7ef8cda
support <support@webeweb.fr>
2018-10-28 14:33:39 +0100

    Add Navigation tree helper

---
+++ Tests/Navigation/NavigationTreeHelperTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation;
+
+use Symfony\Component\HttpFoundation\Request;
+use WBW\Bundle\CoreBundle\Navigation\NavigationTreeHelper;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\TestFixtures;
+
+/**
+ * Navigation tree helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation
+ * @final
+ */
+final class NavigationTreeHelperTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Navigation tree
+     *
+     * @var NavigationTree
+     */
+    private $tree;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set up a Navigation tree mock.
+        $this->tree = TestFixtures::getNavigationTree();
+    }
+
+    /**
+     * Tests the activeTree() method.
+     *
+     * @return void
+     */
+    public function testActiveTree() {
+
+        NavigationTreeHelper::activeTree($this->tree, Request::create("https://github.com/webeweb/core-bundle"));
+
+        $this->assertTrue($this->tree->getNodeAt(0)->getActive());
+
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(0)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(1)->getActive());
+        $this->assertTrue($this->tree->getNodeAt(0)->getNodeAt(2)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(3)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(4)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(5)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(6)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(7)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(8)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(9)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(10)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(11)->getActive());
+        //$this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(12)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(13)->getActive());
+        $this->assertFalse($this->tree->getNodeAt(0)->getNodeAt(14)->getActive());
+    }
+
+    /**
+     * Tests the getBreadcrumbs() method.
+     *
+     * @return void
+     * @depends testActiveTree
+     */
+    public function testGetBreadcrumbs() {
+
+        NavigationTreeHelper::activeTree($this->tree, Request::create("https://github.com/webeweb/core-bundle"));
+
+        $res = NavigationTreeHelper::getBreadcrumbs($this->tree);
+
+        $this->assertCount(2, $res);
+
+        $this->assertEquals("GitHub", $res[0]->getId());
+        $this->assertEquals("Core bundle", $res[1]->getId());
+    }
+
+}
