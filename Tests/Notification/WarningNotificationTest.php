1a6980eb88894b39cc5db22b4bf47c9b4696acc0
support <support@webeweb.fr>
2018-10-29 17:26:24 +0100

    Add Warning notification

---
+++ Tests/Notification/WarningNotificationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Notification;
+
+use WBW\Bundle\CoreBundle\Notification\NotificationInterface;
+use WBW\Bundle\CoreBundle\Notification\WarningNotification;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Warning notification test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Notification
+ */
+class WarningNotificationTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new WarningNotification("warning");
+
+        $this->assertEquals(NotificationInterface::NOTIFICATION_WARNING, $obj->getType());
+        $this->assertEquals("warning", $obj->getContent());
+    }
+
+}
