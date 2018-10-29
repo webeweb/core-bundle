660a3acabaae758b878fbb23d17970139ab345af
support <support@webeweb.fr>
2018-10-29 17:23:57 +0100

    Add Notification event

---
+++ Event/NotificationEvent.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Event;
+
+use WBW\Bundle\CoreBundle\Notification\NotificationInterface;
+
+/**
+ * Notification event.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Event
+ */
+class NotificationEvent extends AbstractEvent {
+
+    /**
+     * Notification.
+     *
+     * @var NotificationInterface
+     */
+    private $notification;
+
+    /**
+     * Constructor.
+     *
+     * @param string $eventName The event name.
+     * @param NotificationInterface $notification The notification.
+     */
+    public function __construct($eventName, NotificationInterface $notification) {
+        parent::__construct($eventName);
+        $this->setNotification($notification);
+    }
+
+    /**
+     * Get the notification.
+     *
+     * @return NotificationInterface Returns the notification.
+     */
+    public function getNotification() {
+        return $this->notification;
+    }
+
+    /**
+     * Set the notification.
+     *
+     * @param NotificationInterface $notification The notification.
+     * @return NotificationEvent Returns this notification event.
+     */
+    protected function setNotification(NotificationInterface $notification) {
+        $this->notification = $notification;
+        return $this;
+    }
+
+}
---
+++ Tests/Event/NotificationEventTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Event;
+
+use WBW\Bundle\CoreBundle\Event\NotificationEvent;
+use WBW\Bundle\CoreBundle\Notification\NotificationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Notification event test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Event
+ */
+class NotificationEventTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        // Set a Notification mock.
+        $notification = $this->getMockBuilder(NotificationInterface::class)->getMock();
+
+        $obj = new NotificationEvent("eventName", $notification);
+
+        $this->assertEquals("eventName", $obj->getEventName());
+        $this->assertSame($notification, $obj->getNotification());
+    }
+
+}
