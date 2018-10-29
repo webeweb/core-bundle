e7aa3d955687a5530e22608c38760c4daf52605b
support <support@webeweb.fr>
2018-10-29 18:16:21 +0100

    Add User helper

---
+++ Tests/Helper/UserHelperTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Helper;
+
+use Symfony\Component\Security\Core\User\User;
+use WBW\Bundle\CoreBundle\Helper\UserHelper;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * User helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Helper
+ */
+class UserHelperTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the hasRoles() method.
+     *
+     * @return void
+     */
+    public function testHasRoles() {
+
+        $arg = new User("github", "github", ["ROLE_USER"]);
+
+        $this->assertFalse(UserHelper::hasRoles(null, ""));
+        $this->assertFalse(UserHelper::hasRoles(null, null));
+        $this->assertFalse(UserHelper::hasRoles("", null));
+        $this->assertFalse(UserHelper::hasRoles("", ""));
+
+        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_SUPER_ADMIN"));
+        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_SUPER_ADMIN", false));
+
+        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_USER"));
+        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_USER", false));
+
+        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_GITHUB"));
+        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_GITHUB", false));
+
+        $this->assertTrue(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_USER"]));
+        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_USER"], false));
+
+        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"]));
+        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"], false));
+    }
+
+}
