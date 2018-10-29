811c58d214ce049af619534ff2961dc2af753713
support <support@webeweb.fr>
2018-10-29 17:26:33 +0100

    Add Default notification

---
+++ Notification/DefaultNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Notification;
+
+/**
+ * Default notification.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Notification
+ */
+class DefaultNotification extends AbstractNotification {
+
+    /**
+     * Constructor.
+     *
+     * @param string $type The type.
+     * @param string $content The content.
+     */
+    public function __construct($type, $content) {
+        parent::__construct($type, $content);
+    }
+
+}
---
+++ Tests/Notification/DefaultNotificationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Notification;
+
+use WBW\Bundle\CoreBundle\Notification\DefaultNotification;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * DefaultNotificationTest.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Notification
+ */
+class DefaultNotificationTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultNotification("type", "default");
+
+        $this->assertEquals("type", $obj->getType());
+        $this->assertEquals("default", $obj->getContent());
+    }
+
+}
