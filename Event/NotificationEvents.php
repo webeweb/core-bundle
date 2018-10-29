98edff3d2c1aee679b4c439d9dd783ce43659d27
support <support@webeweb.fr>
2018-10-29 17:23:49 +0100

    Add Notification events

---
+++ Event/NotificationEvents.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Event;
+
+use WBW\Bundle\CoreBundle\Notification\NotificationInterface;
+
+/**
+ * Notification events.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Event
+ */
+class NotificationEvents {
+
+    /**
+     * Notification "Danger".
+     *
+     * @var string
+     */
+    const NOTIFICATION_DANGER = "webeweb.core.event.notification." . NotificationInterface::NOTIFICATION_DANGER;
+
+    /**
+     * Notification "Info".
+     *
+     * @var string
+     */
+    const NOTIFICATION_INFO = "webeweb.core.event.notification." . NotificationInterface::NOTIFICATION_INFO;
+
+    /**
+     * Notification "Success".
+     *
+     * @var string
+     */
+    const NOTIFICATION_SUCCESS = "webeweb.core.event.notification." . NotificationInterface::NOTIFICATION_SUCCESS;
+
+    /**
+     * Notification "Warning".
+     *
+     * @var string
+     */
+    const NOTIFICATION_WARNING = "webeweb.core.event.notification." . NotificationInterface::NOTIFICATION_WARNING;
+
+}
---
+++ Tests/Event/NotificationEventsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Event;
+
+use WBW\Bundle\CoreBundle\Event\NotificationEvents;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Notification events test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Event
+ */
+class NotificationEventsTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("webeweb.core.event.notification.danger", NotificationEvents::NOTIFICATION_DANGER);
+        $this->assertEquals("webeweb.core.event.notification.info", NotificationEvents::NOTIFICATION_INFO);
+        $this->assertEquals("webeweb.core.event.notification.success", NotificationEvents::NOTIFICATION_SUCCESS);
+        $this->assertEquals("webeweb.core.event.notification.warning", NotificationEvents::NOTIFICATION_WARNING);
+    }
+
+}
