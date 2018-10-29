aca24d9b0944eb9c9be1f991d2fe25ae15d34875
support <support@webeweb.fr>
2018-10-29 17:27:43 +0100

    Add Notification event listener

---
+++ Tests/EventListener/NotificationEventListenerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use WBW\Bundle\CoreBundle\Event\NotificationEvent;
+use WBW\Bundle\CoreBundle\EventListener\NotificationEventListener;
+use WBW\Bundle\CoreBundle\Notification\NotificationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Notification event listener.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\EventListener
+ */
+class NotificationEventListenerTest extends AbstractFrameworkTestCase {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Session mock.
+        $this->session = $this->getMockBuilder(Session::class)->getMock();
+        $this->session->expects($this->any())->method("getFlashBag")->willReturn(new FlashBag());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NotificationEventListener($this->session);
+
+        $this->assertSame($this->session, $obj->getSession());
+    }
+
+    /**
+     * Tests the onNotify() method.
+     *
+     * @return void
+     */
+    public function testOnNotify() {
+
+        // Set a Notification mock.
+        $notification = $this->getMockBuilder(NotificationInterface::class)->getMock();
+
+        $obj = new NotificationEventListener($this->session);
+
+        $this->assertNull($obj->onNotify(new NotificationEvent("eventName", $notification)));
+    }
+
+}
