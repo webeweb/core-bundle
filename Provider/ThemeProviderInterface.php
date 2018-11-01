10cba00cf226a377196c840bb80b6c2a03b322a7
support <support@webeweb.fr>
2018-11-01 10:32:09 +0100

    Add Theme provider interface

---
+++ Provider/ThemeProviderInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider;
+
+/**
+ * Theme provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider
+ */
+interface ThemeProviderInterface extends ProviderInterface {
+
+    /**
+     * Get the view.
+     *
+     * @return string Returns the view.
+     */
+    public function getView();
+}
