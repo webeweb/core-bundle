48d4fb1b68ab730f84b78d85a6daba679a6a4250
support <support@webeweb.fr>
2018-11-01 10:32:37 +0100

    Add Application theme provider interface

---
+++ Provider/Theme/ApplicationThemeProviderInterface.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\ThemeProviderInterface;
+
+/**
+ * Application theme provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+interface ApplicationThemeProviderInterface extends ThemeProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.provider.theme.application";
+
+    /**
+     * Get the description.
+     *
+     * @return string Returns the description.
+     */
+    public function getDescription();
+
+    /**
+     * Get the home.
+     *
+     * @return string Returns the home.
+     */
+    public function getHome();
+
+    /**
+     * Get the name.
+     *
+     * @return string Returns the name.
+     */
+    public function getName();
+
+    /**
+     * Get the route.
+     *
+     * @return string Returns the route.
+     */
+    public function getRoute();
+
+    /**
+     * Get the title.
+     *
+     * @return string Returns the title.
+     */
+    public function getTitle();
+
+    /**
+     * Get the version.
+     *
+     * @return string Returns the version.
+     */
+    public function getVersion();
+
+    /**
+     * Get the year.
+     *
+     * @return string Returns the year.
+     */
+    public function getYear();
+}
