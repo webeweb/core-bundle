9532861ca0a7ea323288bfa0b6f4fac6aa5489a2
support <support@webeweb.fr>
2018-11-01 10:33:47 +0100

    Add Navigation theme provider interface

---
+++ Provider/Theme/NavigationThemeProviderInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationTree;
+use WBW\Bundle\CoreBundle\Provider\ThemeProviderInterface;
+
+/**
+ * Navigation theme provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+interface NavigationThemeProviderInterface extends ThemeProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.provider.theme.navigation";
+
+    /**
+     * Get the tree.
+     *
+     * @return NavigationTree Returns the tree.
+     */
+    public function getTree();
+}
---
+++ Tests/Provider/Theme/NavigationThemeProviderInterfaceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\NavigationThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation theme provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class NavigationThemeProviderInterfaceTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("webeweb.core.provider.theme.navigation", NavigationThemeProviderInterface::SERVICE_NAME);
+    }
+
+}
