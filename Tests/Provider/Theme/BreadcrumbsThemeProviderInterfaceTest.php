eece7610dceb334a4ef80fbdd032cac411f2149d
support <support@webeweb.fr>
2018-11-01 10:32:49 +0100

    Add Breadcrumbs theme provider interface

---
+++ Tests/Provider/Theme/BreadcrumbsThemeProviderInterfaceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\BreadcrumbsThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Breadcrumbs theme provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class BreadcrumbsThemeProviderInterfaceTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("webeweb.core.provider.theme.breadcrumbs", BreadcrumbsThemeProviderInterface::SERVICE_NAME);
+    }
+
+}
