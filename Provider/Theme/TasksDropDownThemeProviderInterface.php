5add7bcc569d59244188dca341678d3784c36946
support <support@webeweb.fr>
2018-11-01 10:35:00 +0100

    Add Tasks drop down theme provider interface

---
+++ Provider/Theme/TasksDropDownThemeProviderInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\ThemeProviderInterface;
+
+/**
+ * Tasks drop down theme provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+interface TasksDropDownThemeProviderInterface extends ThemeProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.provider.theme.tasksdropdown";
+
+    /**
+     * Get the tasks.
+     *
+     * @return array Returns the tasks.
+     */
+    public function getTasks();
+}
