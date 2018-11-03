0deb8dedc09729c819951c1fb3ad16952be4d3e4
webeweb <support@webeweb.fr>
2018-11-03 15:38:34 +0100

    Add Default notifications drop down theme provider

---
+++ Tests/Theme/DefaultNotificationsDropDownThemeProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Theme\DefaultNotificationsDropDownThemeProvider;
+
+/**
+ * Default notifications drop down theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme
+ */
+class DefaultNotificationsDropDownThemeProviderTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultNotificationsDropDownThemeProvider();
+
+        $this->assertEquals([], $obj->getNotifications());
+        $this->assertEquals(null, $obj->getView());
+    }
+
+}
---
+++ Theme/DefaultNotificationsDropDownThemeProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\NotificationsDropDownThemeProviderInterface;
+
+/**
+ * Default notifications drop down theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Theme
+ */
+class DefaultNotificationsDropDownThemeProvider implements NotificationsDropDownThemeProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNotifications() {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getView() {
+        return null;
+    }
+
+}
