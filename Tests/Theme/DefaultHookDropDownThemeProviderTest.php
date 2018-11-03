0550975b18609f17f55e2132047d8a482809464f
webeweb <support@webeweb.fr>
2018-11-03 15:37:54 +0100

    Add Default hook drop down theme provider

---
+++ Tests/Theme/DefaultHookDropDownThemeProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Theme\DefaultHookDropDownThemeProvider;
+
+/**
+ * Default hook drop down theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme
+ */
+class DefaultHookDropDownThemeProviderTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultHookDropDownThemeProvider();
+
+        $this->assertEquals([], $obj->getItems());
+        $this->assertEquals(null, $obj->getView());
+    }
+
+}
---
+++ Theme/DefaultHookDropDownThemeProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\HookDropDownThemeProviderInterface;
+
+/**
+ * Default hook drop down theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Theme
+ */
+class DefaultHookDropDownThemeProvider implements HookDropDownThemeProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItems() {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getView() {
+        return null;
+    }
+
+}
