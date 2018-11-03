cb1fcb6d8e425e0ec9e320570ce7beb6b367438d
webeweb <support@webeweb.fr>
2018-11-03 15:38:06 +0100

    Add Default navigation theme provider

---
+++ Tests/Theme/DefaultNavigationThemeProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationTree;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Theme\DefaultNavigationThemeProvider;
+
+/**
+ * Default navigation theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme
+ */
+class DefaultNavigationThemeProviderTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultNavigationThemeProvider();
+
+        $this->assertEquals(null, $obj->getView());
+        $this->assertInstanceOf(NavigationTree::class, $obj->getTree());
+    }
+
+}
