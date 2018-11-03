5fc69b6f7d48d0047af50f3d83a132feca3751f0
webeweb <support@webeweb.fr>
2018-11-03 15:39:19 +0100

    Add Default user info theme provider

---
+++ Tests/Theme/DefaultUserInfoThemeProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Theme\DefaultUserInfoThemeProvider;
+
+/**
+ * Default user info theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme
+ */
+class DefaultUserInfoThemeProviderTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultUserInfoThemeProvider();
+
+        $this->assertEquals(null, $obj->getView());
+        $this->assertFalse($obj->provideRegisterLink());
+        $this->assertFalse($obj->provideResettingLink());
+    }
+
+}
---
+++ Theme/DefaultUserInfoThemeProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\UserInfoThemeProviderInterface;
+
+/**
+ * Default user info theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Theme
+ */
+class DefaultUserInfoThemeProvider implements UserInfoThemeProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provideRegisterLink() {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provideResettingLink() {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getView() {
+        return null;
+    }
+
+}
