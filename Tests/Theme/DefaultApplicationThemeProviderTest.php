f8164c176f1acb88d9f52895cbfbdbe2b742d36d
webeweb <support@webeweb.fr>
2018-11-03 15:37:22 +0100

    Add Default application theme provider

---
+++ Tests/Theme/DefaultApplicationThemeProviderTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Theme\DefaultApplicationThemeProvider;
+
+/**
+ * Default application theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Bootstrap\Theme
+ */
+class DefaultApplicationThemeProviderTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultApplicationThemeProvider();
+
+        $this->assertEquals("Core bundle", $obj->getDescription());
+        $this->assertEquals("/", $obj->getHome());
+        $this->assertEquals("Core<b>bundle</b>", $obj->getName());
+        $this->assertEquals("/", $obj->getRoute());
+        $this->assertEquals("Core bundle", $obj->getTitle());
+        $this->assertEquals("dev-master", $obj->getVersion());
+        $this->assertEquals(null, $obj->getView());
+        $this->assertEquals("2018", $obj->getYear());
+    }
+
+}
---
+++ Theme/DefaultApplicationThemeProvider.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\ApplicationThemeProviderInterface;
+
+/**
+ * Default application theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\\Theme
+ */
+class DefaultApplicationThemeProvider implements ApplicationThemeProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription() {
+        return "Core bundle";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHome() {
+        return "/";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
+        return "Core<b>bundle</b>";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRoute() {
+        return "/";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle() {
+        return "Core bundle";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion() {
+        return "dev-master";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getView() {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getYear() {
+        return "2018";
+    }
+
+}
