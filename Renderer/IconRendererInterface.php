41a714d206bca3ba0719e694ff49d6c3981d1797
webeweb <support@webeweb.fr>
2018-11-10 15:11:08 +0100

    Add Icon renderer interface

---
+++ Renderer/IconRendererInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Twig\Extension;
+
+/**
+ * Icon renderer interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Twig\Extension
+ */
+interface IconRendererInterface {
+
+    /**
+     * Render an icon.
+     *
+     * @param string $name The icon name.
+     * @param string $style The icon style.
+     * @return string Returns a rendered icon.
+     */
+    public function renderIcon($name, $style);
+}
