27b50517218e373f5ca2bf104fd6abd36ffe6695
webeweb <support@webeweb.fr>
2018-11-10 12:02:38 +0100

    Add Bad user role exception

---
+++ Exception/AbstractException.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Exception;
+
+use WBW\Library\Core\Exception\AbstractCoreException;
+
+/**
+ * Abstract Core exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Exception
+ * @abstract
+ */
+abstract class AbstractException extends AbstractCoreException {
+
+    /**
+     * Constructor.
+     *
+     * @param string $message The message.
+     * @param int $code The code.
+     */
+    public function __construct($message, $code) {
+        parent::__construct($message, $code);
+    }
+
+}
