b449beec1a286ab83b3faccf8a90bb44d3eae473
webeweb <support@webeweb.fr>
2018-11-10 12:07:33 +0100

    Add Kernel event listener

---
+++ EventListener/KernelEventListener.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use WBW\Bundle\CoreBundle\Exception\BadUserRoleException;
+use WBW\Bundle\CoreBundle\Manager\ThemeManager;
+
+/**
+ * Kernel event listener.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\EventListener
+ */
+class KernelEventListener {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.eventlistener.kernel";
+
+    /**
+     * Theme manager.
+     *
+     * @var ThemeManager
+     */
+    private $themeManager;
+
+    /**
+     * Request.
+     *
+     * @var Request
+     */
+    private static $request;
+
+    /**
+     * Token storage.
+     *
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * User.
+     *
+     * @var UserInterface
+     */
+    private $user;
+
+    /**
+     * Constructor.
+     *
+     * @param TokenStorageInterface $tokenStorage The token storage.
+     * @param ThemeManager $providersManager The providers manager.
+     */
+    public function __construct(TokenStorageInterface $tokenStorage, ThemeManager $providersManager) {
+        $this->setThemeManager($providersManager);
+        $this->setTokenStorage($tokenStorage);
+    }
+
+    /**
+     * Get the theme manager.
+     *
+     * @return ThemeManager Returns the theme manager.
+     */
+    public function getThemeManager() {
+        return $this->themeManager;
+    }
+
+    /**
+     * Get the request.
+     *
+     * @return Request Returns the request.
+     */
+    public function getRequest() {
+        return static::$request;
+    }
+
+    /**
+     * Get the token storage.
+     *
+     * @return TokenStorageInterface Returns the token storage.
+     */
+    public function getTokenStorage() {
+        return $this->tokenStorage;
+    }
+
+    /**
+     * Get the current user.
+     *
+     * @return UserInterface Returns the current user in case of success, null otherwise.
+     */
+    public function getUser() {
+        $token = null;
+        if (null === $this->user) {
+            $token = $this->getTokenStorage()->getToken();
+        }
+        if (null !== $token) {
+            $this->user = $token->getUser();
+        }
+        if (true === ($this->user instanceof UserInterface)) {
+            return $this->user;
+        }
+        return null;
+    }
+
+    /**
+     * On kernel exception.
+     *
+     * @param GetResponseForExceptionEvent $event The event.
+     * @return Event Returns the event.
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event) {
+
+        // Get the exception.
+        $ex = $event->getException();
+
+        // Check the exception.
+        if (true === ($ex instanceOf BadUserRoleException) && null !== $ex->getRedirect()) {
+
+            // Set the response.
+            $event->setResponse(new RedirectResponse($ex->getRedirect()));
+        }
+
+        // Return the event.
+        return $event;
+    }
+
+    /**
+     * On kernel request.
+     *
+     * @param GetResponseEvent $event The event.
+     * @return void
+     */
+    public function onKernelRequest(GetResponseEvent $event) {
+
+        // Initialize the request.
+        $this->setRequest($event->getRequest());
+
+        // Register the providers.
+        $this->getThemeManager()->addGlobal();
+    }
+
+    /**
+     * Set the providers manager.
+     *
+     * @param ThemeManager $themeManager The theme manager.
+     * @return KernelEventListener Returns this kernel event listener.
+     */
+    protected function setThemeManager(ThemeManager $themeManager) {
+        $this->themeManager = $themeManager;
+        return $this;
+    }
+
+    /**
+     * Set the request.
+     *
+     * @param Request $request The request.
+     * @return KernelEventListener Returns this kernel event listener.
+     */
+    protected function setRequest(Request $request) {
+        self::$request = $request;
+        return $this;
+    }
+
+    /**
+     * Set the token storage.
+     *
+     * @param TokenStorageInterface $tokenStorage The token storage.
+     * @return KernelEventListener Returns this kernel event listener.
+     */
+    protected function setTokenStorage(TokenStorageInterface $tokenStorage) {
+        $this->tokenStorage = $tokenStorage;
+        return $this;
+    }
+
+}
---
+++ Tests/EventListener/KernelEventListenerTest.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\EventListener;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use WBW\Bundle\CoreBundle\EventListener\KernelEventListener;
+use WBW\Bundle\CoreBundle\Exception\BadUserRoleException;
+use WBW\Bundle\CoreBundle\Manager\ThemeManager;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Kernel event listener test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\EventListener
+ */
+class KernelEventListenerTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Providers manager.
+     *
+     * @var ProviderManager
+     */
+    private $themeManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Theme manager mock.
+        $this->themeManager = new ThemeManager($this->twigEnvironment);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new KernelEventListener($this->tokenStorage, $this->themeManager);
+
+        $this->assertEquals("webeweb.core.eventlistener.kernel", KernelEventListener::SERVICE_NAME);
+        $this->assertNull($obj->getRequest());
+        $this->assertNull($obj->getUser());
+    }
+
+    /**
+     * Tests the getUser() method.
+     *
+     * @return void
+     * @depends testConstruct
+     */
+    public function testGetUser() {
+
+        $obj = new KernelEventListener($this->tokenStorage, $this->themeManager);
+
+        $this->user = null;
+        $this->assertNull($obj->getUser());
+
+        $this->user = $this->getMockBuilder(UserInterface::class)->getMock();
+        $this->assertSame($this->user, $obj->getUser());
+    }
+
+    /**
+     * Tests the onKernelException() method.
+     *
+     * @return void
+     */
+    public function testOnKernelException() {
+
+        $obj = new KernelEventListener($this->tokenStorage, $this->themeManager);
+
+        $arg = new GetResponseForExceptionEvent($this->kernel, new Request(), HttpKernelInterface::MASTER_REQUEST, new Exception());
+        $this->assertSame($arg, $obj->onKernelException($arg));
+    }
+
+    /**
+     * Tests the onKernelException() method.
+     *
+     * @return void
+     */
+    public function testOnKernelExceptionWithBadUserRoleException() {
+
+        $obj = new KernelEventListener($this->tokenStorage, $this->themeManager);
+
+        // Set an User modk.
+        $this->user = $this->getMockBuilder(UserInterface::class)->getMock();
+
+        $arg = new GetResponseForExceptionEvent($this->kernel, new Request(), HttpKernelInterface::MASTER_REQUEST, new BadUserRoleException($this->user, [], "route", "/"));
+        $this->assertSame($arg, $obj->onKernelException($arg));
+    }
+
+    /**
+     * Tests the onKernelRequest() method.
+     *
+     * @return void
+     * @depends testConstruct
+     */
+    public function testOnKernelRequest() {
+
+        $obj = new KernelEventListener($this->tokenStorage, $this->themeManager);
+
+        $obj->onKernelRequest(new GetResponseEvent($this->kernel, new Request(), HttpKernelInterface::MASTER_REQUEST));
+        $this->assertInstanceOf(Request::class, $obj->getRequest());
+    }
+
+}
