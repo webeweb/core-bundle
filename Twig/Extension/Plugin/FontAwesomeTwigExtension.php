4deaab38b54c7a57d2e421a00a07fe8e516f6d9b
webeweb <support@webeweb.fr>
2018-11-12 17:06:18 +0100

    Add Font Awesome Twig extension

---
+++ Twig/Extension/Plugin/FontAwesomeTwigExtension.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Twig\Extension\Plugin;
+
+use Twig_Environment;
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
+use WBW\Bundle\CoreBundle\Renderer\IconRendererInterface;
+use WBW\Library\Core\Argument\ArrayHelper;
+
+/**
+ * Font Awesome Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Twig\Extension\Plugin
+ */
+class FontAwesomeTwigExtension extends AbstractFontAwesomeTwigExtension implements IconRendererInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.twig.extension.plugin.font_awesome";
+
+    /**
+     * Constructor.
+     *
+     * @param Twig_Environment $twigEnvironment The wig environment.
+     */
+    public function __construct(Twig_Environment $twigEnvironment) {
+        parent::__construct($twigEnvironment);
+    }
+
+    /**
+     * Displays a Font Awesome icon.
+     *
+     * @param array $args The arguments.
+     * @return string Returns the Font Awesome icon.
+     */
+    public function fontAwesomeIconFunction(array $args = []) {
+        return $this->fontAwesomeIcon(ArrayHelper::get($args, "font"), ArrayHelper::get($args, "name", "home"), ArrayHelper::get($args, "size"), ArrayHelper::get($args, "fixedWidth", false), ArrayHelper::get($args, "bordered", false), ArrayHelper::get($args, "pull"), ArrayHelper::get($args, "animated"), ArrayHelper::get($args, "style"));
+    }
+
+    /**
+     * Displays a Font Awesome list.
+     *
+     * @param array|string $items The items.
+     * @return string Returns the Font Awesome list.
+     */
+    public function fontAwesomeListFilter($items) {
+        return $this->fontAwesomeList($items);
+    }
+
+    /**
+     * Displays a Font Awesome list icon.
+     *
+     * @param string $icon The icon.
+     * @param string $content The content.
+     * @return string Returns the Font Awesome list icon.
+     */
+    public function fontAwesomeListIconFilter($icon, $content) {
+        return $this->fontAwesomeListIcon($icon, $content);
+    }
+
+    /**
+     * Get the Twig filters.
+     *
+     * @return Twig_SimpleFilter[] Returns the Twig filters.
+     */
+    public function getFilters() {
+        return [
+            new Twig_SimpleFilter("fontAwesomeList", [$this, "fontAwesomeListFilter"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFilter("fontAwesomeListIcon", [$this, "fontAwesomeListIconFilter"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("fontAwesomeIcon", [$this, "fontAwesomeIconFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function renderIcon($name, $style) {
+        return $this->fontAwesomeIconFunction(["name" => $name, "style" => $style]);
+    }
+
+}
