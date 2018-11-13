756ca659e028fa5f59aa65215d0db2016d047acf
webeweb <support@webeweb.fr>
2018-11-13 17:35:07 +0100

    Add test kernel

---
+++ Tests/Fixtures/app/TestKernel.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the bootstrap-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use WBW\Bundle\CoreBundle\Tests\AbstractKernel;
+
+/**
+ * Test kernel.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures
+ */
+class TestKernel extends AbstractKernel {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles() {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new WBW\Bundle\CoreBundle\CoreBundle(),
+        ];
+        return $bundles;
+    }
+
+}
