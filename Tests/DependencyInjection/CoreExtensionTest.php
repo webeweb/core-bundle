6468d58d0ec61a696785abad509ff4f13e256fcd
webeweb <support@webeweb.fr>
2018-11-13 17:34:12 +0100

    Set Twig extensions as service

--- Tests/DependencyInjection/CoreExtensionTest.php
+++ Tests/DependencyInjection/CoreExtensionTest.php
@@ -16,6 +16,11 @@ use WBW\Bundle\CoreBundle\EventListener\KernelEventListener;
 use WBW\Bundle\CoreBundle\EventListener\NotificationEventListener;
 use WBW\Bundle\CoreBundle\Manager\ThemeManager;
 use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\FontAwesomeTwigExtension;
+use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\JQueryInputMaskTwigExtension;
+use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\MaterialDesignIconicFontTwigExtension;
+use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\MeteoconsTwigExtension;
+use WBW\Bundle\CoreBundle\Twig\Extension\RendererTwigExtension;
 
 /**
  * Core extension test.
@@ -42,6 +47,15 @@ class CoreExtensionTest extends AbstractFrameworkTestCase {
 
         // Managers
         $this->assertInstanceOf(ThemeManager::class, $this->containerBuilder->get(ThemeManager::SERVICE_NAME));
+
+        // Twig extensions Plug-ins
+        $this->assertInstanceOf(FontAwesomeTwigExtension::class, $this->containerBuilder->get(FontAwesomeTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(JQueryInputMaskTwigExtension::class, $this->containerBuilder->get(JQueryInputMaskTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(MaterialDesignIconicFontTwigExtension::class, $this->containerBuilder->get(MaterialDesignIconicFontTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(MeteoconsTwigExtension::class, $this->containerBuilder->get(MeteoconsTwigExtension::SERVICE_NAME));
+
+        // Twig extensions Renderers
+        $this->assertInstanceOf(RendererTwigExtension::class, $this->containerBuilder->get(RendererTwigExtension::SERVICE_NAME));
     }
 
 }
