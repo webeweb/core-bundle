69a754e8c782abd19170af3c087ba20ea00c9b9d
webeweb <support@webeweb.fr>
2018-11-15 17:40:42 +0100

    Add Session trait

---
+++ Service/SessionTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Service;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Session trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Service
+ */
+trait SessionTrait {
+
+    /**
+     * Session.
+     *
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * Get the session.
+     *
+     * @return SessionInterface Returns the session.
+     */
+    public function getSession() {
+        return $this->session;
+    }
+
+    /**
+     * Set the session.
+     *
+     * @param SessionInterface $session The session.
+     */
+    protected function setSession(SessionInterface $session) {
+        $this->session = $session;
+        return $this;
+    }
+
+}
---
+++ Tests/Fixtures/Service/TestSessionTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Fixtures\Service;
+
+use WBW\Bundle\CoreBundle\Service\SessionTrait;
+
+/**
+ * Test session trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures\Service
+ */
+class TestSessionTrait {
+
+    use SessionTrait {
+        setSession as public;
+    }
+}
