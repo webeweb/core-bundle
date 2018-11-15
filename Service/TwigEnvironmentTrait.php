e074a80b5c03c0d84ff48a8af685e2ac72b87740
webeweb <support@webeweb.fr>
2018-11-15 17:33:50 +0100

    Add Twig environment trait

---
+++ Service/TwigEnvironmentTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Service;
+
+use Twig_Environment;
+
+/**
+ * Twig environment trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Service
+ */
+trait TwigEnvironmentTrait {
+
+    /**
+     * Twig environment.
+     *
+     * @var Twig_Environment
+     */
+    private $twigEnvironment;
+
+    /**
+     * Get the Twig environment.
+     *
+     * @return Twig_Environment Returns the Twig environment.
+     */
+    public function getTwigEnvironment() {
+        return $this->twigEnvironment;
+    }
+
+    /**
+     * Set the Twig environment.
+     *
+     * @param Twig_Environment $twigEnvironment The Twig environment.
+     */
+    protected function setTwigEnvironment(Twig_Environment $twigEnvironment) {
+        $this->twigEnvironment = $twigEnvironment;
+        return $this;
+    }
+
+}
---
+++ Tests/Service/TwigEnvironmentTraitTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Service;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Service\TestTwigEnvironmentTrait;
+
+/**
+ * Twig environment trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Service
+ */
+class TwigEnvironmentTraitTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestTwigEnvironmentTrait();
+
+        $this->assertNull($obj->getTwigEnvironment());
+    }
+
+    /**
+     * Tests the setTwigEnvironment() method.
+     *
+     * @return void
+     */
+    public function testSetTwigEnvironment() {
+
+        $obj = new TestTwigEnvironmentTrait();
+
+        $obj->setTwigEnvironment($this->twigEnvironment);
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+
+}
