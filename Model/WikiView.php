42087638d6de0c32057a6d81a75ec9518c47f542
webeweb <support@webeweb.fr>
2018-11-17 13:34:45 +0100

    Add Wiki view model

---
+++ Model/WikiView.php
@@ -0,0 +1,173 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Model;
+
+/**
+ * Wiki view model.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Model
+ */
+class WikiView {
+
+    /**
+     * Bundle.
+     *
+     * @var string
+     */
+    private $bundle;
+
+    /**
+     * Category.
+     *
+     * @var string
+     */
+    private $category;
+
+    /**
+     * Package.
+     *
+     * @var string
+     */
+    private $package;
+
+    /**
+     * Page.
+     *
+     * @var string
+     */
+    private $page;
+
+    /**
+     * Title.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Constructor.
+     *
+     * @param string $category The category.
+     * @param string $package The package.
+     * @param string $page The page.
+     * @param string $title The title.
+     */
+    public function __construct($category, $package, $page, $title) {
+        $this->setBundle("Core");
+        $this->setCategory($category);
+        $this->setPackage($package);
+        $this->setPage($page);
+        $this->setTitle($title);
+    }
+
+    /**
+     * Get the bundle.
+     *
+     * @return string Returns the bundle.
+     */
+    public function getBundle() {
+        return $this->bundle;
+    }
+
+    /**
+     * Get the category.
+     *
+     * @return string Returns the category.
+     */
+    public function getCategory() {
+        return $this->category;
+    }
+
+    /**
+     * Get the package.
+     *
+     * @return string Returns the package.
+     */
+    public function getPackage() {
+        return $this->package;
+    }
+
+    /**
+     * Get the page.
+     *
+     * @return string Returns the page.
+     */
+    public function getPage() {
+        return $this->page;
+    }
+
+    /**
+     * Get the title.
+     *
+     * @return string Returns the title.
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * Set the bundle.
+     *
+     * @param string $bundle The bundle.
+     * @return WikiView Returns this wiki view.
+     */
+    public function setBundle($bundle) {
+        $this->bundle = $bundle;
+        return $this;
+    }
+
+    /**
+     * Set the category.
+     *
+     * @param string $category The category.
+     * @return WikiView Returns this wiki view.
+     */
+    public function setCategory($category) {
+        $this->category = $category;
+        return $this;
+    }
+
+    /**
+     * Set the package.
+     *
+     * @param string $package The package.
+     * @return WikiView Returns this wiki view.
+     */
+    public function setPackage($package) {
+        $this->package = $package;
+        return $this;
+    }
+
+    /**
+     * Set the page.
+     *
+     * @param string $page The page.
+     * @return WikiView Returns this wiki view.
+     */
+    public function setPage($page) {
+        $this->page = $page;
+        return $this;
+    }
+
+    /**
+     * Set the title.
+     *
+     * @param string $title The title.
+     * @return WikiView Returns this wiki view.
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+}
---
+++ Tests/Model/WikiViewTest.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Model;
+
+use WBW\Bundle\CoreBundle\Model\WikiView;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Wiki view model test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Model
+ */
+class WikiViewTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new WikiView("category", "package", "page", "title");
+
+        $this->assertEquals("Core", $obj->getBundle());
+        $this->assertEquals("category", $obj->getCategory());
+        $this->assertEquals("package", $obj->getPackage());
+        $this->assertEquals("page", $obj->getPage());
+        $this->assertEquals("title", $obj->getTitle());
+    }
+
+    /**
+     * Tests the setBundle() method.
+     *
+     * @return void
+     */
+    public function testSetBundle() {
+
+        $obj = new WikiView("category", "package", "page", "title");
+
+        $obj->setBundle("bundle");
+        $this->assertEquals("bundle", $obj->getBundle());
+    }
+
+    /**
+     * Tests the setCategory() method.
+     *
+     * @return void
+     */
+    public function testSetCategory() {
+
+        $obj = new WikiView("category", "package", "page", "title");
+
+        $obj->setCategory("category2");
+        $this->assertEquals("category2", $obj->getCategory());
+    }
+
+    /**
+     * Tests the setPackage() method.
+     *
+     * @return void
+     */
+    public function testSetPackage() {
+
+        $obj = new WikiView("category", "package", "page", "title");
+
+        $obj->setPackage("package2");
+        $this->assertEquals("package2", $obj->getPackage());
+    }
+
+    /**
+     * Tests the setPage() method.
+     *
+     * @return void
+     */
+    public function testSetPage() {
+
+        $obj = new WikiView("category", "package", "page", "title");
+
+        $obj->setPage("page2");
+        $this->assertEquals("page2", $obj->getPage());
+    }
+
+    /**
+     * Tests the setTitle() method.
+     *
+     * @return void
+     */
+    public function testSetTitle() {
+
+        $obj = new WikiView("category", "package", "page", "title");
+
+        $obj->setTitle("title2");
+        $this->assertEquals("title2", $obj->getTitle());
+    }
+
+}
