47ab8add862d7701275b1587d86c6332a36139b8
webeweb <support@webeweb.fr>
2018-11-20 18:50:04 +0100

    Add Navigation node "users" with a Font Awesome icon

---
+++ Tests/Navigation/FontAwesome/NavigationNodeUsersTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Navigation\FontAwesome\NavigationNodeUsers;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation node "Users" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome
+ */
+class NavigationNodeUsesrTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeUsers("route");
+
+        $this->assertEquals("navigation.node.users", $obj->getId());
+        $this->assertEquals("fa:user", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
