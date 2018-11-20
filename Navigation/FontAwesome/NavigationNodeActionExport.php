535729df84b837c38fdaef61bb16d30b6b48d81d
webeweb <support@webeweb.fr>
2018-11-20 18:47:55 +0100

    Add Navigation node action "export" with a Font Awesome icon

---
+++ Navigation/FontAwesome/NavigationNodeActionExport.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Navigation\FontAwesome;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationNode;
+
+/**
+ * Navigation node action "Export".
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Navigation\FontAwesome
+ */
+class NavigationNodeActionExport extends NavigationNode {
+
+    /**
+     * Constructor.
+     *
+     * @param string $uri The URI.
+     * @param string $matcher The matcher.
+     */
+    public function __construct($uri = null, $matcher = self::NAVIGATION_MATCHER_URL) {
+        parent::__construct("navigation.node.action.export", "fa:download", $uri, $matcher);
+    }
+
+}
---
+++ Tests/Navigation/FontAwesome/NavigationNodeActionExportTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome;
+
+use WBW\Bundle\CoreBundle\Navigation\FontAwesome\NavigationNodeActionExport;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation node action "Export" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome
+ */
+class NavigationNodeActionExportTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeActionExport("route");
+
+        $this->assertEquals("navigation.node.action.export", $obj->getId());
+        $this->assertEquals("fa:download", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
