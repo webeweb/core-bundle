e5bcb9c27f5d9ea3bdaa59c7bb514019036dae57
webeweb <support@webeweb.fr>
2018-11-20 18:55:13 +0100

    Add Navigation node "users groups" with a Material Design Iconic Font icon

---
+++ Tests/Navigation/MaterialDesignIconicFont/NavigationNodeUsersGroupsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Navigation\MaterialDesignIconicFont\NavigationNodeUsersGroups;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation node "Users groups" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont
+ */
+class NavigationNodeUsersGroupsTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeUsersGroups("route");
+
+        $this->assertEquals("navigation.node.usersGroups", $obj->getId());
+        $this->assertEquals("zmdi:accounts", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
