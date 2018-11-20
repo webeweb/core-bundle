ff665066be39a7975691ba9e81e06050573a5458
webeweb <support@webeweb.fr>
2018-11-20 18:53:09 +0100

    Add Navigation node action "import" with a Material Design Iconic Font icon

---
+++ Tests/Navigation/MaterialDesignIconicFont/NavigationNodeActionImportTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont;
+
+use WBW\Bundle\CoreBundle\Navigation\MaterialDesignIconicFont\NavigationNodeActionImport;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation node action "Import" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont
+ */
+class NavigationNodeActionImportTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeActionImport("route");
+
+        $this->assertEquals("navigation.node.action.import", $obj->getId());
+        $this->assertEquals("zmdi:upload", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
