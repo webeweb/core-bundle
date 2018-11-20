c9cd87787310deb1d8f7216aee9bca8cf63a2fab
webeweb <support@webeweb.fr>
2018-11-20 18:54:30 +0100

    Add Navigation node "parameters" with a Material Design Iconic Font icon

---
+++ Tests/Navigation/MaterialDesignIconicFont/NavigationNodeParametersTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Navigation\MaterialDesignIconicFont\NavigationNodeParameters;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation node "Parameters" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont
+ */
+class NavigationNodeParametersTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeParameters("route");
+
+        $this->assertEquals("navigation.node.parameters", $obj->getId());
+        $this->assertEquals("zmdi:wrench", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
