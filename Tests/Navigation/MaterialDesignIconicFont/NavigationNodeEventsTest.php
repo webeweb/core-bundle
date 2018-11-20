b2e4219c6ac9d415e78eecdca6dff4cd824b6f3b
webeweb <support@webeweb.fr>
2018-11-20 18:54:19 +0100

    Add Navigation node "events" with a Material Design Iconic Font icon

---
+++ Tests/Navigation/MaterialDesignIconicFont/NavigationNodeEventsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont;
+
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Navigation\MaterialDesignIconicFont\NavigationNodeEvents;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Navigation node "Events" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\MaterialDesignIconicFont
+ */
+class NavigationNodeEventsTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeEvents("route");
+
+        $this->assertEquals("navigation.node.events", $obj->getId());
+        $this->assertEquals("zmdi:calendar", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
