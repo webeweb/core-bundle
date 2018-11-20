f2d1a0eb662084ba99204a212412df47ef8548a9
webeweb <support@webeweb.fr>
2018-11-20 18:52:14 +0100

    Add Breadcrumb node action "new user" with a Material Design Iconic Font icon

---
+++ Tests/Navigation/MaterialDesignIconicFont/BreadcrumbNodeActionNewUserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Model\Navigation\MaterialDesignIconicFont;
+
+use WBW\Bundle\CoreBundle\Navigation\MaterialDesignIconicFont\BreadcrumbNodeActionNewUser;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Breadcrumb node action "New user" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Model\Navigation\MaterialDesignIconicFont
+ */
+class BreadcrumbNodeActionNewUserTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BreadcrumbNodeActionNewUser("route");
+
+        $this->assertEquals("navigation.node.action.new", $obj->getId());
+        $this->assertEquals("zmdi:account-add", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
