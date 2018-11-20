784fbf3ab5e32c0f2940776f7087b53e39f70adf
webeweb <support@webeweb.fr>
2018-11-20 18:46:25 +0100

    Add Breadcrumb node action "import" with a Font Awesome icon

---
+++ Tests/Navigation/FontAwesome/BreadcrumbNodeActionImportTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome;
+
+use WBW\Bundle\CoreBundle\Navigation\FontAwesome\BreadcrumbNodeActionImport;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Breadcrumb node action "Import" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome
+ */
+class BreadcrumbNodeActionImportTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BreadcrumbNodeActionImport("route");
+
+        $this->assertEquals("navigation.node.action.import", $obj->getId());
+        $this->assertEquals("fa:upload", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
