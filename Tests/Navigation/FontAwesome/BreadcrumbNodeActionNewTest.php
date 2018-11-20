6633ce07fc82b2ff5a4026e781ffaa59b3c2d8cc
webeweb <support@webeweb.fr>
2018-11-20 18:46:47 +0100

    Add Breadcrumb node action "new" with a Font Awesome icon

---
+++ Tests/Navigation/FontAwesome/BreadcrumbNodeActionNewTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome;
+
+use WBW\Bundle\CoreBundle\Navigation\FontAwesome\BreadcrumbNodeActionNew;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Breadcrumb node action "New" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation\FontAwesome
+ */
+class BreadcrumbNodeActionNewTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BreadcrumbNodeActionNew("route");
+
+        $this->assertEquals("navigation.node.action.new", $obj->getId());
+        $this->assertEquals("fa:plus", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
