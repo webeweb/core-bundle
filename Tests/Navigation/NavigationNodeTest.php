3e6333b852cb0df7dedc086f1e2c821d66ddffc9
webeweb <support@webeweb.fr>
2018-11-21 18:46:08 +0100

    Fix namespace + getUri() case

--- Tests/Navigation/NavigationNodeTest.php
+++ Tests/Navigation/NavigationNodeTest.php
@@ -9,10 +9,9 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\CoreBundle\Tests\Model\Navigation;
+namespace WBW\Bundle\CoreBundle\Tests\Navigation;
 
 use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
-use WBW\Bundle\CoreBundle\Navigation\NavigationItem;
 use WBW\Bundle\CoreBundle\Navigation\NavigationNode;
 use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
 
@@ -20,7 +19,7 @@ use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
  * Navigation node test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\CoreBundle\Tests\Model\Navigation
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation
  */
 class NavigationNodeTest extends AbstractFrameworkTestCase {
 
@@ -38,112 +37,8 @@ class NavigationNodeTest extends AbstractFrameworkTestCase {
         $this->assertNull($obj->getIcon());
         $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
         $this->assertNull($obj->getTarget());
-        $this->assertNull($obj->getURI());
+        $this->assertNull($obj->getUri());
         $this->assertTrue($obj->getVisible());
-    }
-
-    /**
-     * Tests the isDisplayable() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testIsDisplayable() {
-
-        $obj = new NavigationNode("id");
-
-        $this->assertSame($obj, $obj->addNode(new NavigationItem("id1")));
-        $this->assertSame($obj, $obj->addNode(new NavigationNode("id2")));
-
-        $this->assertFalse($obj->isDisplayable());
-
-        $this->assertNotSame($obj, $obj->getLastNode()->setActive(true));
-        $this->assertNotSame($obj, $obj->getLastNode()->setEnable(true));
-        $this->assertTrue($obj->isDisplayable());
-    }
-
-    /**
-     * Tests the setActive() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testSetActive() {
-
-        $obj = new NavigationNode("id");
-
-        $obj->setActive(true);
-        $this->assertTrue($obj->getActive());
-    }
-
-    /**
-     * Tests the setEnable() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testSetEnable() {
-
-        $obj = new NavigationNode("id");
-
-        $obj->setEnable(true);
-        $this->assertTrue($obj->getEnable());
-    }
-
-    /**
-     * Tests the setIcon() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testSetIcon() {
-
-        $obj = new NavigationNode("id");
-
-        $obj->setIcon("icon");
-        $this->assertEquals("icon", $obj->getIcon());
-    }
-
-    /**
-     * Tests the setMatcher() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testSetMatcher() {
-
-        $obj = new NavigationNode("id");
-
-        $obj->setMatcher(NavigationInterface::NAVIGATION_MATCHER_ROUTER);
-        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_ROUTER, $obj->getMatcher());
-    }
-
-    /**
-     * Tests the setTarget() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testSetTarget() {
-
-        $obj = new NavigationNode("id");
-
-        $obj->setTarget("_blank");
-        $this->assertEquals("_blank", $obj->getTarget());
-    }
-
-    /**
-     * Tests the setURI() method.
-     *
-     * @return void
-     * @depends testConstruct
-     */
-    public function testSetURI() {
-
-        $obj = new NavigationNode("id");
-
-        $obj->setURI("route");
-        $this->assertEquals("route", $obj->getURI());
     }
 
 }
