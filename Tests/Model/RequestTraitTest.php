c738b3ad75c2ece022385832f3fbe67abb94e7eb
webeweb <support@webeweb.fr>
2018-11-22 17:24:45 +0100

    Add Request trait

---
+++ Tests/Model/RequestTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Model;
+
+use Symfony\Component\HttpFoundation\Request;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Model\TestRequestTrait;
+
+/**
+ * Request trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Model
+ */
+class RequestTraitTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestRequestTrait();
+
+        $this->assertNull($obj->getRequest());
+    }
+
+    /**
+     * Tests the setRequest() method.
+     *
+     * @return void
+     */
+    public function testSetRequest() {
+
+        // Set a Request mock.
+        $request = new Request();
+
+        $obj = new TestRequestTrait();
+
+        $obj->setRequest($request);
+        $this->assertSame($request, $obj->getRequest());
+    }
+
+}
