61acc841896430d03ca00baef26a5293be48ab12
webeweb <support@webeweb.fr>
2018-11-25 14:53:15 +0100

    Add Assets provider interface

---
+++ Provider/AssetsProviderInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider;
+
+/**
+ * Assets provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider
+ */
+interface AssetsProviderInterface extends ProviderInterface {
+
+    /**
+     * Get the assets directory.
+     *
+     * @return string Returns the assets directory.
+     */
+    public function getAssetsDirectory();
+}
