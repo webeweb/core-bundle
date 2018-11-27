ffed2ea065f73ed74c2afd5c1c262f0b88107e8a
webeweb <support@webeweb.fr>
2018-11-27 08:41:58 +0100

    Add Command helper

---
+++ Helper/CommandHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Helper;
+
+/**
+ * Command helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Helper
+ */
+class CommandHelper {
+
+    /**
+     * Get a checkbox.
+     *
+     * @param bool $checked Checked ?
+     * @return string Returns the checkbox.
+     */
+    public static function getCheckbox($checked) {
+        if (true === $checked) {
+            return sprintf("<fg=green;options=bold>%s</>", "\\" === DIRECTORY_SEPARATOR ? "OK" : "\xE2\x9C\x94");
+        }
+        return sprintf("<fg=yellow;options=bold>%s</>", "\\" === DIRECTORY_SEPARATOR ? "KO" : "!");
+    }
+
+}
---
+++ Tests/Helper/CommandHelperTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Helper;
+
+use WBW\Bundle\CoreBundle\Helper\CommandHelper;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * Command helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Helper
+ */
+class CommandHelperTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the getCheckbox() method.
+     *
+     * @return void
+     */
+    public function testGetCheckbox() {
+
+        // Determines the operating system.
+        if ("\\" !== DIRECTORY_SEPARATOR) {
+
+            $this->assertEquals("<fg=green;options=bold>\xE2\x9C\x94</>", CommandHelper::getCheckbox(true));
+            $this->assertEquals("<fg=yellow;options=bold>!</>", CommandHelper::getCheckbox(false));
+        } else {
+
+            $this->assertEquals("<fg=green;options=bold>OK</>", CommandHelper::getCheckbox(true));
+            $this->assertEquals("<fg=yellow;options=bold>WARNING</>", CommandHelper::getCheckbox(false));
+        }
+    }
+
+}
