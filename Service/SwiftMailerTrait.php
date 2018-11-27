a96335fb8dd215d03d6e84a716c3650ca5b6a913
webeweb <support@webeweb.fr>
2018-11-27 17:08:39 +0100

    Add Swift mailer trait

---
+++ Service/SwiftMailerTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Service;
+
+use Swift_Mailer;
+
+/**
+ * Swift mailer trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Service
+ */
+trait SwiftMailerTrait {
+
+    /**
+     * Swift mailer.
+     *
+     * @var Swift_Mailer
+     */
+    private $swiftMailer;
+
+    /**
+     * Get the swift mailer.
+     *
+     * @return Swift_Mailer Returns the swift mailer.
+     */
+    public function getSwiftMailer() {
+        return $this->swiftMailer;
+    }
+
+    /**
+     * Set the swift mailer.
+     *
+     * @param Swift_Mailer $swiftMailer The swift mailer.
+     */
+    protected function setSwiftMailer(Swift_Mailer $swiftMailer = null) {
+        $this->swiftMailer = $swiftMailer;
+        return $this;
+    }
+
+}
---
+++ Tests/Service/SwiftMailerTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Service;
+
+use Swift_Mailer;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Service\TestSwiftMailerTrait;
+
+/**
+ * Swift mailer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Service
+ */
+class SwiftMailerTraitTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestSwiftMailerTrait();
+
+        $this->assertNull($obj->getSwiftMailer());
+    }
+
+    /**
+     * Tests the setSwiftMailer() method.
+     *
+     * @return void
+     */
+    public function testSetSwiftMailer() {
+
+        // Set a Swift mailer mock.
+        $swiftMailer = $this->getMockBuilder(Swift_Mailer::class)->disableOriginalConstructor()->getMock();
+
+        $obj = new TestSwiftMailerTrait();
+
+        $obj->setSwiftMailer($swiftMailer);
+        $this->assertSame($swiftMailer, $obj->getSwiftMailer());
+    }
+
+}
