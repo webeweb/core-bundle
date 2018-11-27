ed6df7ef03087c8d1ae4a35d719ecb5817f59e1f
webeweb <support@webeweb.fr>
2018-11-27 17:08:01 +0100

    Add Notification event listener trait

---
+++ EventListener/NotificationEventListenerTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\EventListener;
+
+/**
+ * Notification event listener trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\EventListener
+ */
+trait NotificationEventListenerTrait {
+
+    /**
+     * Notification event listener.
+     *
+     * @var NotificationEventListner
+     */
+    private $notificationEventListener;
+
+    /**
+     * Get the Notification event listener.
+     *
+     * @return NotificationEventListner Returns the notification event listener.
+     */
+    public function getNotificationEventListener() {
+        return $this->notificationEventListener;
+    }
+
+    /**
+     * Set the notification event listener.
+     *
+     * @param NotificationEventListener $notificationEventListener The notification event listener.
+     */
+    protected function setNotificationEventListener(NotificationEventListener $notificationEventListener = null) {
+        $this->notificationEventListener = $notificationEventListener;
+        return $this;
+    }
+
+}
---
+++ Tests/EventListener/NotificationEventListenerTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Service;
+
+use WBW\Bundle\CoreBundle\EventListener\NotificationEventListener;
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\EventListener\TestNotificationEventListenerTrait;
+
+/**
+ * Notification event listener trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Service
+ */
+class NotificationEventListenerTraitTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestNotificationEventListenerTrait();
+
+        $this->assertNull($obj->getNotificationEventListener());
+    }
+
+    /**
+     * Tests the setNotificationEVentListener() method.
+     *
+     * @return void
+     */
+    public function testSetNotificationEVentListener() {
+
+        // Set a Notification event listener mock.
+        $notificationEventListener = new NotificationEventListener($this->session);
+
+        $obj = new TestNotificationEventListenerTrait();
+
+        $obj->setNotificationEventListener($notificationEventListener);
+        $this->assertSame($notificationEventListener, $obj->getNotificationEventListener());
+    }
+
+}
