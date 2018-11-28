89559a642dff45f91f30a67b71d44cb299e0dde6
webeweb <support@webeweb.fr>
2018-11-28 17:23:39 +0100

    Add Container trait

---
+++ DependencyInjection/ContainerTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Container;
+
+/**
+ * Container trait
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\DependencyInjection
+ */
+trait ContainerTrait {
+
+    /**
+     * Container.
+     *
+     * @var Container.
+     */
+    private $container;
+
+    /**
+     * Get the container.
+     *
+     * @return Container Returns the container.
+     */
+    public function getContainer() {
+        return $this->container;
+    }
+
+    /**
+     * Set the container.
+     *
+     * @param Container $container The container.
+     */
+    protected function setContainer(Container $container) {
+        $this->container = $container;
+        return $this;
+    }
+
+}
---
+++ Tests/DependencyInjection/ContainerTraitTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\DependencyInjection;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractFrameworkTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\DependencyInjection\TestContainerTrait;
+
+/**
+ * Container trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\DependencyInjection
+ */
+class ContainerTraitTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestContainerTrait();
+
+        $this->assertNull($obj->getContainer());
+    }
+
+    /**
+     * Tests the setContainer() method.
+     *
+     * @return void
+     */
+    public function testSetContainer() {
+
+        $obj = new TestContainerTrait();
+
+        $obj->setContainer($this->containerBuilder);
+        $this->assertSame($this->containerBuilder, $obj->getContainer());
+    }
+
+}
