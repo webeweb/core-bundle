5c50f3cd5c225bd3f1de2365d9c5f3dcd253a9e2
webeweb <support@webeweb.fr>
2018-12-19 16:22:21 +0100

    Add OS helper

---
+++ Tests/Helper/OSHelperTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Helper;
+
+use WBW\Bundle\CoreBundle\Helper\OSHelper;
+
+/**
+ * OS helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Helper
+ */
+class OSHelperTest {
+
+    /**
+     * Tests the isLinux() method.
+     *
+     * @return void
+     */
+    public function testIsLinux() {
+
+        // Determines the operating system.
+        if ("\\" !== DIRECTORY_SEPARATOR) {
+
+            $this->assertTrue(OSHelper::isLinux());
+        } else {
+
+            $this->assertFalse(OSHelper::isLinux());
+        }
+    }
+
+    /**
+     * Tests the isWindows() method.
+     *
+     * @return void
+     */
+    public function testIsWindows() {
+
+        // Determines the operating system.
+        if ("\\" !== DIRECTORY_SEPARATOR) {
+
+            $this->assertFalse(OSHelper::isWindows());
+        } else {
+
+            $this->assertFalse(OSHelper::isWindows());
+        }
+    }
+
+}
