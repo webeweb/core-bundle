8ebe91f93424076124adb43f4dc1eb0a64e94068
webeweb <support@webeweb.fr>
2018-12-20 16:42:26 +0100

    Add PhantomJS helper trait

---
+++ Helper/PhantomJSHelperTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Helper;
+
+/**
+ * PhantomJSHelperTrait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Helper
+ */
+trait PhantomJSHelperTrait {
+
+    /**
+     * PathomJS helper.
+     *
+     * @var PhantomJSHelper
+     */
+    private $phantomJSHelper;
+
+    /**
+     * Get the phantomJS helper.
+     *
+     * @return PhantomJSHelper Returns the phantomJS helper.
+     */
+    public function getPhantomJSHelper() {
+        return $this->phantomJSHelper;
+    }
+
+    /**
+     * Set the phantomJS helper.
+     *
+     * @param PhantomJSHelper $phantomJSHelper The phantomJS helper.
+     */
+    protected function setPhantomJSHelper(PhantomJSHelper $phantomJSHelper = null) {
+        $this->phantomJSHelper = $phantomJSHelper;
+        return $this;
+    }
+
+}
---
+++ Tests/Helper/PhantomJSHelperTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Helper;
+
+use WBW\Bundle\CoreBundle\Helper\PhantomJSHelper;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Helper\TestPhantomJSHelperTrait;
+
+/**
+ * PhantomJS helper trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Helper
+ */
+class PhantomJSHelperTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestPhantomJSHelperTrait();
+
+        $this->assertNull($obj->getPhantomJSHelper());
+    }
+
+    /**
+     * Tests the setPhantomJSHelper() method.
+     *
+     * @return void
+     */
+    public function testSetPhantomJSHelper() {
+
+        // Set a PhantomJS helper mock.
+        $phantomJSHelper = new PhantomJSHelper("path", "base");
+
+        $obj = new TestPhantomJSHelperTrait();
+
+        $obj->setPhantomJSHelper($phantomJSHelper);
+        $this->assertSame($phantomJSHelper, $obj->getPhantomJSHelper());
+    }
+
+}
