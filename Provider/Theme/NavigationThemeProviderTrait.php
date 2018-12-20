475f7a2b65b5b5c0b722a73f1409345ab2e70e20
webeweb <support@webeweb.fr>
2018-12-20 16:44:24 +0100

    Add navigation theme provider trait

---
+++ Provider/Theme/NavigationThemeProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+/**
+ * Navigation theme provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+trait NavigationThemeProviderTrait {
+
+    /**
+     * Navigation theme provider.
+     *
+     * @var NavigationThemeProviderInterface
+     */
+    private $navigationThemeProvider;
+
+    /**
+     * Get the navigation theme provider.
+     *
+     * @return NavigationThemeProviderInterface Returns the navigation theme provider.
+     */
+    public function getNavigationThemeProvider() {
+        return $this->navigationThemeProvider;
+    }
+
+    /**
+     * Set the navigation theme provider.
+     *
+     * @param NavigationThemeProviderInterface $navigationThemeProvider The navigation theme provider.
+     */
+    protected function setNavigationThemeProvider(NavigationThemeProviderInterface $navigationThemeProvider = null) {
+        $this->navigationThemeProvider = $navigationThemeProvider;
+        return $this;
+    }
+
+}
---
+++ Tests/Provider/Theme/NavigationThemeProviderTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\NavigationThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Provider\Theme\TestNavigationThemeProviderTrait;
+
+/**
+ * Navigation theme provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class NavigationThemeProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestNavigationThemeProviderTrait();
+
+        $this->assertNull($obj->getNavigationThemeProvider());
+    }
+
+    /**
+     * Tests the setNavigationThemeProvider() method.
+     *
+     * @return void
+     */
+    public function testSetNavigationThemeProvider() {
+
+        // Set an Navigation theme provider mock.
+        $navigationThemeProvider = $this->getMockBuilder(NavigationThemeProviderInterface::class)->getMock();
+
+        $obj = new TestNavigationThemeProviderTrait();
+
+        $obj->setNavigationThemeProvider($navigationThemeProvider);
+        $this->assertSame($navigationThemeProvider, $obj->getNavigationThemeProvider());
+    }
+
+}
