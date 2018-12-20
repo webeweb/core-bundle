f1637dfcbb97db2464f6f5249286e578cc851a45
webeweb <support@webeweb.fr>
2018-12-20 16:45:05 +0100

    Add tasks drop down theme provider trait

---
+++ Provider/Theme/TasksDropDownThemeProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+/**
+ * TasksDropDown theme provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+trait TasksDropDownThemeProviderTrait {
+
+    /**
+     * Tasks drop down theme provider.
+     *
+     * @var TasksDropDownThemeProviderInterface
+     */
+    private $tasksDropDownThemeProvider;
+
+    /**
+     * Get the tasks drop down theme provider.
+     *
+     * @return TasksDropDownThemeProviderInterface Returns the tasks drop down theme provider.
+     */
+    public function getTasksDropDownThemeProvider() {
+        return $this->tasksDropDownThemeProvider;
+    }
+
+    /**
+     * Set the tasks drop down theme provider.
+     *
+     * @param TasksDropDownThemeProviderInterface $tasksDropDownThemeProvider The tasks drop down theme provider.
+     */
+    protected function setTasksDropDownThemeProvider(TasksDropDownThemeProviderInterface $tasksDropDownThemeProvider = null) {
+        $this->tasksDropDownThemeProvider = $tasksDropDownThemeProvider;
+        return $this;
+    }
+
+}
---
+++ Tests/Provider/Theme/TasksDropDownThemeProviderTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\TasksDropDownThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Provider\Theme\TestTasksDropDownThemeProviderTrait;
+
+/**
+ * Tasks drop down theme provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class TasksDropDownThemeProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestTasksDropDownThemeProviderTrait();
+
+        $this->assertNull($obj->getTasksDropDownThemeProvider());
+    }
+
+    /**
+     * Tests the setTasksDropDownThemeProvider() method.
+     *
+     * @return void
+     */
+    public function testSetTasksDropDownThemeProvider() {
+
+        // Set an Tasks drop down theme provider mock.
+        $tasksDropDownThemeProvider = $this->getMockBuilder(TasksDropDownThemeProviderInterface::class)->getMock();
+
+        $obj = new TestTasksDropDownThemeProviderTrait();
+
+        $obj->setTasksDropDownThemeProvider($tasksDropDownThemeProvider);
+        $this->assertSame($tasksDropDownThemeProvider, $obj->getTasksDropDownThemeProvider());
+    }
+
+}
