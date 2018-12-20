043fedc34bc4fb7403cbbe718059d79493ec850b
webeweb <support@webeweb.fr>
2018-12-20 16:45:17 +0100

    Add user info theme provider trait

---
+++ Provider/Theme/UserInfoThemeProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+/**
+ * UserInfo theme provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+trait UserInfoThemeProviderTrait {
+
+    /**
+     * User info theme provider.
+     *
+     * @var UserInfoThemeProviderInterface
+     */
+    private $userInfoThemeProvider;
+
+    /**
+     * Get the user info theme provider.
+     *
+     * @return UserInfoThemeProviderInterface Returns the user info theme provider.
+     */
+    public function getUserInfoThemeProvider() {
+        return $this->userInfoThemeProvider;
+    }
+
+    /**
+     * Set the user info theme provider.
+     *
+     * @param UserInfoThemeProviderInterface $userInfoThemeProvider The user info theme provider.
+     */
+    protected function setUserInfoThemeProvider(UserInfoThemeProviderInterface $userInfoThemeProvider = null) {
+        $this->userInfoThemeProvider = $userInfoThemeProvider;
+        return $this;
+    }
+
+}
---
+++ Tests/Provider/Theme/UserInfoThemeProviderTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\UserInfoThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Provider\Theme\TestUserInfoThemeProviderTrait;
+
+/**
+ * User info theme provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class UserInfoThemeProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestUserInfoThemeProviderTrait();
+
+        $this->assertNull($obj->getUserInfoThemeProvider());
+    }
+
+    /**
+     * Tests the setUserInfoThemeProvider() method.
+     *
+     * @return void
+     */
+    public function testSetUserInfoThemeProvider() {
+
+        // Set an User info theme provider mock.
+        $userInfoThemeProvider = $this->getMockBuilder(UserInfoThemeProviderInterface::class)->getMock();
+
+        $obj = new TestUserInfoThemeProviderTrait();
+
+        $obj->setUserInfoThemeProvider($userInfoThemeProvider);
+        $this->assertSame($userInfoThemeProvider, $obj->getUserInfoThemeProvider());
+    }
+
+}
