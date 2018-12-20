e6bb4196b1eb734bc02ed0ae141656f8d84234e5
webeweb <support@webeweb.fr>
2018-12-20 16:44:09 +0100

    Add hook drop down theme provider trait

---
+++ Provider/Theme/HookDropDownThemeProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+/**
+ * HookDropDown theme provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+trait HookDropDownThemeProviderTrait {
+
+    /**
+     * Hook drop down theme provider.
+     *
+     * @var HookDropDownThemeProviderInterface
+     */
+    private $hookDropDownThemeProvider;
+
+    /**
+     * Get the hook drop down theme provider.
+     *
+     * @return HookDropDownThemeProviderInterface Returns the hook drop down theme provider.
+     */
+    public function getHookDropDownThemeProvider() {
+        return $this->hookDropDownThemeProvider;
+    }
+
+    /**
+     * Set the hook drop down theme provider.
+     *
+     * @param HookDropDownThemeProviderInterface $hookDropDownThemeProvider The hook drop down theme provider.
+     */
+    protected function setHookDropDownThemeProvider(HookDropDownThemeProviderInterface $hookDropDownThemeProvider = null) {
+        $this->hookDropDownThemeProvider = $hookDropDownThemeProvider;
+        return $this;
+    }
+
+}
---
+++ Tests/Provider/Theme/HookDropDownThemeProviderTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\HookDropDownThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Provider\Theme\TestHookDropDownThemeProviderTrait;
+
+/**
+ * Hook drop down theme provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class HookDropDownThemeProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestHookDropDownThemeProviderTrait();
+
+        $this->assertNull($obj->getHookDropDownThemeProvider());
+    }
+
+    /**
+     * Tests the setHookDropDownThemeProvider() method.
+     *
+     * @return void
+     */
+    public function testSetHookDropDownThemeProvider() {
+
+        // Set an Hook drop down theme provider mock.
+        $hookDropDownThemeProvider = $this->getMockBuilder(HookDropDownThemeProviderInterface::class)->getMock();
+
+        $obj = new TestHookDropDownThemeProviderTrait();
+
+        $obj->setHookDropDownThemeProvider($hookDropDownThemeProvider);
+        $this->assertSame($hookDropDownThemeProvider, $obj->getHookDropDownThemeProvider());
+    }
+
+}
