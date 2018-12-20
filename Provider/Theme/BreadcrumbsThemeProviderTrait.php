bc1d423e522aa1528a4eae5d844143e43fb568c9
webeweb <support@webeweb.fr>
2018-12-20 16:43:44 +0100

    Add breadcrumbs theme provider trait

---
+++ Provider/Theme/BreadcrumbsThemeProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider\Theme;
+
+/**
+ * Breadcrumbs theme provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Provider\Theme
+ */
+trait BreadcrumbsThemeProviderTrait {
+
+    /**
+     * Breadcrumbs theme provider.
+     *
+     * @var BreadcrumbsThemeProviderInterface
+     */
+    private $breadcrumbsThemeProvider;
+
+    /**
+     * Get the breadcrumbs theme provider.
+     *
+     * @return BreadcrumbsThemeProviderInterface Returns the breadcrumbs theme provider.
+     */
+    public function getBreadcrumbsThemeProvider() {
+        return $this->breadcrumbsThemeProvider;
+    }
+
+    /**
+     * Set the breadcrumbs theme provider.
+     *
+     * @param BreadcrumbsThemeProviderInterface $breadcrumbsThemeProvider The breadcrumbs theme provider.
+     */
+    protected function setBreadcrumbsThemeProvider(BreadcrumbsThemeProviderInterface $breadcrumbsThemeProvider = null) {
+        $this->breadcrumbsThemeProvider = $breadcrumbsThemeProvider;
+        return $this;
+    }
+
+}
---
+++ Tests/Provider/Theme/BreadcrumbsThemeProviderTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Provider\Theme;
+
+use WBW\Bundle\CoreBundle\Provider\Theme\BreadcrumbsThemeProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Provider\Theme\TestBreadcrumbsThemeProviderTrait;
+
+/**
+ * Breadcrumbs theme provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Provider\Theme
+ */
+class BreadcrumbsThemeProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestBreadcrumbsThemeProviderTrait();
+
+        $this->assertNull($obj->getBreadcrumbsThemeProvider());
+    }
+
+    /**
+     * Tests the setBreadcrumbsThemeProvider() method.
+     *
+     * @return void
+     */
+    public function testSetBreadcrumbsThemeProvider() {
+
+        // Set an Breadcrumbs theme provider mock.
+        $breadcrumbsThemeProvider = $this->getMockBuilder(BreadcrumbsThemeProviderInterface::class)->getMock();
+
+        $obj = new TestBreadcrumbsThemeProviderTrait();
+
+        $obj->setBreadcrumbsThemeProvider($breadcrumbsThemeProvider);
+        $this->assertSame($breadcrumbsThemeProvider, $obj->getBreadcrumbsThemeProvider());
+    }
+
+}
