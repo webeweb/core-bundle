210dc4f0a2944fcb0450c92f1aee5b1743a13ee0
webeweb <support@webeweb.fr>
2018-12-31 11:52:27 +0100

    Update PHPDoc

--- Color/LightBlueColorInterface.php
+++ Color/LightBlueColorInterface.php
@@ -21,71 +21,100 @@ interface LightBlueColorInterface {
 
     /**
      * Color light blue "50"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_50 = "#E1F5FE";
 
     /**
      * Color light blue "100"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_100 = "#B3E5FC";
 
     /**
      * Color light blue "200"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_200 = "#81D4FA";
 
     /**
      * Color light blue "300"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_300 = "#4FC3F7";
 
     /**
      * Color light blue "400"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_400 = "#29B6F6";
 
     /**
      * Color light blue "500"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_500 = "#03A9F4";
 
     /**
      * Color light blue "600"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_600 = "#039BE5";
 
     /**
      * Color light blue "700"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_700 = "#0288D1";
 
     /**
      * Color light blue "800"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_800 = "#0277BD";
 
     /**
      * Color light blue "900"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_900 = "#01579B";
 
     /**
      * Color light blue "A100"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_A100 = "#80D8FF";
 
     /**
      * Color light blue "A200"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_A200 = "#40C4FF";
 
     /**
      * Color light blue "A400"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_A400 = "#00B0FF";
 
     /**
      * Color light blue "A700"
+     *
+     * @var string
      */
     const COLOR_LIGHT_BLUE_A700 = "#0091EA";
+
 }
