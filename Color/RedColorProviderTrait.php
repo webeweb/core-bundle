c2b714f91a7d3ec003db5c97a83881d5ad02d372
webeweb <support@webeweb.fr>
2018-12-31 15:24:49 +0100

    Add Red color provider trait

---
+++ Color/RedColorProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Red color provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+trait RedColorProviderTrait {
+
+    /**
+     * Red color provider.
+     *
+     * @var RedColorProvider
+     */
+    private $redColorProvider;
+
+    /**
+     * Get the red color provider.
+     *
+     * @return RedColorProvider Returns the red color provider.
+     */
+    public function getRedColorProvider() {
+        return $this->redColorProvider;
+    }
+
+    /**
+     * Set the red color provider.
+     *
+     * @param RedColorProvider $redColorProvider The red color provider.
+     */
+    protected function setRedColorProvider(RedColorProvider $redColorProvider = null) {
+        $this->redColorProvider = $redColorProvider;
+        return $this;
+    }
+
+}
