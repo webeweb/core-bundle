014c24b534e6be8c9085d06ce6253c7235e69f66
webeweb <support@webeweb.fr>
2018-12-31 13:31:02 +0100

    Update Amber color interface inheritance

--- Color/AmberColorInterface.php
+++ Color/AmberColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface AmberColorInterface {
+interface AmberColorInterface extends ColorInterface {
 
     /**
      * Color amber "50"
