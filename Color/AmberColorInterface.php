5b1a6948114dc1d96ab82c8f30d224fce7102cd5
webeweb <support@webeweb.fr>
2018-12-31 10:39:56 +0100

    Add Amber color interface

---
+++ Color/AmberColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Amber color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface AmberColorInterface {
+
+    /**
+     * Color amber "50"
+     */
+    const COLOR_AMBER_50 = "#FFF8E1";
+
+    /**
+     * Color amber "100"
+     */
+    const COLOR_AMBER_100 = "#FFECB3";
+
+    /**
+     * Color amber "200"
+     */
+    const COLOR_AMBER_200 = "#FFE082";
+
+    /**
+     * Color amber "300"
+     */
+    const COLOR_AMBER_300 = "#FFD54F";
+
+    /**
+     * Color amber "400"
+     */
+    const COLOR_AMBER_400 = "#FFCA28";
+
+    /**
+     * Color amber "500"
+     */
+    const COLOR_AMBER_500 = "#FFC107";
+
+    /**
+     * Color amber "600"
+     */
+    const COLOR_AMBER_600 = "#FFB300";
+
+    /**
+     * Color amber "700"
+     */
+    const COLOR_AMBER_700 = "#FFA000";
+
+    /**
+     * Color amber "800"
+     */
+    const COLOR_AMBER_800 = "#FF8F00";
+
+    /**
+     * Color amber "900"
+     */
+    const COLOR_AMBER_900 = "#FF6F00";
+
+    /**
+     * Color amber "A100"
+     */
+    const COLOR_AMBER_A100 = "#FFE57F";
+
+    /**
+     * Color amber "A200"
+     */
+    const COLOR_AMBER_A200 = "#FFD740";
+
+    /**
+     * Color amber "A400"
+     */
+    const COLOR_AMBER_A400 = "#FFC400";
+
+    /**
+     * Color amber "A700"
+     */
+    const COLOR_AMBER_A700 = "#FFAB00";
+}
