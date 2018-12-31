da1bd97844b3265d4c81499fb9e56e602a007520
webeweb <support@webeweb.fr>
2018-12-31 10:48:20 +0100

    Add Yellow color interface

---
+++ Color/YellowColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Yellow color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface YellowColorInterface {
+
+    /**
+     * Color yellow "50"
+     */
+    const COLOR_YELLOW_50 = "#FFFDE7";
+
+    /**
+     * Color yellow "100"
+     */
+    const COLOR_YELLOW_100 = "#FFF9C4";
+
+    /**
+     * Color yellow "200"
+     */
+    const COLOR_YELLOW_200 = "#FFF59D";
+
+    /**
+     * Color yellow "300"
+     */
+    const COLOR_YELLOW_300 = "#FFF176";
+
+    /**
+     * Color yellow "400"
+     */
+    const COLOR_YELLOW_400 = "#FFEE58";
+
+    /**
+     * Color yellow "500"
+     */
+    const COLOR_YELLOW_500 = "#FFEB3B";
+
+    /**
+     * Color yellow "600"
+     */
+    const COLOR_YELLOW_600 = "#FDD835";
+
+    /**
+     * Color yellow "700"
+     */
+    const COLOR_YELLOW_700 = "#FBC02D";
+
+    /**
+     * Color yellow "800"
+     */
+    const COLOR_YELLOW_800 = "#F9A825";
+
+    /**
+     * Color yellow "900"
+     */
+    const COLOR_YELLOW_900 = "#F57F17";
+
+    /**
+     * Color yellow "A100"
+     */
+    const COLOR_YELLOW_A100 = "#FFFF8D";
+
+    /**
+     * Color yellow "A200"
+     */
+    const COLOR_YELLOW_A200 = "#FFFF00";
+
+    /**
+     * Color yellow "A400"
+     */
+    const COLOR_YELLOW_A400 = "#FFEA00";
+
+    /**
+     * Color yellow "A700"
+     */
+    const COLOR_YELLOW_A700 = "#FFD600";
+}
