daa36d7f1947a8c2811cb59eaa189258f74334bc
webeweb <support@webeweb.fr>
2018-12-31 11:52:39 +0100

    Update PHPDoc

--- Color/LimeColorInterface.php
+++ Color/LimeColorInterface.php
@@ -21,71 +21,100 @@ interface LimeColorInterface {
 
     /**
      * Color lime "50"
+     *
+     * @var string
      */
     const COLOR_LIME_50 = "#F9FBE7";
 
     /**
      * Color lime "100"
+     *
+     * @var string
      */
     const COLOR_LIME_100 = "#F0F4C3";
 
     /**
      * Color lime "200"
+     *
+     * @var string
      */
     const COLOR_LIME_200 = "#E6EE9C";
 
     /**
      * Color lime "300"
+     *
+     * @var string
      */
     const COLOR_LIME_300 = "#DCE775";
 
     /**
      * Color lime "400"
+     *
+     * @var string
      */
     const COLOR_LIME_400 = "#D4E157";
 
     /**
      * Color lime "500"
+     *
+     * @var string
      */
     const COLOR_LIME_500 = "#CDDC39";
 
     /**
      * Color lime "600"
+     *
+     * @var string
      */
     const COLOR_LIME_600 = "#C0CA33";
 
     /**
      * Color lime "700"
+     *
+     * @var string
      */
     const COLOR_LIME_700 = "#AFB42B";
 
     /**
      * Color lime "800"
+     *
+     * @var string
      */
     const COLOR_LIME_800 = "#9E9D24";
 
     /**
      * Color lime "900"
+     *
+     * @var string
      */
     const COLOR_LIME_900 = "#827717";
 
     /**
      * Color lime "A100"
+     *
+     * @var string
      */
     const COLOR_LIME_A100 = "#F4FF81";
 
     /**
      * Color lime "A200"
+     *
+     * @var string
      */
     const COLOR_LIME_A200 = "#EEFF41";
 
     /**
      * Color lime "A400"
+     *
+     * @var string
      */
     const COLOR_LIME_A400 = "#C6FF00";
 
     /**
      * Color lime "A700"
+     *
+     * @var string
      */
     const COLOR_LIME_A700 = "#AEEA00";
+
 }
