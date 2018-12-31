b36ce75826366d2bb5ccd7a391bb88fc3d113aa4
webeweb <support@webeweb.fr>
2018-12-31 13:33:13 +0100

    Update Purple color interface inheritance

--- Color/PurpleColorInterface.php
+++ Color/PurpleColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface PurpleColorInterface {
+interface PurpleColorInterface extends ColorInterface {
 
     /**
      * Color purple "50"
