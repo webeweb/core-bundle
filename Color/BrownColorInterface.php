d7e2e40f15a6046e4ad7b7efc14f477ecec00ed2
webeweb <support@webeweb.fr>
2018-12-31 11:51:50 +0100

    Update PHPDoc

--- Color/BrownColorInterface.php
+++ Color/BrownColorInterface.php
@@ -21,51 +21,72 @@ interface BrownColorInterface {
 
     /**
      * Color brown "50"
+     *
+     * @var string
      */
     const COLOR_BROWN_50 = "#EFEBE9";
 
     /**
      * Color brown "100"
+     *
+     * @var string
      */
     const COLOR_BROWN_100 = "#D7CCC8";
 
     /**
      * Color brown "200"
+     *
+     * @var string
      */
     const COLOR_BROWN_200 = "#BCAAA4";
 
     /**
      * Color brown "300"
+     *
+     * @var string
      */
     const COLOR_BROWN_300 = "#A1887F";
 
     /**
      * Color brown "400"
+     *
+     * @var string
      */
     const COLOR_BROWN_400 = "#8D6E63";
 
     /**
      * Color brown "500"
+     *
+     * @var string
      */
     const COLOR_BROWN_500 = "#795548";
 
     /**
      * Color brown "600"
+     *
+     * @var string
      */
     const COLOR_BROWN_600 = "#6D4C41";
 
     /**
      * Color brown "700"
+     *
+     * @var string
      */
     const COLOR_BROWN_700 = "#5D4037";
 
     /**
      * Color brown "800"
+     *
+     * @var string
      */
     const COLOR_BROWN_800 = "#4E342E";
 
     /**
      * Color brown "900"
+     *
+     * @var string
      */
     const COLOR_BROWN_900 = "#3E2723";
+
 }
