0f59ccf04fd9858c568c3f80e2420526c226c7dc
webeweb <support@webeweb.fr>
2018-12-31 13:32:39 +0100

    Update Light green color interface inheritance

--- Color/LightGreenColorInterface.php
+++ Color/LightGreenColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface LightGreenColorInterface {
+interface LightGreenColorInterface extends ColorInterface {
 
     /**
      * Color light green "50"
