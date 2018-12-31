7c610b103f0fab70c3deecd594197bc8fbe99284
webeweb <support@webeweb.fr>
2018-12-31 10:46:15 +0100

    Add Green color interface

---
+++ Color/GreenColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Green color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface GreenColorInterface {
+
+    /**
+     * Color green "50"
+     */
+    const COLOR_GREEN_50 = "#E8F5E9";
+
+    /**
+     * Color green "100"
+     */
+    const COLOR_GREEN_100 = "#C8E6C9";
+
+    /**
+     * Color green "200"
+     */
+    const COLOR_GREEN_200 = "#A5D6A7";
+
+    /**
+     * Color green "300"
+     */
+    const COLOR_GREEN_300 = "#81C784";
+
+    /**
+     * Color green "400"
+     */
+    const COLOR_GREEN_400 = "#66BB6A";
+
+    /**
+     * Color green "500"
+     */
+    const COLOR_GREEN_500 = "#4CAF50";
+
+    /**
+     * Color green "600"
+     */
+    const COLOR_GREEN_600 = "#43A047";
+
+    /**
+     * Color green "700"
+     */
+    const COLOR_GREEN_700 = "#388E3C";
+
+    /**
+     * Color green "800"
+     */
+    const COLOR_GREEN_800 = "#2E7D32";
+
+    /**
+     * Color green "900"
+     */
+    const COLOR_GREEN_900 = "#1B5E20";
+
+    /**
+     * Color green "A100"
+     */
+    const COLOR_GREEN_A100 = "#B9F6CA";
+
+    /**
+     * Color green "A200"
+     */
+    const COLOR_GREEN_A200 = "#69F0AE";
+
+    /**
+     * Color green "A400"
+     */
+    const COLOR_GREEN_A400 = "#00E676";
+
+    /**
+     * Color green "A700"
+     */
+    const COLOR_GREEN_A700 = "#00C853";
+}
