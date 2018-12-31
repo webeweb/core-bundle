68b43fa9ecc2b521bbdbe1401df3b37f459c44e3
webeweb <support@webeweb.fr>
2018-12-31 10:47:05 +0100

    Add Light blue color interface

---
+++ Color/LightGreenColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Light green color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface LightGreenColorInterface {
+
+    /**
+     * Color light green "50"
+     */
+    const COLOR_LIGHT_GREEN_50 = "#F1F8E9";
+
+    /**
+     * Color light green "100"
+     */
+    const COLOR_LIGHT_GREEN_100 = "#DCEDC8";
+
+    /**
+     * Color light green "200"
+     */
+    const COLOR_LIGHT_GREEN_200 = "#C5E1A5";
+
+    /**
+     * Color light green "300"
+     */
+    const COLOR_LIGHT_GREEN_300 = "#AED581";
+
+    /**
+     * Color light green "400"
+     */
+    const COLOR_LIGHT_GREEN_400 = "#9CCC65";
+
+    /**
+     * Color light green "500"
+     */
+    const COLOR_LIGHT_GREEN_500 = "#8BC34A";
+
+    /**
+     * Color light green "600"
+     */
+    const COLOR_LIGHT_GREEN_600 = "#7CB342";
+
+    /**
+     * Color light green "700"
+     */
+    const COLOR_LIGHT_GREEN_700 = "#689F38";
+
+    /**
+     * Color light green "800"
+     */
+    const COLOR_LIGHT_GREEN_800 = "#558B2F";
+
+    /**
+     * Color light green "900"
+     */
+    const COLOR_LIGHT_GREEN_900 = "#33691E";
+
+    /**
+     * Color light green "A100"
+     */
+    const COLOR_LIGHT_GREEN_A100 = "#CCFF90";
+
+    /**
+     * Color light green "A200"
+     */
+    const COLOR_LIGHT_GREEN_A200 = "#B2FF59";
+
+    /**
+     * Color light green "A400"
+     */
+    const COLOR_LIGHT_GREEN_A400 = "#76FF03";
+
+    /**
+     * Color light green "A700"
+     */
+    const COLOR_LIGHT_GREEN_A700 = "#64DD17";
+}
