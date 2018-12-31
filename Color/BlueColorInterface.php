e49bd8454324d9dc42a788996d247287fd6602db
webeweb <support@webeweb.fr>
2018-12-31 10:40:08 +0100

    Add Blue color interface

---
+++ Color/BlueColorInterface.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+/**
+ * Blue color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface BlueColorInterface {
+
+    /**
+     * Color blue "50"
+     */
+    const COLOR_BLUE_50 = "#E3F2FD";
+
+    /**
+     * Color blue "100"
+     */
+    const COLOR_BLUE_100 = "#BBDEFB";
+
+    /**
+     * Color blue "200"
+     */
+    const COLOR_BLUE_200 = "#90CAF9";
+
+    /**
+     * Color blue "300"
+     */
+    const COLOR_BLUE_300 = "#64B5F6";
+
+    /**
+     * Color blue "400"
+     */
+    const COLOR_BLUE_400 = "#42A5F5";
+
+    /**
+     * Color blue "500"
+     */
+    const COLOR_BLUE_500 = "#2196F3";
+
+    /**
+     * Color blue "600"
+     */
+    const COLOR_BLUE_600 = "#1E88E5";
+
+    /**
+     * Color blue "700"
+     */
+    const COLOR_BLUE_700 = "#1976D2";
+
+    /**
+     * Color blue "800"
+     */
+    const COLOR_BLUE_800 = "#1565C0";
+
+    /**
+     * Color blue "900"
+     */
+    const COLOR_BLUE_900 = "#0D47A1";
+
+    /**
+     * Color blue "A100"
+     */
+    const COLOR_BLUE_A100 = "#82B1FF";
+
+    /**
+     * Color blue "A200"
+     */
+    const COLOR_BLUE_A200 = "#448AFF";
+
+    /**
+     * Color blue "A400"
+     */
+    const COLOR_BLUE_A400 = "#2979FF";
+
+    /**
+     * Color blue "A700"
+     */
+    const COLOR_BLUE_A700 = "#2962FF";
+}
---
+++ Tests/Color/BlueColorInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Color;
+
+use WBW\Bundle\CoreBundle\Color\BlueColorInterface;
+
+/**
+ * Blue color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Color
+ */
+class BlueColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("#E3F2FD", BlueColorInterface::COLOR_BLUE_50);
+        $this->assertEquals("#BBDEFB", BlueColorInterface::COLOR_BLUE_100);
+        $this->assertEquals("#90CAF9", BlueColorInterface::COLOR_BLUE_200);
+        $this->assertEquals("#64B5F6", BlueColorInterface::COLOR_BLUE_300);
+        $this->assertEquals("#42A5F5", BlueColorInterface::COLOR_BLUE_400);
+        $this->assertEquals("#2196F3", BlueColorInterface::COLOR_BLUE_500);
+        $this->assertEquals("#1E88E5", BlueColorInterface::COLOR_BLUE_600);
+        $this->assertEquals("#1976D2", BlueColorInterface::COLOR_BLUE_700);
+        $this->assertEquals("#1565C0", BlueColorInterface::COLOR_BLUE_800);
+        $this->assertEquals("#0D47A1", BlueColorInterface::COLOR_BLUE_900);
+        $this->assertEquals("#82B1FF", BlueColorInterface::COLOR_BLUE_A100);
+        $this->assertEquals("#448AFF", BlueColorInterface::COLOR_BLUE_A200);
+        $this->assertEquals("#2979FF", BlueColorInterface::COLOR_BLUE_A400);
+        $this->assertEquals("#2962FF", BlueColorInterface::COLOR_BLUE_A700);
+    }
+
+}
