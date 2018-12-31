cd4c5cae14acbc1f5342302c02f1371da130c07a
webeweb <support@webeweb.fr>
2018-12-31 11:51:55 +0100

    Update PHPDoc

--- Color/CyanColorInterface.php
+++ Color/CyanColorInterface.php
@@ -21,71 +21,100 @@ interface CyanColorInterface {
 
     /**
      * Color cyan "50"
+     *
+     * @var string
      */
     const COLOR_CYAN_50 = "#E0F7FA";
 
     /**
      * Color cyan "100"
+     *
+     * @var string
      */
     const COLOR_CYAN_100 = "#B2EBF2";
 
     /**
      * Color cyan "200"
+     *
+     * @var string
      */
     const COLOR_CYAN_200 = "#80DEEA";
 
     /**
      * Color cyan "300"
+     *
+     * @var string
      */
     const COLOR_CYAN_300 = "#4DD0E1";
 
     /**
      * Color cyan "400"
+     *
+     * @var string
      */
     const COLOR_CYAN_400 = "#26C6DA";
 
     /**
      * Color cyan "500"
+     *
+     * @var string
      */
     const COLOR_CYAN_500 = "#00BCD4";
 
     /**
      * Color cyan "600"
+     *
+     * @var string
      */
     const COLOR_CYAN_600 = "#00ACC1";
 
     /**
      * Color cyan "700"
+     *
+     * @var string
      */
     const COLOR_CYAN_700 = "#0097A7";
 
     /**
      * Color cyan "800"
+     *
+     * @var string
      */
     const COLOR_CYAN_800 = "#00838F";
 
     /**
      * Color cyan "900"
+     *
+     * @var string
      */
     const COLOR_CYAN_900 = "#006064";
 
     /**
      * Color cyan "A100"
+     *
+     * @var string
      */
     const COLOR_CYAN_A100 = "#84FFFF";
 
     /**
      * Color cyan "A200"
+     *
+     * @var string
      */
     const COLOR_CYAN_A200 = "#18FFFF";
 
     /**
      * Color cyan "A400"
+     *
+     * @var string
      */
     const COLOR_CYAN_A400 = "#00E5FF";
 
     /**
      * Color cyan "A700"
+     *
+     * @var string
      */
     const COLOR_CYAN_A700 = "#00B8D4";
+
 }
