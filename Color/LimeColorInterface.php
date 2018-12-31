9f389e74854140c7f3fea2b2b1352bfde9dc412e
webeweb <support@webeweb.fr>
2018-12-31 10:47:17 +0100

    Add Lime color interface

---
+++ Color/LimeColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Lime color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface LimeColorInterface {
+
+    /**
+     * Color lime "50"
+     */
+    const COLOR_LIME_50 = "#F9FBE7";
+
+    /**
+     * Color lime "100"
+     */
+    const COLOR_LIME_100 = "#F0F4C3";
+
+    /**
+     * Color lime "200"
+     */
+    const COLOR_LIME_200 = "#E6EE9C";
+
+    /**
+     * Color lime "300"
+     */
+    const COLOR_LIME_300 = "#DCE775";
+
+    /**
+     * Color lime "400"
+     */
+    const COLOR_LIME_400 = "#D4E157";
+
+    /**
+     * Color lime "500"
+     */
+    const COLOR_LIME_500 = "#CDDC39";
+
+    /**
+     * Color lime "600"
+     */
+    const COLOR_LIME_600 = "#C0CA33";
+
+    /**
+     * Color lime "700"
+     */
+    const COLOR_LIME_700 = "#AFB42B";
+
+    /**
+     * Color lime "800"
+     */
+    const COLOR_LIME_800 = "#9E9D24";
+
+    /**
+     * Color lime "900"
+     */
+    const COLOR_LIME_900 = "#827717";
+
+    /**
+     * Color lime "A100"
+     */
+    const COLOR_LIME_A100 = "#F4FF81";
+
+    /**
+     * Color lime "A200"
+     */
+    const COLOR_LIME_A200 = "#EEFF41";
+
+    /**
+     * Color lime "A400"
+     */
+    const COLOR_LIME_A400 = "#C6FF00";
+
+    /**
+     * Color lime "A700"
+     */
+    const COLOR_LIME_A700 = "#AEEA00";
+}
