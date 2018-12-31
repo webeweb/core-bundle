63e977fef07cba37e8c1322669eac6035891617a
webeweb <support@webeweb.fr>
2018-12-31 10:46:51 +0100

    Add Light blue color interface

---
+++ Color/LightBlueColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Light blue color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface LightBlueColorInterface {
+
+    /**
+     * Color light blue "50"
+     */
+    const COLOR_LIGHT_BLUE_50 = "#E1F5FE";
+
+    /**
+     * Color light blue "100"
+     */
+    const COLOR_LIGHT_BLUE_100 = "#B3E5FC";
+
+    /**
+     * Color light blue "200"
+     */
+    const COLOR_LIGHT_BLUE_200 = "#81D4FA";
+
+    /**
+     * Color light blue "300"
+     */
+    const COLOR_LIGHT_BLUE_300 = "#4FC3F7";
+
+    /**
+     * Color light blue "400"
+     */
+    const COLOR_LIGHT_BLUE_400 = "#29B6F6";
+
+    /**
+     * Color light blue "500"
+     */
+    const COLOR_LIGHT_BLUE_500 = "#03A9F4";
+
+    /**
+     * Color light blue "600"
+     */
+    const COLOR_LIGHT_BLUE_600 = "#039BE5";
+
+    /**
+     * Color light blue "700"
+     */
+    const COLOR_LIGHT_BLUE_700 = "#0288D1";
+
+    /**
+     * Color light blue "800"
+     */
+    const COLOR_LIGHT_BLUE_800 = "#0277BD";
+
+    /**
+     * Color light blue "900"
+     */
+    const COLOR_LIGHT_BLUE_900 = "#01579B";
+
+    /**
+     * Color light blue "A100"
+     */
+    const COLOR_LIGHT_BLUE_A100 = "#80D8FF";
+
+    /**
+     * Color light blue "A200"
+     */
+    const COLOR_LIGHT_BLUE_A200 = "#40C4FF";
+
+    /**
+     * Color light blue "A400"
+     */
+    const COLOR_LIGHT_BLUE_A400 = "#00B0FF";
+
+    /**
+     * Color light blue "A700"
+     */
+    const COLOR_LIGHT_BLUE_A700 = "#0091EA";
+}
