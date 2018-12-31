7c8d06a0e6d997d67bfb7597469b8c695de3aabd
webeweb <support@webeweb.fr>
2018-12-31 11:52:11 +0100

    Update PHPDoc

--- Color/GreenColorInterface.php
+++ Color/GreenColorInterface.php
@@ -21,71 +21,100 @@ interface GreenColorInterface {
 
     /**
      * Color green "50"
+     *
+     * @var string
      */
     const COLOR_GREEN_50 = "#E8F5E9";
 
     /**
      * Color green "100"
+     *
+     * @var string
      */
     const COLOR_GREEN_100 = "#C8E6C9";
 
     /**
      * Color green "200"
+     *
+     * @var string
      */
     const COLOR_GREEN_200 = "#A5D6A7";
 
     /**
      * Color green "300"
+     *
+     * @var string
      */
     const COLOR_GREEN_300 = "#81C784";
 
     /**
      * Color green "400"
+     *
+     * @var string
      */
     const COLOR_GREEN_400 = "#66BB6A";
 
     /**
      * Color green "500"
+     *
+     * @var string
      */
     const COLOR_GREEN_500 = "#4CAF50";
 
     /**
      * Color green "600"
+     *
+     * @var string
      */
     const COLOR_GREEN_600 = "#43A047";
 
     /**
      * Color green "700"
+     *
+     * @var string
      */
     const COLOR_GREEN_700 = "#388E3C";
 
     /**
      * Color green "800"
+     *
+     * @var string
      */
     const COLOR_GREEN_800 = "#2E7D32";
 
     /**
      * Color green "900"
+     *
+     * @var string
      */
     const COLOR_GREEN_900 = "#1B5E20";
 
     /**
      * Color green "A100"
+     *
+     * @var string
      */
     const COLOR_GREEN_A100 = "#B9F6CA";
 
     /**
      * Color green "A200"
+     *
+     * @var string
      */
     const COLOR_GREEN_A200 = "#69F0AE";
 
     /**
      * Color green "A400"
+     *
+     * @var string
      */
     const COLOR_GREEN_A400 = "#00E676";
 
     /**
      * Color green "A700"
+     *
+     * @var string
      */
     const COLOR_GREEN_A700 = "#00C853";
+
 }
