dfd7dd8608366a8058303ebd9b83affd4f7aca71
webeweb <support@webeweb.fr>
2018-12-31 11:52:32 +0100

    Update PHPDoc

--- Color/LightGreenColorInterface.php
+++ Color/LightGreenColorInterface.php
@@ -21,71 +21,100 @@ interface LightGreenColorInterface {
 
     /**
      * Color light green "50"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_50 = "#F1F8E9";
 
     /**
      * Color light green "100"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_100 = "#DCEDC8";
 
     /**
      * Color light green "200"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_200 = "#C5E1A5";
 
     /**
      * Color light green "300"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_300 = "#AED581";
 
     /**
      * Color light green "400"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_400 = "#9CCC65";
 
     /**
      * Color light green "500"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_500 = "#8BC34A";
 
     /**
      * Color light green "600"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_600 = "#7CB342";
 
     /**
      * Color light green "700"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_700 = "#689F38";
 
     /**
      * Color light green "800"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_800 = "#558B2F";
 
     /**
      * Color light green "900"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_900 = "#33691E";
 
     /**
      * Color light green "A100"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_A100 = "#CCFF90";
 
     /**
      * Color light green "A200"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_A200 = "#B2FF59";
 
     /**
      * Color light green "A400"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_A400 = "#76FF03";
 
     /**
      * Color light green "A700"
+     *
+     * @var string
      */
     const COLOR_LIGHT_GREEN_A700 = "#64DD17";
+
 }
