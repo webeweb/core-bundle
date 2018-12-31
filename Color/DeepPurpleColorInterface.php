6934cae8f00f741df5bfe5f8b3a2e74b5d9bd213
webeweb <support@webeweb.fr>
2018-12-31 10:45:47 +0100

    Add Deep purple color interface

---
+++ Color/DeepPurpleColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Deep purple color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface DeepPurpleColorInterface {
+
+    /**
+     * Color deep purple" 50"
+     */
+    const COLOR_DEEP_PURPLE_50 = "#EDE7F6";
+
+    /**
+     * Color deep purple" 100"
+     */
+    const COLOR_DEEP_PURPLE_100 = "#D1C4E9";
+
+    /**
+     * Color deep purple" 200"
+     */
+    const COLOR_DEEP_PURPLE_200 = "#B39DDB";
+
+    /**
+     * Color deep purple" 300"
+     */
+    const COLOR_DEEP_PURPLE_300 = "#9575CD";
+
+    /**
+     * Color deep purple" 400"
+     */
+    const COLOR_DEEP_PURPLE_400 = "#7E57C2";
+
+    /**
+     * Color deep purple" 500"
+     */
+    const COLOR_DEEP_PURPLE_500 = "#673AB7";
+
+    /**
+     * Color deep purple" 600"
+     */
+    const COLOR_DEEP_PURPLE_600 = "#5E35B1";
+
+    /**
+     * Color deep purple" 700"
+     */
+    const COLOR_DEEP_PURPLE_700 = "#512DA8";
+
+    /**
+     * Color deep purple" 800"
+     */
+    const COLOR_DEEP_PURPLE_800 = "#4527A0";
+
+    /**
+     * Color deep purple" 900"
+     */
+    const COLOR_DEEP_PURPLE_900 = "#311B92";
+
+    /**
+     * Color deep purple" A100"
+     */
+    const COLOR_DEEP_PURPLE_A100 = "#B388FF";
+
+    /**
+     * Color deep purple" A200"
+     */
+    const COLOR_DEEP_PURPLE_A200 = "#7C4DFF";
+
+    /**
+     * Color deep purple" A400"
+     */
+    const COLOR_DEEP_PURPLE_A400 = "#651FFF";
+
+    /**
+     * Color deep purple" A700"
+     */
+    const COLOR_DEEP_PURPLE_A700 = "#6200EA";
+}
