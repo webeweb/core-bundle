c954d5961cecfc0189516e746bb28dee2642299a
webeweb <support@webeweb.fr>
2018-12-31 11:53:01 +0100

    Update PHPDoc

--- Color/RedColorInterface.php
+++ Color/RedColorInterface.php
@@ -21,71 +21,100 @@ interface RedColorInterface {
 
     /**
      * Color red "50"
+     *
+     * @var string
      */
     const COLOR_RED_50 = "#FFEBEE";
 
     /**
      * Color red "100"
+     *
+     * @var string
      */
     const COLOR_RED_100 = "#FFCDD2";
 
     /**
      * Color red "200"
+     *
+     * @var string
      */
     const COLOR_RED_200 = "#EF9A9A";
 
     /**
      * Color red "300"
+     *
+     * @var string
      */
     const COLOR_RED_300 = "#E57373";
 
     /**
      * Color red "400"
+     *
+     * @var string
      */
     const COLOR_RED_400 = "#EF5350";
 
     /**
      * Color red "500"
+     *
+     * @var string
      */
     const COLOR_RED_500 = "#F44336";
 
     /**
      * Color red "600"
+     *
+     * @var string
      */
     const COLOR_RED_600 = "#E53935";
 
     /**
      * Color red "700"
+     *
+     * @var string
      */
     const COLOR_RED_700 = "#D32F2F";
 
     /**
      * Color red "800"
+     *
+     * @var string
      */
     const COLOR_RED_800 = "#C62828";
 
     /**
      * Color red "900"
+     *
+     * @var string
      */
     const COLOR_RED_900 = "#B71C1C";
 
     /**
      * Color red "A100"
+     *
+     * @var string
      */
     const COLOR_RED_A100 = "#FF8A80";
 
     /**
      * Color red "A200"
+     *
+     * @var string
      */
     const COLOR_RED_A200 = "#FF5252";
 
     /**
      * Color red "A400"
+     *
+     * @var string
      */
     const COLOR_RED_A400 = "#FF1744";
 
     /**
      * Color red "A700"
+     *
+     * @var string
      */
     const COLOR_RED_A700 = "#D50000";
+
 }
