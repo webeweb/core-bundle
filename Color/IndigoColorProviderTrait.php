05cc8a005ffa5c291a2662485cfd00b5a1a7dabb
webeweb <support@webeweb.fr>
2018-12-31 15:23:31 +0100

    Add Indigo color provider trait

---
+++ Color/IndigoColorProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Indigo color provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+trait IndigoColorProviderTrait {
+
+    /**
+     * Indigo color provider.
+     *
+     * @var IndigoColorProvider
+     */
+    private $indigoColorProvider;
+
+    /**
+     * Get the indigo color provider.
+     *
+     * @return IndigoColorProvider Returns the indigo color provider.
+     */
+    public function getIndigoColorProvider() {
+        return $this->indigoColorProvider;
+    }
+
+    /**
+     * Set the indigo color provider.
+     *
+     * @param IndigoColorProvider $indigoColorProvider The indigo color provider.
+     */
+    protected function setIndigoColorProvider(IndigoColorProvider $indigoColorProvider = null) {
+        $this->indigoColorProvider = $indigoColorProvider;
+        return $this;
+    }
+
+}
