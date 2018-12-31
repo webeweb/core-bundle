15a52ab6afc5e46529e8ed42d2dda896f1b0bdf4
webeweb <support@webeweb.fr>
2018-12-31 11:52:21 +0100

    Update PHPDoc

--- Color/IndigoColorInterface.php
+++ Color/IndigoColorInterface.php
@@ -21,71 +21,100 @@ interface IndigoColorInterface {
 
     /**
      * Color indigo "50"
+     *
+     * @var string
      */
     const COLOR_INDIGO_50 = "#E8EAF6";
 
     /**
      * Color indigo "100"
+     *
+     * @var string
      */
     const COLOR_INDIGO_100 = "#C5CAE9";
 
     /**
      * Color indigo "200"
+     *
+     * @var string
      */
     const COLOR_INDIGO_200 = "#9FA8DA";
 
     /**
      * Color indigo "300"
+     *
+     * @var string
      */
     const COLOR_INDIGO_300 = "#7986CB";
 
     /**
      * Color indigo "400"
+     *
+     * @var string
      */
     const COLOR_INDIGO_400 = "#5C6BC0";
 
     /**
      * Color indigo "500"
+     *
+     * @var string
      */
     const COLOR_INDIGO_500 = "#3F51B5";
 
     /**
      * Color indigo "600"
+     *
+     * @var string
      */
     const COLOR_INDIGO_600 = "#3949AB";
 
     /**
      * Color indigo "700"
+     *
+     * @var string
      */
     const COLOR_INDIGO_700 = "#303F9F";
 
     /**
      * Color indigo "800"
+     *
+     * @var string
      */
     const COLOR_INDIGO_800 = "#283593";
 
     /**
      * Color indigo "900"
+     *
+     * @var string
      */
     const COLOR_INDIGO_900 = "#1A237E";
 
     /**
      * Color indigo "A100"
+     *
+     * @var string
      */
     const COLOR_INDIGO_A100 = "#8C9EFF";
 
     /**
      * Color indigo "A200"
+     *
+     * @var string
      */
     const COLOR_INDIGO_A200 = "#536DFE";
 
     /**
      * Color indigo "A400"
+     *
+     * @var string
      */
     const COLOR_INDIGO_A400 = "#3D5AFE";
 
     /**
      * Color indigo "A700"
+     *
+     * @var string
      */
     const COLOR_INDIGO_A700 = "#304FFE";
+
 }
