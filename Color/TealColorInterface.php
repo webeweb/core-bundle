75d05f3ee0c4eb6b4e5dc964581364a13aa9f2af
webeweb <support@webeweb.fr>
2018-12-31 11:53:05 +0100

    Update PHPDoc

--- Color/TealColorInterface.php
+++ Color/TealColorInterface.php
@@ -21,71 +21,100 @@ interface TealColorInterface {
 
     /**
      * Color teal "50"
+     *
+     * @var string
      */
     const COLOR_TEAL_50 = "#E0F2F1";
 
     /**
      * Color teal "100"
+     *
+     * @var string
      */
     const COLOR_TEAL_100 = "#B2DFDB";
 
     /**
      * Color teal "200"
+     *
+     * @var string
      */
     const COLOR_TEAL_200 = "#80CBC4";
 
     /**
      * Color teal "300"
+     *
+     * @var string
      */
     const COLOR_TEAL_300 = "#4DB6AC";
 
     /**
      * Color teal "400"
+     *
+     * @var string
      */
     const COLOR_TEAL_400 = "#26A69A";
 
     /**
      * Color teal "500"
+     *
+     * @var string
      */
     const COLOR_TEAL_500 = "#009688";
 
     /**
      * Color teal "600"
+     *
+     * @var string
      */
     const COLOR_TEAL_600 = "#00897B";
 
     /**
      * Color teal "700"
+     *
+     * @var string
      */
     const COLOR_TEAL_700 = "#00796B";
 
     /**
      * Color teal "800"
+     *
+     * @var string
      */
     const COLOR_TEAL_800 = "#00695C";
 
     /**
      * Color teal "900"
+     *
+     * @var string
      */
     const COLOR_TEAL_900 = "#004D40";
 
     /**
      * Color teal "A100"
+     *
+     * @var string
      */
     const COLOR_TEAL_A100 = "#A7FFEB";
 
     /**
      * Color teal "A200"
+     *
+     * @var string
      */
     const COLOR_TEAL_A200 = "#64FFDA";
 
     /**
      * Color teal "A400"
+     *
+     * @var string
      */
     const COLOR_TEAL_A400 = "#1DE9B6";
 
     /**
      * Color teal "A700"
+     *
+     * @var string
      */
     const COLOR_TEAL_A700 = "#00BFA5";
+
 }
