f79d014dbebf4858119a68b6893322bbb031f5ac
webeweb <support@webeweb.fr>
2018-12-31 13:32:22 +0100

    Update Indigo color interface inheritance

--- Color/IndigoColorInterface.php
+++ Color/IndigoColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface IndigoColorInterface {
+interface IndigoColorInterface extends ColorInterface {
 
     /**
      * Color indigo "50"
