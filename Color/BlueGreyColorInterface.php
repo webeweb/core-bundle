87eecd06c12c19702a7718f8d351bc40df451c18
webeweb <support@webeweb.fr>
2018-12-31 10:40:20 +0100

    Add Blue grey color interface

---
+++ Color/BlueGreyColorInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Blue grey color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface BlueGreyColorInterface {
+
+    /**
+     * Color blue grey "50"
+     */
+    const COLOR_BLUE_GREY_50 = "#ECEFF1";
+
+    /**
+     * Color blue grey "100"
+     */
+    const COLOR_BLUE_GREY_100 = "#CFD8DC";
+
+    /**
+     * Color blue grey "200"
+     */
+    const COLOR_BLUE_GREY_200 = "#B0BEC5";
+
+    /**
+     * Color blue grey "300"
+     */
+    const COLOR_BLUE_GREY_300 = "#90A4AE";
+
+    /**
+     * Color blue grey "400"
+     */
+    const COLOR_BLUE_GREY_400 = "#78909C";
+
+    /**
+     * Color blue grey "500"
+     */
+    const COLOR_BLUE_GREY_500 = "#607D8B";
+
+    /**
+     * Color blue grey "600"
+     */
+    const COLOR_BLUE_GREY_600 = "#546E7A";
+
+    /**
+     * Color blue grey "700"
+     */
+    const COLOR_BLUE_GREY_700 = "#455A64";
+
+    /**
+     * Color blue grey "800"
+     */
+    const COLOR_BLUE_GREY_800 = "#37474F";
+
+    /**
+     * Color blue grey "900"
+     */
+    const COLOR_BLUE_GREY_900 = "#263238";
+}
