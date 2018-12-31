9b5c1001d40ad5165a8457dfb651b2dfbb066575
webeweb <support@webeweb.fr>
2018-12-31 13:33:19 +0100

    Update Red color interface inheritance

--- Color/RedColorInterface.php
+++ Color/RedColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface RedColorInterface {
+interface RedColorInterface extends ColorInterface {
 
     /**
      * Color red "50"
