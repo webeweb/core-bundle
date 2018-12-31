8e0ff82dac3c0bf8c15fbae19628d4345ed76b4d
webeweb <support@webeweb.fr>
2018-12-31 11:52:01 +0100

    Update PHPDoc

--- Color/DeepOrangeColorInterface.php
+++ Color/DeepOrangeColorInterface.php
@@ -21,71 +21,100 @@ interface DeepOrangeColorInterface {
 
     /**
      * Color deep orange "50"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_50 = "#FBE9E7";
 
     /**
      * Color deep orange "100"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_100 = "#FFCCBC";
 
     /**
      * Color deep orange "200"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_200 = "#FFAB91";
 
     /**
      * Color deep orange "300"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_300 = "#FF8A65";
 
     /**
      * Color deep orange "400"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_400 = "#FF7043";
 
     /**
      * Color deep orange "500"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_500 = "#FF5722";
 
     /**
      * Color deep orange "600"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_600 = "#F4511E";
 
     /**
      * Color deep orange "700"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_700 = "#E64A19";
 
     /**
      * Color deep orange "800"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_800 = "#D84315";
 
     /**
      * Color deep orange "900"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_900 = "#BF360C";
 
     /**
      * Color deep orange "A100"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_A100 = "#FF9E80";
 
     /**
      * Color deep orange "A200"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_A200 = "#FF6E40";
 
     /**
      * Color deep orange "A400"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_A400 = "#FF3D00";
 
     /**
      * Color deep orange "A700"
+     *
+     * @var string
      */
     const COLOR_DEEP_ORANGE_A700 = "#DD2C00";
+
 }
