e428688d81b9442bb08064253f0067458e55ec08
webeweb <support@webeweb.fr>
2018-12-31 11:51:33 +0100

    Update PHPDoc

--- Color/AmberColorInterface.php
+++ Color/AmberColorInterface.php
@@ -21,71 +21,100 @@ interface AmberColorInterface {
 
     /**
      * Color amber "50"
+     *
+     * @var string
      */
     const COLOR_AMBER_50 = "#FFF8E1";
 
     /**
      * Color amber "100"
+     *
+     * @var string
      */
     const COLOR_AMBER_100 = "#FFECB3";
 
     /**
      * Color amber "200"
+     *
+     * @var string
      */
     const COLOR_AMBER_200 = "#FFE082";
 
     /**
      * Color amber "300"
+     *
+     * @var string
      */
     const COLOR_AMBER_300 = "#FFD54F";
 
     /**
      * Color amber "400"
+     *
+     * @var string
      */
     const COLOR_AMBER_400 = "#FFCA28";
 
     /**
      * Color amber "500"
+     *
+     * @var string
      */
     const COLOR_AMBER_500 = "#FFC107";
 
     /**
      * Color amber "600"
+     *
+     * @var string
      */
     const COLOR_AMBER_600 = "#FFB300";
 
     /**
      * Color amber "700"
+     *
+     * @var string
      */
     const COLOR_AMBER_700 = "#FFA000";
 
     /**
      * Color amber "800"
+     *
+     * @var string
      */
     const COLOR_AMBER_800 = "#FF8F00";
 
     /**
      * Color amber "900"
+     *
+     * @var string
      */
     const COLOR_AMBER_900 = "#FF6F00";
 
     /**
      * Color amber "A100"
+     *
+     * @var string
      */
     const COLOR_AMBER_A100 = "#FFE57F";
 
     /**
      * Color amber "A200"
+     *
+     * @var string
      */
     const COLOR_AMBER_A200 = "#FFD740";
 
     /**
      * Color amber "A400"
+     *
+     * @var string
      */
     const COLOR_AMBER_A400 = "#FFC400";
 
     /**
      * Color amber "A700"
+     *
+     * @var string
      */
     const COLOR_AMBER_A700 = "#FFAB00";
+
 }
