0bf94077e3723b94754d3661de8d4b89466136d6
webeweb <support@webeweb.fr>
2018-12-31 11:51:39 +0100

    Update PHPDoc

--- Color/BlueColorInterface.php
+++ Color/BlueColorInterface.php
@@ -20,71 +20,100 @@ interface BlueColorInterface {
 
     /**
      * Color blue "50"
+     *
+     * @var string
      */
     const COLOR_BLUE_50 = "#E3F2FD";
 
     /**
      * Color blue "100"
+     *
+     * @var string
      */
     const COLOR_BLUE_100 = "#BBDEFB";
 
     /**
      * Color blue "200"
+     *
+     * @var string
      */
     const COLOR_BLUE_200 = "#90CAF9";
 
     /**
      * Color blue "300"
+     *
+     * @var string
      */
     const COLOR_BLUE_300 = "#64B5F6";
 
     /**
      * Color blue "400"
+     *
+     * @var string
      */
     const COLOR_BLUE_400 = "#42A5F5";
 
     /**
      * Color blue "500"
+     *
+     * @var string
      */
     const COLOR_BLUE_500 = "#2196F3";
 
     /**
      * Color blue "600"
+     *
+     * @var string
      */
     const COLOR_BLUE_600 = "#1E88E5";
 
     /**
      * Color blue "700"
+     *
+     * @var string
      */
     const COLOR_BLUE_700 = "#1976D2";
 
     /**
      * Color blue "800"
+     *
+     * @var string
      */
     const COLOR_BLUE_800 = "#1565C0";
 
     /**
      * Color blue "900"
+     *
+     * @var string
      */
     const COLOR_BLUE_900 = "#0D47A1";
 
     /**
      * Color blue "A100"
+     *
+     * @var string
      */
     const COLOR_BLUE_A100 = "#82B1FF";
 
     /**
      * Color blue "A200"
+     *
+     * @var string
      */
     const COLOR_BLUE_A200 = "#448AFF";
 
     /**
      * Color blue "A400"
+     *
+     * @var string
      */
     const COLOR_BLUE_A400 = "#2979FF";
 
     /**
      * Color blue "A700"
+     *
+     * @var string
      */
     const COLOR_BLUE_A700 = "#2962FF";
+
 }
