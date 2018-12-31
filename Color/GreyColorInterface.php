1e0f01eb10bc5a39cae9b456bfa8c25eea638dd8
webeweb <support@webeweb.fr>
2018-12-31 11:52:16 +0100

    Update PHPDoc

--- Color/GreyColorInterface.php
+++ Color/GreyColorInterface.php
@@ -21,51 +21,72 @@ interface GreyColorInterface {
 
     /**
      * Color grey "50"
+     *
+     * @var string
      */
     const COLOR_GREY_50 = "#FAFAFA";
 
     /**
      * Color grey "100"
+     *
+     * @var string
      */
     const COLOR_GREY_100 = "#F5F5F5";
 
     /**
      * Color grey "200"
+     *
+     * @var string
      */
     const COLOR_GREY_200 = "#EEEEEE";
 
     /**
      * Color grey "300"
+     *
+     * @var string
      */
     const COLOR_GREY_300 = "#E0E0E0";
 
     /**
      * Color grey "400"
+     *
+     * @var string
      */
     const COLOR_GREY_400 = "#BDBDBD";
 
     /**
      * Color grey "500"
+     *
+     * @var string
      */
     const COLOR_GREY_500 = "#9E9E9E";
 
     /**
      * Color grey "600"
+     *
+     * @var string
      */
     const COLOR_GREY_600 = "#757575";
 
     /**
      * Color grey "700"
+     *
+     * @var string
      */
     const COLOR_GREY_700 = "#616161";
 
     /**
      * Color grey "800"
+     *
+     * @var string
      */
     const COLOR_GREY_800 = "#424242";
 
     /**
      * Color grey "900"
+     *
+     * @var string
      */
     const COLOR_GREY_900 = "#212121";
+
 }
