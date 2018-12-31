43090deb03f5da135d06279c2eb18dce0678c9fe
webeweb <support@webeweb.fr>
2018-12-31 14:25:57 +0100

    Add Pink color provider interface

---
+++ Tests/Color/PinkColorProviderTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Color;
+
+use WBW\Bundle\CoreBundle\Color\ColorInterface;
+use WBW\Bundle\CoreBundle\Color\PinkColorInterface;
+use WBW\Bundle\CoreBundle\Color\PinkColorProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Pink color provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Color
+ */
+class PinkColorProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the getColors() method.
+     *
+     * @return void
+     */
+    public function testGetColors() {
+
+        $obj = new PinkColorProvider();
+
+        $res = $obj->getColors();
+        $this->assertCount(12, $res);
+
+        $this->assertArrayHasKey(ColorInterface::COLOR_50, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_100, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_200, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_300, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_400, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_500, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_600, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_700, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_A100, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_A200, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_A400, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_A700, $res);
+
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_50, $res[ColorInterface::COLOR_50]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_100, $res[ColorInterface::COLOR_100]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_200, $res[ColorInterface::COLOR_200]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_300, $res[ColorInterface::COLOR_300]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_400, $res[ColorInterface::COLOR_400]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_500, $res[ColorInterface::COLOR_500]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_500, $res[ColorInterface::COLOR_500]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_600, $res[ColorInterface::COLOR_600]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_700, $res[ColorInterface::COLOR_700]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_A100, $res[ColorInterface::COLOR_A100]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_A200, $res[ColorInterface::COLOR_A200]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_A400, $res[ColorInterface::COLOR_A400]);
+        $this->assertEquals(PinkColorInterface::COLOR_PINK_A700, $res[ColorInterface::COLOR_A700]);
+    }
+
+    /**
+     * Tests the getName() method.
+     *
+     * @return void
+     */
+    public function testGetName() {
+
+        $obj = new PinkColorProvider();
+
+        $res = ColorInterface::COLOR_PINK;
+        $this->assertEquals($res, $obj->getName());
+    }
+
+}
