c1eb3cc2b95a0f438a709a60155e2421167f404e
webeweb <support@webeweb.fr>
2018-12-31 11:53:11 +0100

    Update PHPDoc

--- Color/YellowColorInterface.php
+++ Color/YellowColorInterface.php
@@ -21,71 +21,100 @@ interface YellowColorInterface {
 
     /**
      * Color yellow "50"
+     *
+     * @var string
      */
     const COLOR_YELLOW_50 = "#FFFDE7";
 
     /**
      * Color yellow "100"
+     *
+     * @var string
      */
     const COLOR_YELLOW_100 = "#FFF9C4";
 
     /**
      * Color yellow "200"
+     *
+     * @var string
      */
     const COLOR_YELLOW_200 = "#FFF59D";
 
     /**
      * Color yellow "300"
+     *
+     * @var string
      */
     const COLOR_YELLOW_300 = "#FFF176";
 
     /**
      * Color yellow "400"
+     *
+     * @var string
      */
     const COLOR_YELLOW_400 = "#FFEE58";
 
     /**
      * Color yellow "500"
+     *
+     * @var string
      */
     const COLOR_YELLOW_500 = "#FFEB3B";
 
     /**
      * Color yellow "600"
+     *
+     * @var string
      */
     const COLOR_YELLOW_600 = "#FDD835";
 
     /**
      * Color yellow "700"
+     *
+     * @var string
      */
     const COLOR_YELLOW_700 = "#FBC02D";
 
     /**
      * Color yellow "800"
+     *
+     * @var string
      */
     const COLOR_YELLOW_800 = "#F9A825";
 
     /**
      * Color yellow "900"
+     *
+     * @var string
      */
     const COLOR_YELLOW_900 = "#F57F17";
 
     /**
      * Color yellow "A100"
+     *
+     * @var string
      */
     const COLOR_YELLOW_A100 = "#FFFF8D";
 
     /**
      * Color yellow "A200"
+     *
+     * @var string
      */
     const COLOR_YELLOW_A200 = "#FFFF00";
 
     /**
      * Color yellow "A400"
+     *
+     * @var string
      */
     const COLOR_YELLOW_A400 = "#FFEA00";
 
     /**
      * Color yellow "A700"
+     *
+     * @var string
      */
     const COLOR_YELLOW_A700 = "#FFD600";
+
 }
