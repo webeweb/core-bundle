1c2d0c81e7819eda9d90e74af1c70c719006d379
webeweb <support@webeweb.fr>
2018-12-31 10:46:39 +0100

    Add Indigo color interface

---
+++ Color/IndigoColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Indigo color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface IndigoColorInterface {
+
+    /**
+     * Color indigo "50"
+     */
+    const COLOR_INDIGO_50 = "#E8EAF6";
+
+    /**
+     * Color indigo "100"
+     */
+    const COLOR_INDIGO_100 = "#C5CAE9";
+
+    /**
+     * Color indigo "200"
+     */
+    const COLOR_INDIGO_200 = "#9FA8DA";
+
+    /**
+     * Color indigo "300"
+     */
+    const COLOR_INDIGO_300 = "#7986CB";
+
+    /**
+     * Color indigo "400"
+     */
+    const COLOR_INDIGO_400 = "#5C6BC0";
+
+    /**
+     * Color indigo "500"
+     */
+    const COLOR_INDIGO_500 = "#3F51B5";
+
+    /**
+     * Color indigo "600"
+     */
+    const COLOR_INDIGO_600 = "#3949AB";
+
+    /**
+     * Color indigo "700"
+     */
+    const COLOR_INDIGO_700 = "#303F9F";
+
+    /**
+     * Color indigo "800"
+     */
+    const COLOR_INDIGO_800 = "#283593";
+
+    /**
+     * Color indigo "900"
+     */
+    const COLOR_INDIGO_900 = "#1A237E";
+
+    /**
+     * Color indigo "A100"
+     */
+    const COLOR_INDIGO_A100 = "#8C9EFF";
+
+    /**
+     * Color indigo "A200"
+     */
+    const COLOR_INDIGO_A200 = "#536DFE";
+
+    /**
+     * Color indigo "A400"
+     */
+    const COLOR_INDIGO_A400 = "#3D5AFE";
+
+    /**
+     * Color indigo "A700"
+     */
+    const COLOR_INDIGO_A700 = "#304FFE";
+}
