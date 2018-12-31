c625110aeff3ceae763c5cd1d3759e31d060b42b
webeweb <support@webeweb.fr>
2018-12-31 13:33:26 +0100

    Update Teal color interface inheritance

--- Color/TealColorInterface.php
+++ Color/TealColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface TealColorInterface {
+interface TealColorInterface extends ColorInterface {
 
     /**
      * Color teal "50"
