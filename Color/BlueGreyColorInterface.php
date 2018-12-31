d4b0b7b194e5848b15f84c0bf2e20062481bbba5
webeweb <support@webeweb.fr>
2018-12-31 11:51:45 +0100

    Update PHPDoc

--- Color/BlueGreyColorInterface.php
+++ Color/BlueGreyColorInterface.php
@@ -21,51 +21,72 @@ interface BlueGreyColorInterface {
 
     /**
      * Color blue grey "50"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_50 = "#ECEFF1";
 
     /**
      * Color blue grey "100"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_100 = "#CFD8DC";
 
     /**
      * Color blue grey "200"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_200 = "#B0BEC5";
 
     /**
      * Color blue grey "300"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_300 = "#90A4AE";
 
     /**
      * Color blue grey "400"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_400 = "#78909C";
 
     /**
      * Color blue grey "500"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_500 = "#607D8B";
 
     /**
      * Color blue grey "600"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_600 = "#546E7A";
 
     /**
      * Color blue grey "700"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_700 = "#455A64";
 
     /**
      * Color blue grey "800"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_800 = "#37474F";
 
     /**
      * Color blue grey "900"
+     *
+     * @var string
      */
     const COLOR_BLUE_GREY_900 = "#263238";
+
 }
