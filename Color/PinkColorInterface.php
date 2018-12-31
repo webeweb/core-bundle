e9db451d6eb2b5ee810da2c4f56ce8044fd7bf73
webeweb <support@webeweb.fr>
2018-12-31 11:52:50 +0100

    Update PHPDoc

--- Color/PinkColorInterface.php
+++ Color/PinkColorInterface.php
@@ -21,71 +21,100 @@ interface PinkColorInterface {
 
     /**
      * Color pink "50"
+     *
+     * @var string
      */
     const COLOR_PINK_50 = "#FCE4EC";
 
     /**
      * Color pink "100"
+     *
+     * @var string
      */
     const COLOR_PINK_100 = "#F8BBD0";
 
     /**
      * Color pink "200"
+     *
+     * @var string
      */
     const COLOR_PINK_200 = "#F48FB1";
 
     /**
      * Color pink "300"
+     *
+     * @var string
      */
     const COLOR_PINK_300 = "#F06292";
 
     /**
      * Color pink "400"
+     *
+     * @var string
      */
     const COLOR_PINK_400 = "#EC407A";
 
     /**
      * Color pink "500"
+     *
+     * @var string
      */
     const COLOR_PINK_500 = "#E91E63";
 
     /**
      * Color pink "600"
+     *
+     * @var string
      */
     const COLOR_PINK_600 = "#D81B60";
 
     /**
      * Color pink "700"
+     *
+     * @var string
      */
     const COLOR_PINK_700 = "#C2185B";
 
     /**
      * Color pink "800"
+     *
+     * @var string
      */
     const COLOR_PINK_800 = "#AD1457";
 
     /**
      * Color pink "900"
+     *
+     * @var string
      */
     const COLOR_PINK_900 = "#880E4F";
 
     /**
      * Color pink "A100"
+     *
+     * @var string
      */
     const COLOR_PINK_A100 = "#FF80AB";
 
     /**
      * Color pink "A200"
+     *
+     * @var string
      */
     const COLOR_PINK_A200 = "#FF4081";
 
     /**
      * Color pink "A400"
+     *
+     * @var string
      */
     const COLOR_PINK_A400 = "#F50057";
 
     /**
      * Color pink "A700"
+     *
+     * @var string
      */
     const COLOR_PINK_A700 = "#C51162";
+
 }
