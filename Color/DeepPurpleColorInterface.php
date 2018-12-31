252f7a9dfe901b9b9153408c242faa056e644a0d
webeweb <support@webeweb.fr>
2018-12-31 11:52:06 +0100

    Update PHPDoc

--- Color/DeepPurpleColorInterface.php
+++ Color/DeepPurpleColorInterface.php
@@ -21,71 +21,100 @@ interface DeepPurpleColorInterface {
 
     /**
      * Color deep purple" 50"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_50 = "#EDE7F6";
 
     /**
      * Color deep purple" 100"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_100 = "#D1C4E9";
 
     /**
      * Color deep purple" 200"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_200 = "#B39DDB";
 
     /**
      * Color deep purple" 300"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_300 = "#9575CD";
 
     /**
      * Color deep purple" 400"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_400 = "#7E57C2";
 
     /**
      * Color deep purple" 500"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_500 = "#673AB7";
 
     /**
      * Color deep purple" 600"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_600 = "#5E35B1";
 
     /**
      * Color deep purple" 700"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_700 = "#512DA8";
 
     /**
      * Color deep purple" 800"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_800 = "#4527A0";
 
     /**
      * Color deep purple" 900"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_900 = "#311B92";
 
     /**
      * Color deep purple" A100"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_A100 = "#B388FF";
 
     /**
      * Color deep purple" A200"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_A200 = "#7C4DFF";
 
     /**
      * Color deep purple" A400"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_A400 = "#651FFF";
 
     /**
      * Color deep purple" A700"
+     *
+     * @var string
      */
     const COLOR_DEEP_PURPLE_A700 = "#6200EA";
+
 }
