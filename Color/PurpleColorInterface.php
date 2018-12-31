d17f63fdcb9feab08534fd334e7647785f3c4c50
webeweb <support@webeweb.fr>
2018-12-31 11:52:56 +0100

    Update PHPDoc

--- Color/PurpleColorInterface.php
+++ Color/PurpleColorInterface.php
@@ -21,71 +21,100 @@ interface PurpleColorInterface {
 
     /**
      * Color purple "50"
+     *
+     * @var string
      */
     const COLOR_PURPLE_50 = "#F3E5F5";
 
     /**
      * Color purple "100"
+     *
+     * @var string
      */
     const COLOR_PURPLE_100 = "#E1BEE7";
 
     /**
      * Color purple "200"
+     *
+     * @var string
      */
     const COLOR_PURPLE_200 = "#CE93D8";
 
     /**
      * Color purple "300"
+     *
+     * @var string
      */
     const COLOR_PURPLE_300 = "#BA68C8";
 
     /**
      * Color purple "400"
+     *
+     * @var string
      */
     const COLOR_PURPLE_400 = "#AB47BC";
 
     /**
      * Color purple "500"
+     *
+     * @var string
      */
     const COLOR_PURPLE_500 = "#9C27B0";
 
     /**
      * Color purple "600"
+     *
+     * @var string
      */
     const COLOR_PURPLE_600 = "#8E24AA";
 
     /**
      * Color purple "700"
+     *
+     * @var string
      */
     const COLOR_PURPLE_700 = "#7B1FA2";
 
     /**
      * Color purple "800"
+     *
+     * @var string
      */
     const COLOR_PURPLE_800 = "#6A1B9A";
 
     /**
      * Color purple "900"
+     *
+     * @var string
      */
     const COLOR_PURPLE_900 = "#4A148C";
 
     /**
      * Color purple "A100"
+     *
+     * @var string
      */
     const COLOR_PURPLE_A100 = "#EA80FC";
 
     /**
      * Color purple "A200"
+     *
+     * @var string
      */
     const COLOR_PURPLE_A200 = "#E040FB";
 
     /**
      * Color purple "A400"
+     *
+     * @var string
      */
     const COLOR_PURPLE_A400 = "#D500F9";
 
     /**
      * Color purple "A700"
+     *
+     * @var string
      */
     const COLOR_PURPLE_A700 = "#AA00FF";
+
 }
