73f43cd097d17dd1ab3cc94c0bf73b9bb1b1ac31
webeweb <support@webeweb.fr>
2018-12-31 14:22:19 +0100

    Add Brown color provider interface

---
+++ Color/BrownColorProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+use WBW\Bundle\CoreBundle\Provider\ColorProviderInterface;
+
+/**
+ * Brown color provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+class BrownColorProvider implements BrownColorInterface, ColorProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getColors() {
+        return [
+            self::COLOR_50  => self::COLOR_BROWN_50,
+            self::COLOR_100 => self::COLOR_BROWN_100,
+            self::COLOR_200 => self::COLOR_BROWN_200,
+            self::COLOR_300 => self::COLOR_BROWN_300,
+            self::COLOR_400 => self::COLOR_BROWN_400,
+            self::COLOR_500 => self::COLOR_BROWN_500,
+            self::COLOR_600 => self::COLOR_BROWN_600,
+            self::COLOR_700 => self::COLOR_BROWN_700,
+        ];
+    }
+
+    /**
+     *{@inheritdoc}
+     */
+    public function getName() {
+        return self::COLOR_BROWN;
+    }
+
+}
---
+++ Tests/Color/BrownColorProviderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Color;
+
+use WBW\Bundle\CoreBundle\Color\BrownColorInterface;
+use WBW\Bundle\CoreBundle\Color\BrownColorProvider;
+use WBW\Bundle\CoreBundle\Color\ColorInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Brown color provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Color
+ */
+class BrownColorProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the getColors() method.
+     *
+     * @return void
+     */
+    public function testGetColors() {
+
+        $obj = new BrownColorProvider();
+
+        $res = $obj->getColors();
+        $this->assertCount(8, $res);
+
+        $this->assertArrayHasKey(ColorInterface::COLOR_50, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_100, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_200, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_300, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_400, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_500, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_600, $res);
+        $this->assertArrayHasKey(ColorInterface::COLOR_700, $res);
+
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_50, $res[ColorInterface::COLOR_50]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_100, $res[ColorInterface::COLOR_100]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_200, $res[ColorInterface::COLOR_200]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_300, $res[ColorInterface::COLOR_300]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_400, $res[ColorInterface::COLOR_400]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_500, $res[ColorInterface::COLOR_500]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_500, $res[ColorInterface::COLOR_500]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_600, $res[ColorInterface::COLOR_600]);
+        $this->assertEquals(BrownColorInterface::COLOR_BROWN_700, $res[ColorInterface::COLOR_700]);
+    }
+
+    /**
+     * Tests the getName() method.
+     *
+     * @return void
+     */
+    public function testGetName() {
+
+        $obj = new BrownColorProvider();
+
+        $res = ColorInterface::COLOR_BROWN;
+        $this->assertEquals($res, $obj->getName());
+    }
+
+}
