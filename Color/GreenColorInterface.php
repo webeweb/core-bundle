d4d48939e14b668cfd80430537ed9dbe080f25ec
webeweb <support@webeweb.fr>
2018-12-31 13:31:59 +0100

    Update Green color interface inheritance

--- Color/GreenColorInterface.php
+++ Color/GreenColorInterface.php
@@ -17,7 +17,7 @@ namespace WBW\Bundle\CoreBundle\Color;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
  */
-interface GreenColorInterface {
+interface GreenColorInterface extends ColorInterface {
 
     /**
      * Color green "50"
