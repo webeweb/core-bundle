8ca533e88d7b3cb8f4b04db776314270ecda154b
webeweb <support@webeweb.fr>
2018-12-31 10:46:27 +0100

    Add Grey color interface

---
+++ Color/GreyColorInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Grey color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface GreyColorInterface {
+
+    /**
+     * Color grey "50"
+     */
+    const COLOR_GREY_50 = "#FAFAFA";
+
+    /**
+     * Color grey "100"
+     */
+    const COLOR_GREY_100 = "#F5F5F5";
+
+    /**
+     * Color grey "200"
+     */
+    const COLOR_GREY_200 = "#EEEEEE";
+
+    /**
+     * Color grey "300"
+     */
+    const COLOR_GREY_300 = "#E0E0E0";
+
+    /**
+     * Color grey "400"
+     */
+    const COLOR_GREY_400 = "#BDBDBD";
+
+    /**
+     * Color grey "500"
+     */
+    const COLOR_GREY_500 = "#9E9E9E";
+
+    /**
+     * Color grey "600"
+     */
+    const COLOR_GREY_600 = "#757575";
+
+    /**
+     * Color grey "700"
+     */
+    const COLOR_GREY_700 = "#616161";
+
+    /**
+     * Color grey "800"
+     */
+    const COLOR_GREY_800 = "#424242";
+
+    /**
+     * Color grey "900"
+     */
+    const COLOR_GREY_900 = "#212121";
+}
