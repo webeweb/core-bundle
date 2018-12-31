7e0a5c1bcd8c9ac55719c7ebff66a2d86abd42d1
webeweb <support@webeweb.fr>
2018-12-31 10:47:49 +0100

    Add Purple color interface

---
+++ Color/PurpleColorInterface.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color;
+
+/**
+ * Purple color interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color
+ */
+interface PurpleColorInterface {
+
+    /**
+     * Color purple "50"
+     */
+    const COLOR_PURPLE_50 = "#F3E5F5";
+
+    /**
+     * Color purple "100"
+     */
+    const COLOR_PURPLE_100 = "#E1BEE7";
+
+    /**
+     * Color purple "200"
+     */
+    const COLOR_PURPLE_200 = "#CE93D8";
+
+    /**
+     * Color purple "300"
+     */
+    const COLOR_PURPLE_300 = "#BA68C8";
+
+    /**
+     * Color purple "400"
+     */
+    const COLOR_PURPLE_400 = "#AB47BC";
+
+    /**
+     * Color purple "500"
+     */
+    const COLOR_PURPLE_500 = "#9C27B0";
+
+    /**
+     * Color purple "600"
+     */
+    const COLOR_PURPLE_600 = "#8E24AA";
+
+    /**
+     * Color purple "700"
+     */
+    const COLOR_PURPLE_700 = "#7B1FA2";
+
+    /**
+     * Color purple "800"
+     */
+    const COLOR_PURPLE_800 = "#6A1B9A";
+
+    /**
+     * Color purple "900"
+     */
+    const COLOR_PURPLE_900 = "#4A148C";
+
+    /**
+     * Color purple "A100"
+     */
+    const COLOR_PURPLE_A100 = "#EA80FC";
+
+    /**
+     * Color purple "A200"
+     */
+    const COLOR_PURPLE_A200 = "#E040FB";
+
+    /**
+     * Color purple "A400"
+     */
+    const COLOR_PURPLE_A400 = "#D500F9";
+
+    /**
+     * Color purple "A700"
+     */
+    const COLOR_PURPLE_A700 = "#AA00FF";
+}
