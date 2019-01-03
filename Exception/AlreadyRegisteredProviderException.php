8a98062743bc82845fd74df9ce49919e0eb9656c
webeweb <support@webeweb.fr>
2019-01-03 17:42:02 +0100

    Add Already registered provider exception

---
+++ Exception/AlreadyRegisteredProviderException.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Exception;
+
+use ReflectionException;
+use WBW\Bundle\CoreBundle\Provider\ProviderInterface;
+use WBW\Library\Core\Argument\ObjectHelper;
+
+/**
+ * Already registered provider exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Exception
+ */
+class AlreadyRegisteredProviderException extends AbstractException {
+
+    /**
+     * Constructor.
+     *
+     * @param ProviderInterface $provider The provider.
+     * @throws ReflectionException Throws a reflection exception if an error occurs.
+     */
+    public function __construct(ProviderInterface $provider) {
+        parent::__construct(sprintf("The provider \"%s\" is already registered", ObjectHelper::getName($provider)), 500);
+    }
+}
---
+++ Tests/Exception/AlreadyRegisteredProviderExceptionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Exception;
+
+use WBW\Bundle\CoreBundle\Exception\AlreadyRegisteredProviderException;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Theme\DefaultApplicationThemeProvider;
+
+/**
+ * Already registered provider exception test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package Exception
+ */
+class AlreadyRegisteredProviderExceptionTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        // Set a Provider mock.
+        $provider = new DefaultApplicationThemeProvider();
+
+        $ex = new AlreadyRegisteredProviderException($provider);
+
+        $res = "The provider \"WBW\\Bundle\\CoreBundle\\Theme\\DefaultApplicationThemeProvider\" is already registered";
+        $this->assertEquals($res, $ex->getMessage());
+    }
+
+}
