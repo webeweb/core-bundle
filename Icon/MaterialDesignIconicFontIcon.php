aa1f02515bb3ddb914eec3867064aa83412aea2e
webeweb <support@webeweb.fr>
2019-01-04 18:51:59 +0100

    Update Material Design iconic font icon

--- Icon/MaterialDesignIconicFontIcon.php
+++ Icon/MaterialDesignIconicFontIcon.php
@@ -17,13 +17,168 @@ namespace WBW\Bundle\CoreBundle\Icon;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Icon
  */
-class MaterialDesignIconicFontIcon extends AbstractIcon {
+class MaterialDesignIconicFontIcon extends AbstractIcon implements MaterialDesignIconicFontIconInterface {
+
+    /**
+     * Border.
+     *
+     * @var string
+     */
+    private $border;
+
+    /**
+     * Fixed width.
+     *
+     * @var bool
+     */
+    private $fixedWidth;
+
+    /**
+     * Flip.
+     *
+     * @var string
+     */
+    private $flip;
+
+    /**
+     * Pull.
+     *
+     * @var string
+     */
+    private $pull;
+
+    /**
+     * Rotate.
+     *
+     * @var string
+     */
+    private $rotate;
+
+    /**
+     * Size.
+     *
+     * @var string
+     */
+    private $size;
+
+    /**
+     * Spin.
+     *
+     * @var string
+     */
+    private $spin;
 
     /**
      * Constructor.
      */
     public function __construct() {
         parent::__construct();
+        $this->setFixedWidth(false);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBorder() {
+        return $this->border;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFixedWidth() {
+        return $this->fixedWidth;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFlip() {
+        return $this->flip;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPull() {
+        return $this->pull;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRotate() {
+        return $this->rotate;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize() {
+        return $this->size;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSpin() {
+        return $this->spin;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setBorder($border) {
+        $this->border = $border;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setFixedWidth($fixedWidth) {
+        $this->fixedWidth = $fixedWidth;
+        return $this;
+    }
+
+    /**
+     *{@inheritdoc}
+     */
+    public function setFlip($flip) {
+        $this->flip = $flip;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPull($pull) {
+        $this->pull = $pull;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRotate($rotate) {
+        $this->rotate = $rotate;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSize($size) {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSpin($spin) {
+        $this->spin = $spin;
+        return $this;
     }
 
 }
--- Tests/Icon/MaterialDesignIconicFontIconTest.php
+++ Tests/Icon/MaterialDesignIconicFontIconTest.php
@@ -31,7 +31,104 @@ class MaterialDesignIconicFontIconTest extends AbstractTestCase {
 
         $obj = new MaterialDesignIconicFontIcon();
 
-        $this->assertNotNull($obj);
+        $this->assertNull($obj->getBorder());
+        $this->assertFalse($obj->getFixedWidth());
+        $this->assertNull($obj->getFlip());
+        $this->assertNull($obj->getPull());
+        $this->assertNull($obj->getRotate());
+        $this->assertNull($obj->getSize());
+        $this->assertNull($obj->getSpin());
+    }
+
+    /**
+     * Tests the setBorder() mmethod.
+     *
+     * @return void
+     */
+    public function testSetBorder() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setBorder("border");
+        $this->assertEquals("border", $obj->getBorder());
+    }
+
+    /**
+     * Tests the setFixedWidth() mmethod.
+     *
+     * @return void
+     */
+    public function testSetFixedWidth() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setFixedWidth(true);
+        $this->assertTrue($obj->getFixedWidth());
+    }
+
+    /**
+     * Tests the setFlip() mmethod.
+     *
+     * @return void
+     */
+    public function testSetFlip() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setFlip("flip");
+        $this->assertEquals("flip", $obj->getFlip());
+    }
+
+    /**
+     * Tests the setPull() mmethod.
+     *
+     * @return void
+     */
+    public function testSetPull() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setPull("pull");
+        $this->assertEquals("pull", $obj->getPull());
+    }
+
+    /**
+     * Tests the setRotate() mmethod.
+     *
+     * @return void
+     */
+    public function testSetRotate() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setRotate("rotate");
+        $this->assertEquals("rotate", $obj->getRotate());
+    }
+
+    /**
+     * Tests the setSize() mmethod.
+     *
+     * @return void
+     */
+    public function testSetSize() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setSize("size");
+        $this->assertEquals("size", $obj->getSize());
+    }
+
+    /**
+     * Tests the setSpin() mmethod.
+     *
+     * @return void
+     */
+    public function testSetSpin() {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setSpin("spin");
+        $this->assertEquals("spin", $obj->getSpin());
     }
 
 }
