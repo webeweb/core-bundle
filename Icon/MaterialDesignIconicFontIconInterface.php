3982ca9fc12489a8d099ad16d23d879c2df8a389
webeweb <support@webeweb.fr>
2019-01-04 18:34:34 +0100

    Add Material design iconic font icon interface

---
+++ Icon/MaterialDesignIconicFontIconInterface.php
@@ -0,0 +1,239 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Icon;
+
+/**
+ * Material design iconic font icon interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Icon
+ */
+interface MaterialDesignIconicFontIconInterface extends IconInterface {
+
+    /**
+     * Material design iconic font border "Border".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_BORDER = "border";
+
+    /**
+     * Material design iconic font border "Border circle".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_BORDER_CIRCLE = "border-circle";
+
+    /**
+     * Material design iconic font flip "Horizontal".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_FLIP_HORIZONTAL = "horizontal";
+
+    /**
+     * Material design iconic font flip "Vertical".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_FLIP_VERTICAL = "vertical";
+
+    /**
+     * Material design iconic font pull "Left".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_PULL_LEFT = "left";
+
+    /**
+     * Material design iconic font pull "Right".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_PULL_RIGHT = "right";
+
+    /**
+     * Material design iconic font rotate "180".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_ROTATE_180 = "180";
+
+    /**
+     * Material design iconic font rotate "270".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_ROTATE_270 = "270";
+
+    /**
+     * Material design iconic font rotate "90".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_ROTATE_90 = "90";
+
+    /**
+     * Material design iconic font size "2x".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SIZE_2X = "2x";
+
+    /**
+     * Material design iconic font size "3x".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SIZE_3X = "3x";
+
+    /**
+     * Material design iconic font size "4x".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SIZE_4X = "4x";
+
+    /**
+     * Material design iconic font size "5x".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SIZE_5X = "5x";
+
+    /**
+     * Material design iconic font size "lg".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SIZE_LG = "lg";
+
+    /**
+     * Material design iconic font spin "Spin".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SPIN = "spin";
+
+    /**
+     * Material design iconic font spin "Spin reverse".
+     *
+     * @var string
+     */
+    const MATERIAL_DESIGN_ICONIC_FONT_SPIN_REVERSE = "spin-reverse";
+
+    /**
+     * Get the border.
+     *
+     * @return string Returns the border.
+     */
+    public function getBorder();
+
+    /**
+     * Get the fixed width.
+     *
+     * @return bool Returns the fixed width.
+     */
+    public function getFixedWidth();
+
+    /**
+     * Get the flip.
+     *
+     * @return string Returns the flip.
+     */
+    public function getFlip();
+
+    /**
+     * Get the pull.
+     *
+     * @return string Returns the pull.
+     */
+    public function getPull();
+
+    /**
+     * Get the rotate.
+     *
+     * @return string Returns the rotate.
+     */
+    public function getRotate();
+
+    /**
+     * Get the size.
+     *
+     * @return string Returns the size.
+     */
+    public function getSize();
+
+    /**
+     * Get the spin.
+     *
+     * @return string Returns the spin.
+     */
+    public function getSpin();
+
+    /**
+     * Set the border.
+     *
+     * @param string $border The border.
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setBorder($border);
+
+    /**
+     * Set the fixed width.
+     *
+     * @param bool $fixedWidth Fixed width ?
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setFixedWidth($fixedWidth);
+
+    /**
+     * Set the flip.
+     *
+     * @param string $flip The flip.
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setFlip($flip);
+
+    /**
+     * Set the pull.
+     *
+     * @param string $pull The pull.
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setPull($pull);
+
+    /**
+     * Set the rotate.
+     *
+     * @param string $rotate The rotate.
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setRotate($rotate);
+
+    /**
+     * Set the size.
+     *
+     * @param string $size The size.
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setSize($size);
+
+    /**
+     * Set the spin.
+     *
+     * @param string $spin The spin.
+     * @return MaterialDesignIconicFontIconInterface Returns this material design iconic font icon.
+     */
+    public function setSpin($spin);
+
+}
