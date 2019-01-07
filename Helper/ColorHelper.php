e610c60af8a7e8021d7066e68f3c546e2512035f
webeweb <support@webeweb.fr>
2019-01-07 17:48:35 +0100

    Add Color helper

---
+++ Helper/ColorHelper.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Helper;
+
+use WBW\Bundle\CoreBundle\Color\AmberColorProvider;
+use WBW\Bundle\CoreBundle\Color\BlueColorProvider;
+use WBW\Bundle\CoreBundle\Color\BlueGreyColorProvider;
+use WBW\Bundle\CoreBundle\Color\BrownColorProvider;
+use WBW\Bundle\CoreBundle\Color\CyanColorProvider;
+use WBW\Bundle\CoreBundle\Color\DeepOrangeColorProvider;
+use WBW\Bundle\CoreBundle\Color\DeepPurpleColorProvider;
+use WBW\Bundle\CoreBundle\Color\GreenColorProvider;
+use WBW\Bundle\CoreBundle\Color\GreyColorProvider;
+use WBW\Bundle\CoreBundle\Color\IndigoColorProvider;
+use WBW\Bundle\CoreBundle\Color\LightBlueColorProvider;
+use WBW\Bundle\CoreBundle\Color\LightGreenColorProvider;
+use WBW\Bundle\CoreBundle\Color\LimeColorProvider;
+use WBW\Bundle\CoreBundle\Color\OrangeColorProvider;
+use WBW\Bundle\CoreBundle\Color\PinkColorProvider;
+use WBW\Bundle\CoreBundle\Color\PurpleColorProvider;
+use WBW\Bundle\CoreBundle\Color\RedColorProvider;
+use WBW\Bundle\CoreBundle\Color\TealColorProvider;
+use WBW\Bundle\CoreBundle\Color\YellowColorProvider;
+
+/**
+ * Color helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Helper
+ */
+class ColorHelper {
+
+    /**
+     * Get the Material Design Color Palette.
+     *
+     * @return ColorProviderInterface[] Returns the Material Design Color Palette.
+     */
+    public static function getMaterialDesignColorPalette() {
+        return [
+            new RedColorProvider(),
+            new PinkColorProvider(),
+            new PurpleColorProvider(),
+            new DeepPurpleColorProvider(),
+            new IndigoColorProvider(),
+            new BlueColorProvider(),
+            new LightBlueColorProvider(),
+            new CyanColorProvider(),
+            new TealColorProvider(),
+            new GreenColorProvider(),
+            new LightGreenColorProvider(),
+            new LimeColorProvider(),
+            new YellowColorProvider(),
+            new AmberColorProvider(),
+            new OrangeColorProvider(),
+            new DeepOrangeColorProvider(),
+            new BrownColorProvider(),
+            new GreyColorProvider(),
+            new BlueGreyColorProvider(),
+        ];
+    }
+}
---
+++ Tests/Helper/ColorHelperTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Helper;
+
+use WBW\Bundle\CoreBundle\Helper\ColorHelper;
+use WBW\Bundle\CoreBundle\Provider\Color\AmberColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\BlueColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\BlueGreyColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\BrownColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\CyanColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\DeepOrangeColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\DeepPurpleColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\GreenColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\GreyColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\IndigoColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\LightBlueColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\LightGreenColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\LimeColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\OrangeColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\PinkColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\PurpleColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\RedColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\TealColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\Color\YellowColorProviderInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Color helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Helper
+ */
+class ColorHelperTest extends AbstractTestCase {
+
+    /**
+     * Tests the getMaterialDesignColorPalette() method.
+     *
+     * @return void
+     */
+    public function testGetMaterialDesignColorPalette() {
+
+        $res = ColorHelper::getMaterialDesignColorPalette();
+
+        $this->assertCount(19, $res);
+
+        $this->assertInstanceOf(RedColorProviderInterface::class, $res[0]);
+        $this->assertInstanceOf(PinkColorProviderInterface::class, $res[1]);
+        $this->assertInstanceOf(PurpleColorProviderInterface::class, $res[2]);
+        $this->assertInstanceOf(DeepPurpleColorProviderInterface::class, $res[3]);
+        $this->assertInstanceOf(IndigoColorProviderInterface::class, $res[4]);
+        $this->assertInstanceOf(BlueColorProviderInterface::class, $res[5]);
+        $this->assertInstanceOf(LightBlueColorProviderInterface::class, $res[6]);
+        $this->assertInstanceOf(CyanColorProviderInterface::class, $res[7]);
+        $this->assertInstanceOf(TealColorProviderInterface::class, $res[8]);
+        $this->assertInstanceOf(GreenColorProviderInterface::class, $res[9]);
+        $this->assertInstanceOf(LightGreenColorProviderInterface::class, $res[10]);
+        $this->assertInstanceOf(LimeColorProviderInterface::class, $res[11]);
+        $this->assertInstanceOf(YellowColorProviderInterface::class, $res[12]);
+        $this->assertInstanceOf(AmberColorProviderInterface::class, $res[13]);
+        $this->assertInstanceOf(OrangeColorProviderInterface::class, $res[14]);
+        $this->assertInstanceOf(DeepOrangeColorProviderInterface::class, $res[15]);
+        $this->assertInstanceOf(BrownColorProviderInterface::class, $res[16]);
+        $this->assertInstanceOf(GreyColorProviderInterface::class, $res[17]);
+        $this->assertInstanceOf(BlueGreyColorProviderInterface::class, $res[18]);
+    }
+}
