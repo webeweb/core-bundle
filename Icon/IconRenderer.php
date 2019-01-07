b3781f3af2a996aac03bc9925cd1e58c60b6e986
webeweb <support@webeweb.fr>
2019-01-07 18:51:42 +0100

    Add Icon renderer

---
+++ Icon/IconRenderer.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Icon;
+
+/**
+ * Icon renderer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Icon
+ */
+class IconRenderer {
+
+    /**
+     * Render a style.
+     *
+     * @param IconInterface $icon The icon.
+     * @return string Returns the rendered style in case of success, null otherwise.
+     */
+    public static function renderStyle(IconInterface $icon) {
+        return null !== $icon->getStyle() ? $icon->getStyle() : null;
+    }
+
+}
---
+++ Tests/Icon/IconRendererTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\TestsIcon;
+
+use WBW\Bundle\CoreBundle\Icon\IconInterface;
+use WBW\Bundle\CoreBundle\Icon\IconRenderer;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Icon renderer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\TestsIcon
+ */
+class IconRendererTest extends AbstractTestCase {
+
+    /**
+     * Icon.
+     *
+     * @var IconInterface
+     */
+    private $icon;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set an Icon mock.
+        $this->icon = $this->getMockBuilder(IconInterface::class)->getMock();
+        $this->icon->expects($this->any())->method("getStyle")->willReturn("color: #000000;");
+    }
+
+    /**
+     * Tests the renderStyle() method.
+     *
+     * @return void.
+     */
+    public function testRenderStyle() {
+
+        $res = IconRenderer::renderStyle($this->icon);
+        $this->assertEquals("color: #000000;", $res);
+    }
+
+}
