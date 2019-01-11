c552c2208507e98ff5784ba2abbff1e22347daf0
webeweb <support@webeweb.fr>
2019-01-11 16:30:08 +0100

    Add abstract Form type test

---
+++ Tests/Form/Type/AbstractFormTypeTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Abstract form type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Form\Type
+ * @abstract
+ */
+abstract class AbstractFormTypeTest extends AbstractTestCase {
+
+    /**
+     * Childs.
+     *
+     * @var array
+     */
+    protected $childs;
+
+    /**
+     * Defaults.
+     *
+     * @var array
+     */
+    protected $defaults;
+
+    /**
+     * Form
+     *
+     * @var FormInterface
+     */
+    protected $form;
+
+    /**
+     * Form builder.
+     *
+     * @var FormBuilderInterface
+     */
+    protected $formBuilder;
+
+    /**
+     * Options resolver.
+     *
+     * @var OptionsResolver
+     */
+    protected $resolver;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Form mock.
+        $this->form = $this->getMockBuilder(FormInterface::class)->getMock();
+
+        // Set a Form builder mock.
+        $this->formBuilder = $this->getMockBuilder(FormBuilderInterface::class)->getMock();
+        $this->formBuilder->expects($this->any())->method("add")->willReturnCallback(function($child, $type = null, array $options = []) {
+            $this->childs[$child] = [
+                "type"    => $type,
+                "options" => $options,
+            ];
+            return $this->formBuilder;
+        });
+        $this->formBuilder->expects($this->any())->method("addEventListener")->willReturn($this->formBuilder);
+        $this->formBuilder->expects($this->any())->method("addModelTransformer")->willReturn($this->formBuilder);
+        $this->formBuilder->expects($this->any())->method("get")->willReturn($this->formBuilder);
+
+        // Set an Options resolver mock.
+        $this->resolver = $this->getMockBuilder(OptionsResolver::class)->getMock();
+        $this->resolver->expects($this->any())->method("setDefaults")->willReturnCallback(function(array $defauls) {
+            $this->defaults = $defauls;
+            return $this->resolver;
+        });
+    }
+}
