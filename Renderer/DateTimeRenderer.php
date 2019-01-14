38d6bfa814e2bdf43d9a4311b1c1d29f859f87f4
webeweb <support@webeweb.fr>
2019-01-14 17:01:10 +0100

    Add Date/time renderer

---
+++ Renderer/DateTimeRenderer.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Renderer;
+
+use DateTime;
+
+/**
+ * Date/time renderer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Renderer
+ */
+class DateTimeRenderer {
+
+    /**
+     * Date/time format.
+     *
+     * @var string
+     */
+    const DATETIME_FORMAT = "Y-m-d H:i";
+
+    /**
+     * Render a date/time.
+     *
+     * @param DateTime $dateTime The date/time.
+     * @param string $format The format.
+     * @return string Returns the rendered date/time.
+     */
+    public static function renderDateTime(DateTime $dateTime = null, $format = self::DATETIME_FORMAT) {
+        if (null === $dateTime) {
+            return "";
+        }
+        return $dateTime->format($format);
+    }
+
+}
---
+++ Tests/Renderer/DateTimeRendererTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Form\Renderer;
+
+use DateTime;
+use WBW\Bundle\CoreBundle\Renderer\DateTimeRenderer;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Date/time renderer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Form\Renderer
+ */
+class DateTimeRendererTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("Y-m-d H:i", DateTimeRenderer::DATETIME_FORMAT);
+    }
+
+    /**
+     * Tests the renderDateTime() method.
+     *
+     * @return void
+     */
+    public function testRenderDateTime() {
+
+        $this->assertEquals("2018-01-14 17:00", DateTimeRenderer::renderDateTime(new DateTime("2018-01-14 17:00")));
+    }
+
+    /**
+     * Tests the renderDateTime() method.
+     *
+     * @return void
+     */
+    public function testRenderDateTimeWithFormat() {
+
+        $this->assertEquals("14/01/2018 17:00", DateTimeRenderer::renderDateTime(new DateTime("2018-01-14 17:00"), "d/m/Y H:i"));
+    }
+
+    /**
+     * Tests the renderDateTime() method.
+     *
+     * @return void
+     */
+    public function testRenderDateTimeWithoutArguments() {
+
+        $this->assertEquals("", DateTimeRenderer::renderDateTime());
+    }
+
+}
