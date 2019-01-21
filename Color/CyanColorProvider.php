cb89174528142fd7332e7b380dc9026b3eaad6e0
webeweb <support@webeweb.fr>
2019-01-21 17:41:40 +0100

    Move Cyan color provider classes

--- Color/CyanColorProvider.php
+++ Color/CyanColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\CyanColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\CyanColorProvider as BaseCyanColorProvider;
 
 /**
  * Cyan color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\CyanColorProvider} instead.
  */
-class CyanColorProvider extends AbstractColorProvider implements CyanColorProviderInterface, ColorInterface {
+class CyanColorProvider extends BaseCyanColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.cyan";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::CYAN_COLOR_50,
-            self::COLOR_100  => self::CYAN_COLOR_100,
-            self::COLOR_200  => self::CYAN_COLOR_200,
-            self::COLOR_300  => self::CYAN_COLOR_300,
-            self::COLOR_400  => self::CYAN_COLOR_400,
-            self::COLOR_500  => self::CYAN_COLOR_500,
-            self::COLOR_600  => self::CYAN_COLOR_600,
-            self::COLOR_700  => self::CYAN_COLOR_700,
-            self::COLOR_A100 => self::CYAN_COLOR_A100,
-            self::COLOR_A200 => self::CYAN_COLOR_A200,
-            self::COLOR_A400 => self::CYAN_COLOR_A400,
-            self::COLOR_A700 => self::CYAN_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::CYAN_COLOR_NAME;
-    }
 }
