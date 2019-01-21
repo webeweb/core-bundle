80d0cfc1be738fecafb36f03909128641964b500
webeweb <support@webeweb.fr>
2019-01-21 17:47:41 +0100

    Move Lime color provider classes

--- Color/LimeColorProvider.php
+++ Color/LimeColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\LimeColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\LimeColorProvider as BaseLimeColorProvider;
 
 /**
  * Lime color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\LimeColorProvider} instead.
  */
-class LimeColorProvider extends AbstractColorProvider implements LimeColorProviderInterface, ColorInterface {
+class LimeColorProvider extends BaseLimeColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.lime";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::LIME_COLOR_50,
-            self::COLOR_100  => self::LIME_COLOR_100,
-            self::COLOR_200  => self::LIME_COLOR_200,
-            self::COLOR_300  => self::LIME_COLOR_300,
-            self::COLOR_400  => self::LIME_COLOR_400,
-            self::COLOR_500  => self::LIME_COLOR_500,
-            self::COLOR_600  => self::LIME_COLOR_600,
-            self::COLOR_700  => self::LIME_COLOR_700,
-            self::COLOR_A100 => self::LIME_COLOR_A100,
-            self::COLOR_A200 => self::LIME_COLOR_A200,
-            self::COLOR_A400 => self::LIME_COLOR_A400,
-            self::COLOR_A700 => self::LIME_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::LIME_COLOR_NAME;
-    }
 }
---
+++ Tests/Color/MaterialDesignColorPalette/LimeColorProviderTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Color\MaterialDesignColorPalette;
+
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\LimeColorProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette\TestLimeColorProviderTrait;
+
+/**
+ * Lime color provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Color\MaterialDesignColorPalette
+ */
+class LimeColorProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestLimeColorProviderTrait();
+
+        $this->assertNull($obj->getLimeColorProvider());
+    }
+
+    /**
+     * Tests the setLimeColorProvider() method.
+     *
+     * @return void
+     */
+    public function testSetLimeColorProvider() {
+
+        // Set a Lime color provider mock.
+        $limeColorProvider = new LimeColorProvider();
+
+        $obj = new TestLimeColorProviderTrait();
+
+        $obj->setLimeColorProvider($limeColorProvider);
+        $this->assertSame($limeColorProvider, $obj->getLimeColorProvider());
+    }
+}
