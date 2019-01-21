f3dacc108de4a96e82ccecb0cefa3187a1e39b4e
webeweb <support@webeweb.fr>
2019-01-21 18:07:37 +0100

    Declare Utility Twig extension as service

--- Tests/DependencyInjection/CoreExtensionTest.php
+++ Tests/DependencyInjection/CoreExtensionTest.php
@@ -43,6 +43,7 @@ use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\MaterialDesignColorPaletteTwigEx
 use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\MaterialDesignIconicFontTwigExtension;
 use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\MeteoconsTwigExtension;
 use WBW\Bundle\CoreBundle\Twig\Extension\RendererTwigExtension;
+use WBW\Bundle\CoreBundle\Twig\Extension\UtilityTwigExtension;
 
 /**
  * Core extension test.
@@ -104,5 +105,8 @@ class CoreExtensionTest extends AbstractTestCase {
 
         // Renderer Twig extension
         $this->assertInstanceOf(RendererTwigExtension::class, $this->containerBuilder->get(RendererTwigExtension::SERVICE_NAME));
+
+        // Utility Twig extension
+        $this->assertInstanceOf(UtilityTwigExtension::class, $this->containerBuilder->get(UtilityTwigExtension::SERVICE_NAME));
     }
 }
