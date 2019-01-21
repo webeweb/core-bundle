8643d73307b57f2cbca05fc665be614d42a83e83
webeweb <support@webeweb.fr>
2019-01-21 17:43:09 +0100

    Move Deep purple color provider classes

---
+++ Tests/Color/MaterialDesignColorPalette/DeepPurpleColorProviderTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Color\MaterialDesignColorPalette;
+
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\DeepPurpleColorProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette\TestDeepPurpleColorProviderTrait;
+
+/**
+ * Deep purple color provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Color\MaterialDesignColorPalette
+ */
+class DeepPurpleColorProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestDeepPurpleColorProviderTrait();
+
+        $this->assertNull($obj->getDeepPurpleColorProvider());
+    }
+
+    /**
+     * Tests the setDeepPurpleColorProvider() method.
+     *
+     * @return void
+     */
+    public function testSetDeepPurpleColorProvider() {
+
+        // Set an Deep purple color provider mock.
+        $deepPurpleColorProvider = new DeepPurpleColorProvider();
+
+        $obj = new TestDeepPurpleColorProviderTrait();
+
+        $obj->setDeepPurpleColorProvider($deepPurpleColorProvider);
+        $this->assertSame($deepPurpleColorProvider, $obj->getDeepPurpleColorProvider());
+    }
+}
