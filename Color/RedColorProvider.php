fe4ec5633b945e43d5b0b09d1a556a9218de460c
webeweb <support@webeweb.fr>
2019-01-21 17:49:49 +0100

    Move Red color provider classes

--- Color/RedColorProvider.php
+++ Color/RedColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\RedColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\RedColorProvider as BaseRedColorProvider;
 
 /**
  * Red color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\RedColorProvider} instead.
  */
-class RedColorProvider extends AbstractColorProvider implements RedColorProviderInterface, ColorInterface {
+class RedColorProvider extends BaseRedColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.red";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::RED_COLOR_50,
-            self::COLOR_100  => self::RED_COLOR_100,
-            self::COLOR_200  => self::RED_COLOR_200,
-            self::COLOR_300  => self::RED_COLOR_300,
-            self::COLOR_400  => self::RED_COLOR_400,
-            self::COLOR_500  => self::RED_COLOR_500,
-            self::COLOR_600  => self::RED_COLOR_600,
-            self::COLOR_700  => self::RED_COLOR_700,
-            self::COLOR_A100 => self::RED_COLOR_A100,
-            self::COLOR_A200 => self::RED_COLOR_A200,
-            self::COLOR_A400 => self::RED_COLOR_A400,
-            self::COLOR_A700 => self::RED_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::RED_COLOR_NAME;
-    }
 }
---
+++ Tests/Color/MaterialDesignColorPalette/RedColorProviderTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Color\MaterialDesignColorPalette;
+
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\RedColorProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette\TestRedColorProviderTrait;
+
+/**
+ * Red color provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Color\MaterialDesignColorPalette
+ */
+class RedColorProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestRedColorProviderTrait();
+
+        $this->assertNull($obj->getRedColorProvider());
+    }
+
+    /**
+     * Tests the setRedColorProvider() method.
+     *
+     * @return void
+     */
+    public function testSetRedColorProvider() {
+
+        // Set an Red color provider mock.
+        $redColorProvider = new RedColorProvider();
+
+        $obj = new TestRedColorProviderTrait();
+
+        $obj->setRedColorProvider($redColorProvider);
+        $this->assertSame($redColorProvider, $obj->getRedColorProvider());
+    }
+}
