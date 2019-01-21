14882bde153b9a4b67002e6da62f148d6eb15a28
webeweb <support@webeweb.fr>
2019-01-21 17:43:49 +0100

    Move Green color provider classes

--- Color/GreenColorProvider.php
+++ Color/GreenColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\GreenColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\GreenColorProvider as BaseGreenColorProvider;
 
 /**
  * Green color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\GreenColorProvider} instead.
  */
-class GreenColorProvider extends AbstractColorProvider implements GreenColorProviderInterface, ColorInterface {
+class GreenColorProvider extends BaseGreenColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.green";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::GREEN_COLOR_50,
-            self::COLOR_100  => self::GREEN_COLOR_100,
-            self::COLOR_200  => self::GREEN_COLOR_200,
-            self::COLOR_300  => self::GREEN_COLOR_300,
-            self::COLOR_400  => self::GREEN_COLOR_400,
-            self::COLOR_500  => self::GREEN_COLOR_500,
-            self::COLOR_600  => self::GREEN_COLOR_600,
-            self::COLOR_700  => self::GREEN_COLOR_700,
-            self::COLOR_A100 => self::GREEN_COLOR_A100,
-            self::COLOR_A200 => self::GREEN_COLOR_A200,
-            self::COLOR_A400 => self::GREEN_COLOR_A400,
-            self::COLOR_A700 => self::GREEN_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::GREEN_COLOR_NAME;
-    }
 }
