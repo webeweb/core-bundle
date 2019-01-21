ca718b79c14cbd9d33e33c83f4f2955b882c63e9
webeweb <support@webeweb.fr>
2019-01-21 17:49:17 +0100

    Move Purple color provider classes

--- Color/PurpleColorProvider.php
+++ Color/PurpleColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\PurpleColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\PurpleColorProvider as BasePurpleColorProvider;
 
 /**
  * Purple color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\PurpleColorProvider} instead.
  */
-class PurpleColorProvider extends AbstractColorProvider implements PurpleColorProviderInterface, ColorInterface {
+class PurpleColorProvider extends BasePurpleColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.purple";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::PURPLE_COLOR_50,
-            self::COLOR_100  => self::PURPLE_COLOR_100,
-            self::COLOR_200  => self::PURPLE_COLOR_200,
-            self::COLOR_300  => self::PURPLE_COLOR_300,
-            self::COLOR_400  => self::PURPLE_COLOR_400,
-            self::COLOR_500  => self::PURPLE_COLOR_500,
-            self::COLOR_600  => self::PURPLE_COLOR_600,
-            self::COLOR_700  => self::PURPLE_COLOR_700,
-            self::COLOR_A100 => self::PURPLE_COLOR_A100,
-            self::COLOR_A200 => self::PURPLE_COLOR_A200,
-            self::COLOR_A400 => self::PURPLE_COLOR_A400,
-            self::COLOR_A700 => self::PURPLE_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::PURPLE_COLOR_NAME;
-    }
 }
---
+++ Tests/Fixtures/Color/MaterialDesignColorPalette/TestPurpleColorProviderTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette;
+
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\PurpleColorProviderTrait;
+
+/**
+ * Test purple color provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette
+ */
+class TestPurpleColorProviderTrait {
+
+    use PurpleColorProviderTrait {
+        setPurpleColorProvider as public;
+    }
+}
