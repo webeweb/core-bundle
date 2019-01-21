4436c0eabcc72f794abace9df126540dfc992489
webeweb <support@webeweb.fr>
2019-01-21 17:48:43 +0100

    Move Pink color provider classes

--- Color/PinkColorProvider.php
+++ Color/PinkColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\PinkColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\PinkColorProvider as BasePinkColorProvider;
 
 /**
  * Pink color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\PinkColorProvider} instead.
  */
-class PinkColorProvider extends AbstractColorProvider implements PinkColorProviderInterface, ColorInterface {
+class PinkColorProvider extends BasePinkColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.pink";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::PINK_COLOR_50,
-            self::COLOR_100  => self::PINK_COLOR_100,
-            self::COLOR_200  => self::PINK_COLOR_200,
-            self::COLOR_300  => self::PINK_COLOR_300,
-            self::COLOR_400  => self::PINK_COLOR_400,
-            self::COLOR_500  => self::PINK_COLOR_500,
-            self::COLOR_600  => self::PINK_COLOR_600,
-            self::COLOR_700  => self::PINK_COLOR_700,
-            self::COLOR_A100 => self::PINK_COLOR_A100,
-            self::COLOR_A200 => self::PINK_COLOR_A200,
-            self::COLOR_A400 => self::PINK_COLOR_A400,
-            self::COLOR_A700 => self::PINK_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::PINK_COLOR_NAME;
-    }
 }
