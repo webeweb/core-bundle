78188236c2f2da2aff79491abefa0f692410e874
webeweb <support@webeweb.fr>
2019-01-21 17:50:21 +0100

    Move Teal color provider classes

--- Color/TealColorProvider.php
+++ Color/TealColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\TealColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\TealColorProvider as BaseTealColorProvider;
 
 /**
  * Teal color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\TealColorProvider} instead.
  */
-class TealColorProvider extends AbstractColorProvider implements TealColorProviderInterface, ColorInterface {
+class TealColorProvider extends BaseTealColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.teal";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::TEAL_COLOR_50,
-            self::COLOR_100  => self::TEAL_COLOR_100,
-            self::COLOR_200  => self::TEAL_COLOR_200,
-            self::COLOR_300  => self::TEAL_COLOR_300,
-            self::COLOR_400  => self::TEAL_COLOR_400,
-            self::COLOR_500  => self::TEAL_COLOR_500,
-            self::COLOR_600  => self::TEAL_COLOR_600,
-            self::COLOR_700  => self::TEAL_COLOR_700,
-            self::COLOR_A100 => self::TEAL_COLOR_A100,
-            self::COLOR_A200 => self::TEAL_COLOR_A200,
-            self::COLOR_A400 => self::TEAL_COLOR_A400,
-            self::COLOR_A700 => self::TEAL_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::TEAL_COLOR_NAME;
-    }
 }
---
+++ Tests/Fixtures/Color/MaterialDesignColorPalette/TestTealColorProviderTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette;
+
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\TealColorProviderTrait;
+
+/**
+ * Test teal color provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures\Color\MaterialDesignColorPalette
+ */
+class TestTealColorProviderTrait {
+
+    use TealColorProviderTrait {
+        setTealColorProvider as public;
+    }
+}
