0c474bd7259c82a0053d14b15125556bf14fcf8c
webeweb <support@webeweb.fr>
2019-01-21 17:54:22 +0100

    Update file header

--- Tests/Color/MaterialDesignColorPalette/AmberColorProviderTest.php
+++ Tests/Color/MaterialDesignColorPalette/AmberColorProviderTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the core-bundle package.
  *
- * (c) 2018 WEBEWEB
+ * (c) 2019 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
