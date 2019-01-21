c4c2ca1900eb2f85a22b8d7a8b290bbcce4fcd6d
webeweb <support@webeweb.fr>
2019-01-21 17:45:18 +0100

    Move Indigo color provider classes

--- Color/IndigoColorProvider.php
+++ Color/IndigoColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\IndigoColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\IndigoColorProvider as BaseIndigoColorProvider;
 
 /**
  * Indigo color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\LightBlueColorProvider} instead.
  */
-class IndigoColorProvider extends AbstractColorProvider implements IndigoColorProviderInterface, ColorInterface {
+class IndigoColorProvider extends BaseIndigoColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.indigo";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::INDIGO_COLOR_50,
-            self::COLOR_100  => self::INDIGO_COLOR_100,
-            self::COLOR_200  => self::INDIGO_COLOR_200,
-            self::COLOR_300  => self::INDIGO_COLOR_300,
-            self::COLOR_400  => self::INDIGO_COLOR_400,
-            self::COLOR_500  => self::INDIGO_COLOR_500,
-            self::COLOR_600  => self::INDIGO_COLOR_600,
-            self::COLOR_700  => self::INDIGO_COLOR_700,
-            self::COLOR_A100 => self::INDIGO_COLOR_A100,
-            self::COLOR_A200 => self::INDIGO_COLOR_A200,
-            self::COLOR_A400 => self::INDIGO_COLOR_A400,
-            self::COLOR_A700 => self::INDIGO_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::INDIGO_COLOR_NAME;
-    }
 }
