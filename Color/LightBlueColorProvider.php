bd3847449526ccebc65b971b29e590efc60a1ad3
webeweb <support@webeweb.fr>
2019-01-21 17:45:54 +0100

    Move Light blue color provider classes

--- Color/LightBlueColorProvider.php
+++ Color/LightBlueColorProvider.php
@@ -11,54 +11,15 @@
 
 namespace WBW\Bundle\CoreBundle\Color;
 
-use WBW\Bundle\CoreBundle\Provider\Color\LightBlueColorProviderInterface;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\LightBlueColorProvider as BaseLightBlueColorProvider;
 
 /**
  * Light blue color provider.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Color
+ * @deprecated since Core bundle 1.8, use {@see WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\LightBlueColorProvider} instead.
  */
-class LightBlueColorProvider extends AbstractColorProvider implements LightBlueColorProviderInterface, ColorInterface {
+class LightBlueColorProvider extends BaseLightBlueColorProvider implements ColorInterface {
 
-    /**
-     * Service name.
-     *
-     * @var string
-     */
-    const SERVICE_NAME = "webeweb.core.provider.color.light_blue";
-
-    /**
-     * Constructor.
-     */
-    public function __construct() {
-        parent::__construct("MaterialDesignColorPalette");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getColors() {
-        return [
-            self::COLOR_50   => self::LIGHT_BLUE_COLOR_50,
-            self::COLOR_100  => self::LIGHT_BLUE_COLOR_100,
-            self::COLOR_200  => self::LIGHT_BLUE_COLOR_200,
-            self::COLOR_300  => self::LIGHT_BLUE_COLOR_300,
-            self::COLOR_400  => self::LIGHT_BLUE_COLOR_400,
-            self::COLOR_500  => self::LIGHT_BLUE_COLOR_500,
-            self::COLOR_600  => self::LIGHT_BLUE_COLOR_600,
-            self::COLOR_700  => self::LIGHT_BLUE_COLOR_700,
-            self::COLOR_A100 => self::LIGHT_BLUE_COLOR_A100,
-            self::COLOR_A200 => self::LIGHT_BLUE_COLOR_A200,
-            self::COLOR_A400 => self::LIGHT_BLUE_COLOR_A400,
-            self::COLOR_A700 => self::LIGHT_BLUE_COLOR_A700,
-        ];
-    }
-
-    /**
-     *{@inheritdoc}
-     */
-    public function getName() {
-        return self::LIGHT_BLUE_COLOR_NAME;
-    }
 }
---
+++ Color/MaterialDesignColorPalette/LightBlueColorProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette;
+
+use WBW\Bundle\CoreBundle\Color\AbstractColorProvider;
+use WBW\Bundle\CoreBundle\Provider\Color\LightBlueColorProviderInterface;
+
+/**
+ * Light blue color provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette
+ */
+class LightBlueColorProvider extends AbstractColorProvider implements LightBlueColorProviderInterface, MaterialDesignColorPaletteInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.provider.color.light_blue";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct("MaterialDesignColorPalette");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getColors() {
+        return [
+            self::COLOR_50   => self::LIGHT_BLUE_COLOR_50,
+            self::COLOR_100  => self::LIGHT_BLUE_COLOR_100,
+            self::COLOR_200  => self::LIGHT_BLUE_COLOR_200,
+            self::COLOR_300  => self::LIGHT_BLUE_COLOR_300,
+            self::COLOR_400  => self::LIGHT_BLUE_COLOR_400,
+            self::COLOR_500  => self::LIGHT_BLUE_COLOR_500,
+            self::COLOR_600  => self::LIGHT_BLUE_COLOR_600,
+            self::COLOR_700  => self::LIGHT_BLUE_COLOR_700,
+            self::COLOR_A100 => self::LIGHT_BLUE_COLOR_A100,
+            self::COLOR_A200 => self::LIGHT_BLUE_COLOR_A200,
+            self::COLOR_A400 => self::LIGHT_BLUE_COLOR_A400,
+            self::COLOR_A700 => self::LIGHT_BLUE_COLOR_A700,
+        ];
+    }
+
+    /**
+     *{@inheritdoc}
+     */
+    public function getName() {
+        return self::LIGHT_BLUE_COLOR_NAME;
+    }
+}
