cf5cbfce145a6f65c265d845dedf77556ab78d83
webeweb <support@webeweb.fr>
2019-02-16 15:13:30 +0100

    Add FOSUser breadcrumb nodes

---
+++ Navigation/FOSUserBreadcrumbNodes.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Navigation;
+
+/**
+ * FOSUser breadcrumb nodes.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Navigation
+ */
+class FOSUserBreadcrumbNodes {
+
+    /**
+     * Get a FOSUser breadcrumb node with Font Awesome icons.
+     *
+     * @return BreadcrumbNode[] Returns the FOSUser breadcrumb node.
+     */
+    public static function getFontAwesomeBreadcrumbNodes() {
+
+        $breadcrumbNodes = [];
+
+        $breadcrumbNodes[] = new BreadcrumbNode("label.edit_profile", "fa:user", "fos_user_profile_edit", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $breadcrumbNodes[] = new BreadcrumbNode("label.show_profile", "fa:user", "fos_user_profile_show", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $breadcrumbNodes[] = new BreadcrumbNode("label.change_password", "fa:lock", "fos_user_change_password", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+
+        return $breadcrumbNodes;
+    }
+
+    /**
+     * Get a FOSUser breadcrumb node with Material Design Iconic Font icons.
+     *
+     * @return BreadcrumbNode[] Returns the FOSUser breadcrumb node.
+     */
+    public static function getMaterialDesignIconicFontBreadcrumbNodes() {
+
+        $breadcrumbNodes = [];
+
+        $breadcrumbNodes[] = new BreadcrumbNode("label.edit_profile", "zmdi:account", "fos_user_profile_edit", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $breadcrumbNodes[] = new BreadcrumbNode("label.show_profile", "zmdi:account", "fos_user_profile_show", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $breadcrumbNodes[] = new BreadcrumbNode("label.change_password", "zmdi:lock", "fos_user_change_password", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+
+        return $breadcrumbNodes;
+    }
+}
