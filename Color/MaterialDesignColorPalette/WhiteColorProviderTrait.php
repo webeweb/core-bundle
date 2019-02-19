b999cd047cf2734334c43f3ce91c4e051614c56b
webeweb <support@webeweb.fr>
2019-02-19 17:35:02 +0100

    Add White color provider trait

---
+++ Color/MaterialDesignColorPalette/WhiteColorProviderTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette;
+
+/**
+ * White color provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette
+ */
+trait WhiteColorProviderTrait {
+
+    /**
+     * White color provider.
+     *
+     * @var WhiteColorProvider
+     */
+    private $whiteColorProvider;
+
+    /**
+     * Get the white color provider.
+     *
+     * @return WhiteColorProvider Returns the white color provider.
+     */
+    public function getWhiteColorProvider() {
+        return $this->whiteColorProvider;
+    }
+
+    /**
+     * Set the white color provider.
+     *
+     * @param WhiteColorProvider|null $whiteColorProvider The white color provider.
+     */
+    protected function setWhiteColorProvider(WhiteColorProvider $whiteColorProvider = null) {
+        $this->whiteColorProvider = $whiteColorProvider;
+        return $this;
+    }
+}
