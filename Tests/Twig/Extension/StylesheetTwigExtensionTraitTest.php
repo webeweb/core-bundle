89bb0b961d961e2d66e43db677f3c81f9eafbbd3
webeweb <support@webeweb.fr>
2019-02-25 18:02:22 +0100

    Add Stylesheet Twig extension trait

---
+++ Tests/Twig/Extension/StylesheetTwigExtensionTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Twig\Extension;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Twig\Extension\TestStylesheetTwigExtensionTrait;
+use WBW\Bundle\CoreBundle\Twig\Extension\StylesheetTwigExtension;
+
+/**
+ * Stylesheet Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Twig\Extension
+ */
+class StylesheetTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestStylesheetTwigExtensionTrait();
+
+        $this->assertNull($obj->getStylesheetTwigExtension());
+    }
+
+    /**
+     * Tests the setStylesheetTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetStylesheetTwigExtension() {
+
+        // Set a Stylesheet Twig extension mock.
+        $stylesheetTwigExtension = new StylesheetTwigExtension($this->twigEnvironment);
+
+        $obj = new TestStylesheetTwigExtensionTrait();
+
+        $obj->setStylesheetTwigExtension($stylesheetTwigExtension);
+        $this->assertSame($stylesheetTwigExtension, $obj->getStylesheetTwigExtension());
+    }
+}
---
+++ Twig/Extension/StylesheetTwigExtensionTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Twig\Extension;
+
+/**
+ * Stylesheet Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Twig\Extension
+ */
+trait StylesheetTwigExtensionTrait {
+
+    /**
+     * Stylesheet Twig extension.
+     *
+     * @var StylesheetTwigExtension
+     */
+    private $stylesheetTwigExtension;
+
+    /**
+     * Get the stylesheet Twig extension.
+     *
+     * @return StylesheetTwigExtension Returns the stylesheet Twig extension.
+     */
+    public function getStylesheetTwigExtension() {
+        return $this->stylesheetTwigExtension;
+    }
+
+    /**
+     * Set the stylesheet twig extension.
+     *
+     * @param StylesheetTwigExtension|null $stylesheetTwigExtension The stylesheet twig extension.
+     */
+    protected function setStylesheetTwigExtension(StylesheetTwigExtension $stylesheetTwigExtension = null) {
+        $this->stylesheetTwigExtension = $stylesheetTwigExtension;
+        return $this;
+    }
+}
