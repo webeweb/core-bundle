c2ae35e023c63c22c09563fa98a87e3db723a318
webeweb <support@webeweb.fr>
2019-02-25 18:01:40 +0100

    Add Stylesheet Twig extension

---
+++ Tests/Twig/Extension/StylesheetTwigExtensionTest.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Twig\Extension;
+
+use Twig_Node;
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Twig\Extension\StylesheetTwigExtension;
+
+/**
+ * Stylesheet Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Twig\Extension
+ */
+class StylesheetTwigExtensionTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new StylesheetTwigExtension($this->twigEnvironment);
+
+        $this->assertEquals("webeweb.core.twig.extension.stylesheet", StylesheetTwigExtension::SERVICE_NAME);
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+
+    /**
+     * Tests the cssRGBA() method.
+     *
+     * @return void
+     */
+    public function testCssRGBA() {
+
+        $obj = new StylesheetTwigExtension($this->twigEnvironment);
+
+        $this->assertEquals("rgba(0, 0, 0, 0.00)", $obj->cssRGBA("#000000", 0.00));
+        $this->assertEquals("rgba(0, 0, 0, 0.00)", $obj->cssRGBA("000000", 0.00));
+        $this->assertEquals("rgba(0, 0, 0, 0.00)", $obj->cssRGBA("#000", 0.00));
+        $this->assertEquals("rgba(0, 0, 0, 0.00)", $obj->cssRGBA("000", 0.00));
+
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("#AAAAAA", 0.50));
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("AAAAAA", 0.50));
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("#AAA", 0.50));
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("AAA", 0.50));
+
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("#aaaaaa", 0.50));
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("aaaaaa", 0.50));
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("#aaa", 0.50));
+        $this->assertEquals("rgba(170, 170, 170, 0.50)", $obj->cssRGBA("aaa", 0.50));
+
+        $this->assertEquals("rgba(255, 255, 255, 1.00)", $obj->cssRGBA("#FFFFFF", 1.00));
+        $this->assertEquals("rgba(255, 255, 255, 1.00)", $obj->cssRGBA("FFFFFF", 1.00));
+        $this->assertEquals("rgba(255, 255, 255, 1.00)", $obj->cssRGBA("#FFF", 1.00));
+        $this->assertEquals("rgba(255, 255, 255, 1.00)", $obj->cssRGBA("FFF", 1.00));
+    }
+
+    /**
+     * Tests the getFilters() method.
+     *
+     * @return void
+     */
+    public function testGetFilters() {
+
+        $obj = new StylesheetTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFilters();
+        $this->assertCount(1, $res);
+
+        $this->assertInstanceOf(Twig_SimpleFilter::class, $res[0]);
+        $this->assertEquals("cssRGBA", $res[0]->getName());
+        $this->assertEquals([$obj, "cssRGBA"], $res[0]->getCallable());
+        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+    }
+
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new StylesheetTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFunctions();
+        $this->assertCount(1, $res);
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
+        $this->assertEquals("cssRGBA", $res[0]->getName());
+        $this->assertEquals([$obj, "cssRGBA"], $res[0]->getCallable());
+        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+    }
+}
---
+++ Twig/Extension/StylesheetTwigExtension.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Twig\Extension;
+
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
+use WBW\Library\Core\Argument\StringHelper;
+
+/**
+ * Stylesheet Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Twig\Extension\CSS
+ */
+class StylesheetTwigExtension extends AbstractTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.core.twig.extension.stylesheet";
+
+    /**
+     * Displays a rgba().
+     *
+     * @param string $color The hexadecimal color.
+     * @param float $alpha The alpha channel.
+     * @return string Returns the rgba().
+     */
+    public function cssRGBA($color, $alpha) {
+
+        if (0 === preg_match_all("/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/", strtolower($color), $hex)) {
+            return "";
+        }
+
+        $length = strlen($hex[1][0]);
+
+        $pattern = "/[A-Fa-f0-9]{" . intval($length / 3) . "}/";
+        preg_match_all($pattern, $hex[1][0], $channels);
+
+        if (3 === $length) {
+            $channels[0][0] = str_repeat($channels[0][0], 2);
+            $channels[0][1] = str_repeat($channels[0][1], 2);
+            $channels[0][2] = str_repeat($channels[0][2], 2);
+        }
+
+        $template = "rgba(%r%, %g%, %b%, %a%)";
+
+        return StringHelper::replace($template, ["%r%", "%g%", "%b%", "%a%"], [hexdec($channels[0][0]), hexdec($channels[0][1]), hexdec($channels[0][2]), number_format($alpha, 2)]);
+    }
+
+    /**
+     * Get the Twig filters.
+     *
+     * @return Twig_SimpleFilter[] Returns the Twig filters.
+     */
+    public function getFilters() {
+        return [
+            new Twig_SimpleFilter("cssRGBA", [$this, "cssRGBA"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("cssRGBA", [$this, "cssRGBA"], ["is_safe" => ["html"]]),
+        ];
+    }
+}
