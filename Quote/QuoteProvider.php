bb7d272f56eecfb25887de8a18f3c86429f169e1
webeweb <support@webeweb.fr>
2019-04-15 19:13:56 +0200

    Add Quote provider implementation

---
+++ Quote/QuoteProvider.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Quote;
+
+use DateTime;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use Symfony\Component\Yaml\Yaml;
+use WBW\Bundle\CoreBundle\Provider\QuoteProviderInterface;
+use WBW\Library\Core\Argument\ArrayHelper;
+
+/**
+ * Quote provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Quote
+ */
+class QuoteProvider implements QuoteProviderInterface {
+
+    /**
+     * Filename.
+     *
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * Quotes.
+     *
+     * @var QuoteInterface[]
+     */
+    private $quotes;
+
+    /**
+     * Constructor.
+     *
+     * @param string $filename The filename.
+     * @throws FileNotFoundException Throws a file not found exception.
+     */
+    public function __construct($filename) {
+        $this->setFilename($filename);
+        $this->setQuotes([]);
+    }
+
+    /**
+     *{@inheritdoc}
+     */
+    public function getAuthors() {
+
+        $authors = [];
+
+        foreach ($this->quotes as $current) {
+            if (true === in_array($current->getAuthor(), $authors)) {
+                continue;
+            }
+            $authors[] = $current->getAuthor();
+        }
+
+        asort($authors);
+
+        return $authors;
+    }
+
+    /**
+     * Get the filename.
+     *
+     * @return string Returns the filename.
+     */
+    public function getFilename() {
+        return $this->filename;
+    }
+
+    /**
+     * Get the quotes.
+     *
+     * @return QuoteInterface[] Returns the quotes.
+     */
+    public function getQuotes() {
+        return $this->quotes;
+    }
+
+    /**
+     * Parses.
+     *
+     * @return void
+     */
+    public function parse() {
+
+        $fileContent = file_get_contents($this->filename);
+
+        $yamlContent = Yaml::parse($fileContent);
+
+        foreach ($yamlContent as $k => $v) {
+
+            $date = DateTime::createFromFormat("!m.d", $k);
+
+            $model = new Quote();
+            $model->setDate(false === $date ? nulll : $date);
+            $model->setAuthor(ArrayHelper::get($v, "author"));
+            $model->setContent(ArrayHelper::get($v, "content"));
+
+            $this->quotes[] = $model;
+        }
+    }
+
+    /**
+     * Set the filename.
+     *
+     * @param string $filename The filename.
+     * @return QuoteProvider Returns this quote provider.
+     * @throws FileNotFoundException Throws a file not found exception.
+     */
+    protected function setFilename($filename) {
+        if (false === realpath($filename)) {
+            throw new FileNotFoundException(sprintf("The file \"%s\" was not found", $filename));
+        }
+        $this->filename = realpath($filename);
+        return $this;
+    }
+
+    /**
+     * Set the quotes.
+     *
+     * @param QuoteInterface[] $quotes The quotes.
+     * @return QuoteProvider Returns this quote provider.
+     */
+    protected function setQuotes(array $quotes) {
+        $this->quotes = $quotes;
+        return $this;
+    }
+}
---
+++ Tests/Quote/QuoteProviderTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Quote;
+
+use Exception;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use WBW\Bundle\CoreBundle\Quote\QuoteProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Quote provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Quote
+ */
+class QuoteProviderTest extends AbstractTestCase {
+
+    /**
+     * Filename.
+     *
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        $this->filename = getcwd() . "/Resources/translations/WorldsWisdom.fr.yml";
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new QuoteProvider($this->filename);
+
+        $this->assertEquals($this->filename, $obj->getFilename());
+        $this->assertEquals([], $obj->getAuthors());
+        $this->assertEquals([], $obj->getQuotes());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructWithFileNotFoundException() {
+
+        try {
+
+            new QuoteProvider("exception");
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(FileNotFoundException::class, $ex);
+            $this->assertEquals("The file \"exception\" was not found", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the parse() method.
+     *
+     * @return void
+     */
+    public function testParse() {
+
+        $obj = new QuoteProvider($this->filename);
+
+        $obj->parse();
+        $this->assertCount(171, $obj->getAuthors());
+        $this->assertCount(366, $obj->getQuotes());
+    }
+}
