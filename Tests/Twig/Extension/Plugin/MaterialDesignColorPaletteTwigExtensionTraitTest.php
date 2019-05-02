acd3a2cae8b9a33ba03d5877638bb0172011b115
webeweb <support@webeweb.fr>
2019-05-02 18:59:17 +0200

    Add Material Design color palette Twig extension trait

---
+++ Tests/Twig/Extension/Plugin/MaterialDesignColorPaletteTwigExtensionTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Twig\Extension\Plugin;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Twig\Extension\Plugin\TestMaterialDesignColorPaletteTwigExtensionTrait;
+use WBW\Bundle\CoreBundle\Twig\Extension\Plugin\MaterialDesignColorPaletteTwigExtension;
+
+/**
+ * Material Design color palette Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Twig\Extension\Plugin
+ */
+class MaterialDesignColorPaletteTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestMaterialDesignColorPaletteTwigExtensionTrait();
+
+        $this->assertNull($obj->getMaterialDesignColorPaletteTwigExtension());
+    }
+
+    /**
+     * Tests the setMaterialDesignColorPaletteTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetMaterialDesignColorPaletteTwigExtension() {
+
+        // Set a Material Design color palette Twig extension mock.
+        $materialDesignColorPaletteTwigExtension = new MaterialDesignColorPaletteTwigExtension($this->twigEnvironment);
+
+        $obj = new TestMaterialDesignColorPaletteTwigExtensionTrait();
+
+        $obj->setMaterialDesignColorPaletteTwigExtension($materialDesignColorPaletteTwigExtension);
+        $this->assertSame($materialDesignColorPaletteTwigExtension, $obj->getMaterialDesignColorPaletteTwigExtension());
+    }
+}
---
+++ Twig/Extension/Plugin/MaterialDesignColorPaletteTwigExtensionTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Twig\Extension\Plugin;
+
+/**
+ * Material Design color palette Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Twig\Extension
+ */
+trait MaterialDesignColorPaletteTwigExtensionTrait {
+
+    /**
+     * Material Design color palette Twig extension.
+     *
+     * @var MaterialDesignColorPaletteTwigExtension
+     */
+    private $materialDesignColorPaletteTwigExtension;
+
+    /**
+     * Get the Material Design color palette Twig extension.
+     *
+     * @return MaterialDesignColorPaletteTwigExtension Returns the Material Design color palette Twig extension.
+     */
+    public function getMaterialDesignColorPaletteTwigExtension() {
+        return $this->materialDesignColorPaletteTwigExtension;
+    }
+
+    /**
+     * Set the Material Design color palette Twig extension.
+     *
+     * @param MaterialDesignColorPaletteTwigExtension|null $materialDesignColorPaletteTwigExtension The Material Design color palette Twig extension.
+     */
+    protected function setMaterialDesignColorPaletteTwigExtension(MaterialDesignColorPaletteTwigExtension $materialDesignColorPaletteTwigExtension = null) {
+        $this->materialDesignColorPaletteTwigExtension = $materialDesignColorPaletteTwigExtension;
+        return $this;
+    }
+}
