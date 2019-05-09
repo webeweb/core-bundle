6bd1117b37bbc5a46fb19c9e145c99b95782815d
webeweb <support@webeweb.fr>
2019-05-09 18:57:53 +0200

    Update Color manager

--- Manager/ColorManager.php
+++ Manager/ColorManager.php
@@ -11,13 +11,19 @@
 
 namespace WBW\Bundle\CoreBundle\Manager;
 
+use ReflectionException;
+use WBW\Bundle\CoreBundle\Exception\AlreadyRegisteredProviderException;
+use WBW\Bundle\CoreBundle\Helper\ColorHelper;
+use WBW\Bundle\CoreBundle\Provider\ColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\ProviderInterface;
+
 /**
  * Color manager.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\CoreBundle\Manager
  */
-class ColorManager extends AbstractColorManager {
+class ColorManager extends AbstractManager {
 
     /**
      * Service name.
@@ -25,4 +31,40 @@ class ColorManager extends AbstractColorManager {
      * @var string
      */
     const SERVICE_NAME = "webeweb.core.manager.color";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function contains(ProviderInterface $provider) {
+
+        if (false === ($provider instanceof ColorProviderInterface)) {
+            return false;
+        }
+
+        $identifier = ColorHelper::getIdentifier($provider);
+
+        foreach ($this->getProviders() as $current) {
+            if (false === ($current instanceof ColorProviderInterface) || $identifier !== ColorHelper::getIdentifier($current)) {
+                continue;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Register a color provider.
+     *
+     * @param ColorProviderInterface $colorProvider The color provider.
+     * @return ManagerInterface Returns this manager.
+     * @throws AlreadyRegisteredProviderException Throws an already registered provider exception.
+     * @throws ReflectionException Throws a reflection exception if an error occurs.
+     */
+    public function registerProvider(ColorProviderInterface $colorProvider) {
+        if (true === $this->contains($colorProvider)) {
+            throw new AlreadyRegisteredProviderException($colorProvider);
+        }
+        return $this->addProvider($colorProvider);
+    }
 }
--- Tests/Manager/ColorManagerTest.php
+++ Tests/Manager/ColorManagerTest.php
@@ -11,7 +11,13 @@
 
 namespace WBW\Bundle\CoreBundle\Tests\Manager;
 
+use Exception;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\PinkColorProvider;
+use WBW\Bundle\CoreBundle\Color\MaterialDesignColorPalette\RedColorProvider;
+use WBW\Bundle\CoreBundle\Exception\AlreadyRegisteredProviderException;
 use WBW\Bundle\CoreBundle\Manager\ColorManager;
+use WBW\Bundle\CoreBundle\Provider\ColorProviderInterface;
+use WBW\Bundle\CoreBundle\Provider\QuoteProviderInterface;
 use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
 
 /**
@@ -21,6 +27,23 @@ use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
  * @package WBW\Bundle\CoreBundle\Tests\Manager
  */
 class ColorManagerTest extends AbstractTestCase {
+
+    /**
+     * Color provider
+     *
+     * @var ColorProviderInterface
+     */
+    private $colorProvider;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Color provider mock.
+        $this->colorProvider = new RedColorProvider();
+    }
 
     /**
      * Tests the __construct() method.
@@ -34,5 +57,58 @@ class ColorManagerTest extends AbstractTestCase {
         $obj = new ColorManager();
 
         $this->assertEquals([], $obj->getProviders());
+    }
+
+    /**
+     * Tests the contains() method.
+     *
+     * @return void
+     */
+    public function testContains() {
+
+        // Set a Color provider mock.
+        $quoteProvider = $this->getMockBuilder(QuoteProviderInterface::class)->getMock();
+
+        $obj = new ColorManager();
+
+        $obj->addProvider(new PinkColorProvider());
+        $obj->addProvider($this->colorProvider);
+        $this->assertTrue($obj->contains($this->colorProvider));
+
+        $obj->addProvider($quoteProvider);
+        $this->assertFalse($obj->contains($quoteProvider));
+    }
+
+    /**
+     * Tests the registerProvider() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testRegisterProvider() {
+
+        $obj = new ColorManager();
+
+        $obj->registerProvider($this->colorProvider);
+        $this->assertSame($this->colorProvider, $obj->getProviders()[0]);
+    }
+
+    /**
+     * Tests the registerProvider() method.
+     *
+     * @return void
+     */
+    public function testRegisterProviderWithAlreadyRegisteredException() {
+
+        $obj = new ColorManager();
+
+        try {
+
+            $obj->registerProvider($this->colorProvider);
+            $obj->registerProvider($this->colorProvider);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(AlreadyRegisteredProviderException::class, $ex);
+        }
     }
 }
