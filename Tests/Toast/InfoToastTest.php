d992fd360dc7b28f3f1d9f05b4a970e373dd951a
webeweb <support@webeweb.fr>
2019-06-26 18:43:01 +0200

    Add Info toast

---
+++ Tests/Toast/InfoToastTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Toast;
+
+use WBW\Bundle\CoreBundle\Toast\InfoToast;
+use WBW\Bundle\CoreBundle\Toast\ToastInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Info toast test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Toast
+ */
+class InfoToastTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new InfoToast("info");
+
+        $this->assertEquals(ToastInterface::TOAST_INFO, $obj->getType());
+        $this->assertEquals("info", $obj->getContent());
+    }
+}
---
+++ Toast/InfoToast.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Toast;
+
+/**
+ * Info toast.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Toast
+ */
+class InfoToast extends AbstractToast {
+
+    /**
+     * Constructor.
+     *
+     * @param string $content The content.
+     */
+    public function __construct($content) {
+        parent::__construct(self::TOAST_INFO, $content);
+    }
+}
