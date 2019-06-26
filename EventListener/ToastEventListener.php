d942579e431934bfddae2dac259a64a356de4ddf
webeweb <support@webeweb.fr>
2019-06-26 18:45:37 +0200

    Add Toast event listener

---
+++ EventListener/ToastEventListener.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use WBW\Bundle\CoreBundle\Event\ToastEvent;
+use WBW\Bundle\CoreBundle\Service\SessionTrait;
+
+/**
+ * Toast event listener.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\EventListener
+ */
+class ToastEventListener {
+
+    use SessionTrait;
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.event_listener.toast";
+
+    /**
+     * Constructor.
+     *
+     * @param SessionInterface $session The session.
+     */
+    public function __construct(SessionInterface $session) {
+        $this->setSession($session);
+    }
+
+    /**
+     * On toast.
+     *
+     * @param ToastEvent $event The event.
+     * @return ToastEvent Returns the event.
+     */
+    public function onToast(ToastEvent $event) {
+        if (true === ($this->getSession() instanceof Session)) {
+            $this->getSession()->getFlashBag()->add($event->getToast()->getType(), $event->getToast()->getContent());
+        }
+        return $event;
+    }
+}
---
+++ Tests/EventListener/ToastEventListenerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use WBW\Bundle\CoreBundle\Event\ToastEvent;
+use WBW\Bundle\CoreBundle\EventListener\ToastEventListener;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Toast\ToastInterface;
+
+/**
+ * Toast event listener.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\EventListener
+ */
+class ToastEventListenerTest extends AbstractTestCase {
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set a Session mock.
+        $this->session = $this->getMockBuilder(Session::class)->getMock();
+        $this->session->expects($this->any())->method("getFlashBag")->willReturn(new FlashBag());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new ToastEventListener($this->session);
+
+        $this->assertEquals("wbw.core.event_listener.toast", ToastEventListener::SERVICE_NAME);
+        $this->assertSame($this->session, $obj->getSession());
+    }
+
+    /**
+     * Tests the onToast() method.
+     *
+     * @return void
+     */
+    public function testOnToast() {
+
+        // Set a Toast mock.
+        $toast = $this->getMockBuilder(ToastInterface::class)->getMock();
+
+        $obj = new ToastEventListener($this->session);
+
+        $arg = new ToastEvent("eventName", $toast);
+
+        $this->assertSame($arg, $obj->onToast($arg));
+    }
+}
