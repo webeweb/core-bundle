5451c024c34ff9f4afc385cdb3ff6721170bed67
webeweb <support@webeweb.fr>
2019-08-12 18:10:54 +0200

    Update dependencies

--- Tests/Fixtures/app/TestKernel.php
+++ Tests/Fixtures/app/TestKernel.php
@@ -25,9 +25,11 @@ class TestKernel extends AbstractKernel {
     public function registerBundles() {
         $bundles = [
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new FOS\UserBundle\FOSUserBundle,
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new WBW\Bundle\CoreBundle\WBWCoreBundle(),
         ];
