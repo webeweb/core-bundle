3210e812161f5a27638a356ee10bb959f1804818
webeweb <support@webeweb.fr>
2019-09-09 18:58:15 +0200

    Add Divider node

---
+++ Tests/Navigation/DividerNodeTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Navigation;
+
+use WBW\Bundle\CoreBundle\Navigation\DividerNode;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Navigation\TestNavigationNode;
+
+/**
+ * Divider node test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Navigation
+ */
+class DividerNodeTest extends AbstractTestCase {
+
+    /**
+     * Tests the addNode() method.
+     *
+     * @return void
+     */
+    public function testAddNode() {
+
+        // Set a Navigation node mock.
+        $node = new TestNavigationNode("node");
+
+        $obj = new DividerNode("id");
+
+        $this->assertSame($obj, $obj->addNode($node));
+        $this->assertEquals([], $obj->getNodes());
+
+        $this->assertNull($node->getParent());
+        $this->assertNull($node->getAlphabeticalTreeNodeParent());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DividerNode("id");
+
+        $this->assertFalse($obj->getActive());
+        $this->assertFalse($obj->getEnable());
+        $this->assertNull($obj->getIcon());
+        $this->assertNull($obj->getMatcher());
+        $this->assertNull($obj->getUri());
+        $this->assertTrue($obj->getVisible());
+    }
+}
