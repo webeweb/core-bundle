a7fc97da12c93a0fb0a66487fe6ff8d865e6721e
webeweb <support@webeweb.fr>
2019-09-09 20:56:49 +0200

    Add Kernel helper

---
+++ Helper/KernelHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Helper;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class KernelHelper {
+
+    /**
+     * Get a project directory.
+     *
+     * @param KernelInterface $kernel The kernel.
+     * @return string Returns the project directory.
+     */
+    public static function getProjectDir(KernelInterface $kernel) {
+
+        $method = "getProjectDir";
+        if (true === method_exists($kernel, $method)) {
+            return $kernel->$method();
+        } else {
+            $method = "getRootDir";
+        }
+
+        return $kernel->$method() . "/..";
+    }
+}
---
+++ Tests/Helper/KernelHelperTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Helper;
+
+use TestKernel;
+use WBW\Bundle\CoreBundle\Helper\KernelHelper;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Kernel helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Helper
+ */
+class KernelHelperTest extends AbstractTestCase {
+
+    /**
+     * Tests the getProjectDir() method.
+     *
+     * @return void
+     */
+    public function testGetProjectDir() {
+
+        $this->assertEquals(getcwd(), KernelHelper::getProjectDir(new TestKernel("test", true)));
+    }
+}
