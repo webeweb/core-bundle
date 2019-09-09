5cd6c14022db1282338fb192de74a6c9f9e7afe5
webeweb <support@webeweb.fr>
2019-09-09 18:58:26 +0200

    Add Header node

---
+++ Navigation/HeaderNode.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Navigation;
+
+/**
+ * Header node.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Navigation
+ */
+class HeaderNode extends AbstractNavigationNode {
+
+    /**
+     * Constructor.
+     *
+     * @param string $name The name.
+     * @param string $icon The icon.
+     */
+    public function __construct($name, $icon = null) {
+        parent::__construct($name, $icon, null, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addNode(AbstractNavigationNode $node) {
+        return $this;
+    }
+}
