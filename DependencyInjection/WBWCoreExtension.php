d2089b857bab5bd356ab68de4b8abfa2f3ae136b
webeweb <support@webeweb.fr>
2019-10-08 19:50:27 +0200

    Update Core extension: - set parameters

--- DependencyInjection/WBWCoreExtension.php
+++ DependencyInjection/WBWCoreExtension.php
@@ -41,26 +41,32 @@ class WBWCoreExtension extends Extension {
         $config = $this->processConfiguration($configuration, $configs);
 
         if (true === $config["commands"]) {
+            $container->setParameter(implode(".", [$this->getAlias(), "commands"]), $config["commands"]);
             $serviceLoader->load("commands.yml");
         }
 
         if (true === $config["event_listeners"]) {
+            $container->setParameter(implode(".", [$this->getAlias(), "event_listeners"]), $config["event_listeners"]);
             $serviceLoader->load("event_listeners.yml");
         }
 
         if (true === $config["providers"]) {
+            $container->setParameter(implode(".", [$this->getAlias(), "providers"]), $config["providers"]);
             $serviceLoader->load("providers.yml");
         }
 
         if (true === $config["security_event_listener"]) {
+            $container->setParameter(implode(".", [$this->getAlias(), "security_event_listener"]), $config["security_event_listener"]);
             $serviceLoader->load("services/security_event_listener.yml");
         }
 
         if (true === $config["twig"]) {
+            $container->setParameter(implode(".", [$this->getAlias(), "twig"]), $config["twig"]);
             $serviceLoader->load("twig.yml");
         }
 
         if (true === $config["quote_providers"]["worlds_wisdom"]) {
+            $container->setParameter(implode(".", [$this->getAlias(), "quote_providers", "worlds_wisdom"]), $config["quote_providers"]["worlds_wisdom"]);
             $serviceLoader->load("services/worlds_wisdom_quote_provider.yml");
         }
     }
