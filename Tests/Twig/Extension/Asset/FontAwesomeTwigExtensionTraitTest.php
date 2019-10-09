a753392fba60b814aeee1baf1eb5c91f7ebf8afa
webeweb <support@webeweb.fr>
2019-10-09 12:52:33 +0200

    Move Font Awesome Twig extension trait

---
+++ Tests/Twig/Extension/Asset/FontAwesomeTwigExtensionTraitTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Twig\Extension\Asset;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Twig\Extension\Asset\TestFontAwesomeTwigExtensionTrait;
+use WBW\Bundle\CoreBundle\Twig\Extension\Asset\FontAwesomeTwigExtension;
+
+/**
+ * Font Awesome Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Twig\Extension\Asset
+ */
+class FontAwesomeTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $obj = new TestFontAwesomeTwigExtensionTrait();
+
+        $this->assertNull($obj->getFontAwesomeTwigExtension());
+    }
+
+    /**
+     * Tests the setFontAwesomeTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetFontAwesomeTwigExtension() {
+
+        // Set a Font Awesome Twig extension mock.
+        $fontAwesomeTwigExtension = new FontAwesomeTwigExtension($this->twigEnvironment);
+
+        $obj = new TestFontAwesomeTwigExtensionTrait();
+
+        $obj->setFontAwesomeTwigExtension($fontAwesomeTwigExtension);
+        $this->assertSame($fontAwesomeTwigExtension, $obj->getFontAwesomeTwigExtension());
+    }
+}
