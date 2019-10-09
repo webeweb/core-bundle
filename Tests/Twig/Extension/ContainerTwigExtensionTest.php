b237eb044f088fa8331b0b8ffbf17ec6d5b96c5c
webeweb <support@webeweb.fr>
2019-10-09 18:15:31 +0200

    Add Container Twig extension

---
+++ Tests/Twig/Extension/ContainerTwigExtensionTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Twig\Extension;
+
+use Twig\TwigFunction;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Twig\Extension\ContainerTwigExtension;
+
+/**
+ * Container Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package Tests\Twig\Extension
+ */
+class ContainerTwigExtensionTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("wbw.core.twig.extension.container", ContainerTwigExtension::SERVICE_NAME);
+
+        $obj = new ContainerTwigExtension($this->containerBuilder);
+
+        $this->assertSame($this->containerBuilder, $obj->getContainer());
+    }
+
+    /**
+     * Tests the getContainerParameterFunction() method.
+     *
+     * @return void
+     */
+    public function testGetContainerParameterFunction() {
+
+        $obj = new ContainerTwigExtension($this->containerBuilder);
+
+        $this->assertNotNull($obj->getContainerParameterFunction("kernel.root_dir"));
+    }
+
+    /**
+     * Tests the getFilters() method.
+     *
+     * @return void
+     */
+    public function testGetFilters() {
+
+        $obj = new ContainerTwigExtension($this->containerBuilder);
+
+        $res = $obj->getFilters();
+        $this->assertCount(0, $res);
+    }
+
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new ContainerTwigExtension($this->containerBuilder);
+
+        $res = $obj->getFunctions();
+        $this->assertCount(1, $res);
+
+        $this->assertInstanceOf(TwigFunction::class, $res[0]);
+        $this->assertEquals("getContainerParameter", $res[0]->getName());
+        $this->assertEquals([$obj, "getContainerParameterFunction"], $res[0]->getCallable());
+    }
+}
---
+++ Twig/Extension/ContainerTwigExtension.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Twig\Extension;
+
+use Symfony\Component\DependencyInjection\Container;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use WBW\Bundle\CoreBundle\DependencyInjection\ContainerTrait;
+
+/**
+ * Container Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Twig\Extension
+ */
+class ContainerTwigExtension extends AbstractTwigExtension {
+
+    use ContainerTrait;
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.twig.extension.container";
+
+    /**
+     * Constructor.
+     *
+     * @param Container $container The container.
+     */
+    public function __construct(Container $container) {
+        $this->setContainer($container);
+    }
+
+    /**
+     * Get the Twig filters.
+     *
+     * @return TwigFilter[] Returns the Twig filters.
+     */
+    public function getFilters() {
+        return [];
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return TwigFunction[] Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new TwigFunction("getContainerParameter", [$this, "getContainerParameterFunction"]),
+        ];
+    }
+
+    /**
+     * Get a container parameter.
+     *
+     * @param string $name The name.
+     * @return mixed|null Returns the container parameter in case of success, null otherwise.
+     */
+    public function getContainerParameterFunction($name) {
+        return $this->getContainer()->getParameter($name);
+    }
+}
