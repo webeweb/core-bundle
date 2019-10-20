c3fcc91cb92041804eda22f458a77eaaf7292aec
webeweb <support@webeweb.fr>
2019-10-20 13:18:59 +0200

    Add string Environment trait

---
+++ Model/Attribute/StringEnvironmentTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Model\Attribute;
+
+/**
+ * String environment trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Model\Attribute
+ */
+trait StringEnvironmentTrait {
+
+    /**
+     * Environment.
+     *
+     * @var string
+     */
+    private $environment;
+
+    /**
+     * Get the environment.
+     *
+     * @return string Returns the environment.
+     */
+    public function getEnvironment() {
+        return $this->environment;
+    }
+
+    /**
+     * Set the environment.
+     *
+     * @param string $environment The environment.
+     */
+    public function setEnvironment($environment) {
+        $this->environment = $environment;
+        return $this;
+    }
+}
