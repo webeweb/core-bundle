a0052245b010229d9312f98a9f514a9bfe9d6c46
webeweb <support@webeweb.fr>
2019-10-20 13:14:29 +0200

    Add float Including VAT price trait

---
+++ Model/Attribute/FloatIncludingVatPriceTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Model\Attribute;
+
+/**
+ * Float including VAT price trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Model\Attribute
+ */
+trait FloatIncludingVatPriceTrait {
+
+    /**
+     * Including VAT price.
+     *
+     * @var float
+     */
+    private $includingVatPrice;
+
+    /**
+     * Get the including VAT price.
+     *
+     * @return float Returns the including VAT price.
+     */
+    public function getIncludingVatPrice() {
+        return $this->includingVatPrice;
+    }
+
+    /**
+     * Set the including VAT price.
+     *
+     * @param float $includingVatPrice The including VAT price.
+     */
+    public function setIncludingVatPrice($includingVatPrice) {
+        $this->includingVatPrice = $includingVatPrice;
+        return $this;
+    }}
