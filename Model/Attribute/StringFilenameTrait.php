09dcae75ac2b0f4b4fb770ca1efbdafd460bb1cd
webeweb <support@webeweb.fr>
2019-10-20 13:19:16 +0200

    Add string Filename trait

---
+++ Model/Attribute/StringFilenameTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Model\Attribute;
+
+/**
+ * String filename trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Model\Attribute
+ */
+trait StringFilenameTrait {
+
+    /**
+     * Filename.
+     *
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * Get the filename.
+     *
+     * @return string Returns the filename.
+     */
+    public function getFilename() {
+        return $this->filename;
+    }
+
+    /**
+     * Set the filename.
+     *
+     * @param string $filename The filename.
+     */
+    public function setFilename($filename) {
+        $this->filename = $filename;
+        return $this;
+    }
+}
