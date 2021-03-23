ce18b71e19dc48b78a188bbf08666ab0ce2dc349
webeweb <support@webeweb.fr>
2021-03-23 19:04:55 +0100

    Add Time timestamp data transformer

---
+++ Form/DataTransformer/Timestamp/TimeTimestampDataTransformer.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Form\DataTransformer\Timestamp;
+
+use WBW\Bundle\CoreBundle\Form\DataTransformer\AbstractTimestampDataTransformer;
+
+/**
+ * Time timestamp data transformer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Form\DataTransformer\Timestamp
+ */
+class TimeTimestampDataTransformer extends AbstractTimestampDataTransformer {
+
+    /**
+     * Constructor.
+     *
+     * @param string $format The format.
+     * @param string|null $timezone The timezone.
+     */
+    public function __construct(string $format = "H:i:s", ?string $timezone = "UTC") {
+        parent::__construct($format, $timezone);
+    }
+}
