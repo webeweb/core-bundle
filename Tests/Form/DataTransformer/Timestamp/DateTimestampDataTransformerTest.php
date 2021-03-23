da7e9b3847c25b87a78a4173b767845190ea047b
webeweb <support@webeweb.fr>
2021-03-23 19:04:39 +0100

    Add Date timestamp data transformer

---
+++ Tests/Form/DataTransformer/Timestamp/DateTimestampDataTransformerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Form\DataTransformer\Timestamp;
+
+use WBW\Bundle\CoreBundle\Form\DataTransformer\Timestamp\DateTimestampDataTransformer;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Date timestamp data transformer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Form\DataTransformer\Timestamp
+ */
+class DateTimestampDataTransformerTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DateTimestampDataTransformer();
+
+        $this->assertEquals("Y-m-d", $obj->getFormat());
+        $this->assertEquals("UTC", $obj->getTimezone());
+    }
+}
