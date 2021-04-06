d7e163c443f8bb6b99e5d3f1e441498288191584
webeweb <support@webeweb.fr>
2021-04-06 18:07:17 +0200

    Add Filter user response event

---
+++ Event/FilterUserResponseEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Event;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use WBW\Bundle\CoreBundle\Component\HttpFoundation\ResponseTrait;
+use WBW\Bundle\CoreBundle\Model\UserInterface;
+
+/**
+ * Filter user response event.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Event
+ */
+class FilterUserResponseEvent extends UserEvent {
+
+    use ResponseTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param UserInterface $user The user.
+     * @param Request $request The request.
+     * @param Response $response The response.
+     */
+    public function __construct(UserInterface $user, Request $request, Response $response) {
+        parent::__construct($user, $request);
+        $this->setResponse($response);
+    }
+}
