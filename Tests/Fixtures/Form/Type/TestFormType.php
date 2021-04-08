2af9204669b67b120abbbe7db413101f9986c1b9
webeweb <support@webeweb.fr>
2021-04-08 18:57:02 +0200

    Add abstract Form type

---
+++ Tests/Fixtures/Form/Type/TestFormType.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Fixtures\Form\Type;
+
+use WBW\Bundle\CoreBundle\Form\Type\AbstractFormType;
+
+/**
+ * Test form type.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures\Form\Type
+ */
+class TestFormType extends AbstractFormType {
+
+}
---
+++ Tests/Form/Type/AbstractFormTypeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Form\Type;
+
+use WBW\Bundle\CoreBundle\DependencyInjection\WBWCoreExtension;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Form\Type\TestFormType;
+
+/**
+ * Abstract form type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Form\Type
+ */
+class AbstractFormTypeTest extends AbstractTestCase {
+
+    /**
+     * Tests the getBlockPrefix() method.
+     *
+     * @erturn void
+     */
+    public function testGetBlockPrefix(): void {
+
+        $obj = new TestFormType();
+
+        $this->assertEquals(WBWCoreExtension::EXTENSION_ALIAS, $obj->getBlockPrefix());
+    }
+}
