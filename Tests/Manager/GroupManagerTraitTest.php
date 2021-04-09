d24bc2535f43a29d5a26c08dd5f682b414980987
webeweb <support@webeweb.fr>
2021-04-09 19:22:35 +0200

    Add Group manager trait

---
+++ Tests/Manager/GroupManagerTraitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Manager;
+
+use WBW\Bundle\CoreBundle\Manager\GroupManagerInterface;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Manager\TestGroupManagerTrait;
+
+/**
+ * Group manager trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Manager
+ */
+class GroupManagerTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setGroupManager() method.
+     *
+     * @return void
+     */
+    public function testSetGroupManager(): void {
+
+        // Set an Group manager mock.
+        $groupManager = $this->getMockBuilder(GroupManagerInterface::class)->getMock();
+
+        $obj = new TestGroupManagerTrait();
+
+        $obj->setGroupManager($groupManager);
+        $this->assertSame($groupManager, $obj->getGroupManager());
+    }
+}
