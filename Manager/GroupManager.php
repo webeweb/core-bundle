a52d860fa90d531f70aff6e1bf54c7597d93a475
webeweb <support@webeweb.fr>
2021-06-30 11:29:32 +0200

    Add Group manager

---
+++ Manager/GroupManager.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Manager;
+
+use WBW\Bundle\CoreBundle\Model\GroupInterface;
+
+/**
+ * Group manager.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Manager
+ * @abstract
+ */
+abstract class GroupManager implements GroupManagerInterface {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createGroup(string $name): GroupInterface {
+
+        $class = $this->getClass();
+
+        return new $class($name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findGroupByName(string $name): ?GroupInterface {
+        return $this->findGroupBy([
+            "name" => $name,
+        ]);
+    }
+}
---
+++ Tests/Fixtures/Manager/TestGroupManager.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Fixtures\Manager;
+
+use WBW\Bundle\CoreBundle\Manager\GroupManager;
+use WBW\Bundle\CoreBundle\Model\GroupInterface;
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Model\TestGroup;
+
+/**
+ * Test group manager.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures\Manager
+ */
+class TestGroupManager extends GroupManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deleteGroup(GroupInterface $group): void {
+        // NOTHING TO DO
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findGroupBy(array $criteria): ?GroupInterface {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findGroups(): array {
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getClass(): ?string {
+        return TestGroup::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function updateGroup(GroupInterface $group): void {
+        // NOTHING TO DO
+    }
+}
