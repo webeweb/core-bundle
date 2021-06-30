5ff707beed4e67d0f32c91fdd655838932ba5a2c
webeweb <support@webeweb.fr>
2021-06-30 10:16:42 +0200

    Add Form factory

---
+++ Form/Factory/FormFactory.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Form\Factory;
+
+use Symfony\Component\Form\FormFactoryInterface as BaseFormFactory;
+use Symfony\Component\Form\FormInterface;
+use WBW\Bundle\CoreBundle\Component\Form\FormFactoryTrait;
+use WBW\Library\Traits\Strings\StringNameTrait;
+use WBW\Library\Traits\Strings\StringTypeTrait;
+
+/**
+ * Form factory.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Form\Factory
+ */
+class FormFactory implements FormFactoryInterface {
+
+    use FormFactoryTrait;
+    use StringNameTrait;
+    use StringTypeTrait;
+
+    /**
+     * Validation groups.
+     *
+     * @var array|null
+     */
+    private $validationGroups;
+
+    /**
+     * Constructor.
+     *
+     * @param BaseFormFactory $formFactory The form factory.
+     * @param string $name The name.
+     * @param string $type The type.
+     * @param array|null $validationGroups The validation groups.
+     */
+    public function __construct(BaseFormFactory $formFactory, string $name, string $type, array $validationGroups = null) {
+        $this->setFormFactory($formFactory);
+        $this->setName($name);
+        $this->setType($type);
+        $this->setValidationGroups($validationGroups);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createForm(array $options = []): FormInterface {
+
+        $options = array_merge($options, [
+            "validation_groups" => $this->getValidationGroups(),
+        ]);
+
+        return $this->getFormFactory()->createNamed($this->getName(), $this->getType(), null, $options);
+    }
+
+    /**
+     * Get the validation groups.
+     *
+     * @return array|null Returns the validation groups.
+     */
+    public function getValidationGroups(): ?array {
+        return $this->validationGroups;
+    }
+
+    /**
+     * Set the validation groups.
+     *
+     * @param array|null $validationGroups The validation groups.
+     * @return FormFactory Returns this form factory.
+     */
+    protected function setValidationGroups(?array $validationGroups): FormFactory {
+        $this->validationGroups = $validationGroups;
+        return $this;
+    }
+}
---
+++ Tests/Form/Factory/FormFactoryTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Form\Factory;
+
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use WBW\Bundle\CoreBundle\Form\Factory\FormFactory;
+use WBW\Bundle\CoreBundle\Form\Type\ChangePasswordFormType;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Form factory test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\CoreBundle\Tests\Form\Factory
+ */
+class FormFactoryTest extends AbstractTestCase {
+
+    /**
+     * Form factory.
+     *
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        $myself = $this;
+
+        // Set a Create named closure.
+        $createNamed = function($name, $type = "Symfony\\Component\\Form\\Extension\\Core\\Type\\FormType", $data = null, array $options = []) use ($myself) {
+
+            // Set a Form mock.
+            $form = $this->getMockBuilder(FormInterface::class)->getMock();
+            $form->expects($this->any())->method("getData")->willReturn($data);
+            $form->expects($this->any())->method("getName")->willReturn($name);
+
+            return $form;
+        };
+
+        // Set a Form factory mock.
+        $this->formFactory = $this->getMockBuilder(FormFactoryInterface::class)->getMock();
+        $this->formFactory->expects($this->any())->method("createNamed")->willReturnCallback($createNamed);
+    }
+
+    /**
+     * Tests the createForm() method.
+     *
+     * @return void
+     */
+    public function testCreateForm(): void {
+
+        $obj = new FormFactory($this->formFactory, "wbw_core_form_type_change_password", ChangePasswordFormType::class, []);
+
+        $res = $obj->createForm();
+        $this->assertInstanceOf(FormInterface::class, $res);
+
+        $this->assertEquals("wbw_core_form_type_change_password", $res->getName());
+        $this->assertNull($res->getData());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new FormFactory($this->formFactory, "wbw_core_form_type_change_password", ChangePasswordFormType::class, []);
+
+        $this->assertEquals($this->formFactory, $obj->getFormFactory());
+        $this->assertEquals("wbw_core_form_type_change_password", $obj->getName());
+        $this->assertEquals(ChangePasswordFormType::class, $obj->getType());
+        $this->assertEquals([], $obj->getValidationGroups());
+    }
+}
