f2f09c4ae2282ac0d5a0e6c9d45a36591535788b
webeweb <support@webeweb.fr>
2021-12-02 10:28:52 +0100

    Add FullCalendar asset

--- Tests/Helper/AssetsHelperTest.php
+++ Tests/Helper/AssetsHelperTest.php
@@ -71,13 +71,14 @@ class AssetsHelperTest extends AbstractTestCase {
         $plugins = $config["assets"]["wbw.core.asset.core"]["plugins"];
 
         $res = TestAssetsHelper::listAssets($this->directoryAssets);
-        $this->assertCount(15, $res);
+        $this->assertCount(16, $res);
 
         $i = -1;
 
         $this->assertRegExp("/animate\.css\-" . preg_quote($plugins["animate_css"]["version"]) . "\.zip$/", $res[++$i]);
         $this->assertRegExp("/clippy\.js\.zip$/", $res[++$i]);
         $this->assertRegExp("/fontawesome\-" . preg_quote($plugins["font_awesome"]["version"]) . "\.zip$/", $res[++$i]);
+        $this->assertRegExp("/fullcalendar\-" . preg_quote($plugins["full_calendar"]["version"]) . "\.zip$/", $res[++$i]);
         $this->assertRegExp("/jquery\-" . preg_quote($plugins["jquery"]["version"]) . "\.zip$/", $res[++$i]);
         $this->assertRegExp("/jquery\-contextmenu\-" . preg_quote($plugins["jquery_context_menu"]["version"]) . "\.zip$/", $res[++$i]);
         $this->assertRegExp("/jquery\-easyautocomplete\-" . preg_quote($plugins["jquery_easy_autocomplete"]["version"]) . "\.zip$/", $res[++$i]);
@@ -118,7 +119,7 @@ class AssetsHelperTest extends AbstractTestCase {
     public function testUnzipAssets(): void {
 
         $res = TestAssetsHelper::unzipAssets($this->directoryAssets, $this->directoryPublic);
-        $this->assertCount(15, $res);
+        $this->assertCount(16, $res);
 
         foreach ($res as $k => $v) {
             $this->assertDirectoryExists(str_replace([$this->directoryAssets, ".zip"], [$this->directoryPublic, ""], $k));
