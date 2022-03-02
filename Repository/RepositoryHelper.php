6b3244c5bd5a89e2c6b7b8628fc744b17e7e57fa
webeweb <support@webeweb.fr>
2022-03-02 19:37:33 +0100

    Model classes have been moved into core-library

--- Repository/RepositoryHelper.php
+++ Repository/RepositoryHelper.php
@@ -18,7 +18,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping\MappingException;
 use WBW\Bundle\CoreBundle\Doctrine\ORM\EntityManagerTrait;
-use WBW\Bundle\CoreBundle\Model\RepositoryReport;
+use WBW\Library\Symfony\Model\RepositoryReport;
 
 /**
  * Repository helper.
