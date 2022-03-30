8c1a34952d97f4e52016cf26f58d5baae7a09df9
webeweb <support@webeweb.fr>
2022-03-30 14:03:32 +0200

    Add Repository service interface

---
+++ Service/RepositoryServiceInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Service;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\Mapping\MappingException;
+use WBW\Library\Symfony\Model\RepositoryReportInterface;
+
+/**
+ * Repository service interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CoreBundle\Service
+ */
+interface RepositoryServiceInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.service.repository";
+
+    /**
+     * Find all repositories reports.
+     *
+     * @return RepositoryReportInterface[] Returns the repositories reports.
+     * @throws MappingException Throws a mapping exception if an error occurs.
+     * @throws DBALException Throws a DBAL exception if an error occurs.
+     * @throws \Doctrine\DBAL\Driver\Exception Throws a driver exception if an error occurs.
+     */
+    public function findRepositoriesReports(): array;
+}
