d3742662773901244e7574e9ee1d054f0c76980e
webeweb <support@webeweb.fr>
2022-10-07 09:03:24 +0200

    Add Stylesheet provider

---
+++ Provider/StylesheetProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Provider;
+
+use WBW\Library\Symfony\Provider\StylesheetProviderInterface;
+
+/**
+ * Stylesheet provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CoreBundle\Provider
+ */
+class StylesheetProvider implements StylesheetProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.core.provider.stylesheet";
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStylesheets(): array {
+        return [];
+    }
+}
