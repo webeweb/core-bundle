e43b721e90a5eccb11874c99f176fbbd6f35505e
webeweb <support@webeweb.fr>
2022-10-11 09:50:24 +0200

    Remove javascript Font Awesome

--- Provider/JavascriptProvider.php
+++ Provider/JavascriptProvider.php
@@ -34,7 +34,6 @@ class JavascriptProvider implements JavascriptProviderInterface {
     public function getJavascripts(): array {
 
         return [
-            "wbwCoreFontAwesome"                => "@WBWCore/assets/wbwCoreFontAwesome.js.twig",
             "wbwCoreJQueryInputMask"            => "@WBWCore/assets/wbwCoreJQueryInputMask.js.twig",
             "wbwCoreLeaflet"                    => "@WBWCore/assets/wbwCoreLeaflet.js.twig",
             "wbwCoreMaterialDesignColorPalette" => "@WBWCore/assets/wbwCoreMaterialDesignColorPalette.js.twig",
--- Tests/Provider/JavascriptProviderTest.php
+++ Tests/Provider/JavascriptProviderTest.php
@@ -34,7 +34,6 @@ class JavascriptProviderTest extends AbstractTestCase {
         $obj = new JavascriptProvider();
 
         $exp = [
-            "wbwCoreFontAwesome"                => "@WBWCore/assets/wbwCoreFontAwesome.js.twig",
             "wbwCoreJQueryInputMask"            => "@WBWCore/assets/wbwCoreJQueryInputMask.js.twig",
             "wbwCoreLeaflet"                    => "@WBWCore/assets/wbwCoreLeaflet.js.twig",
             "wbwCoreMaterialDesignColorPalette" => "@WBWCore/assets/wbwCoreMaterialDesignColorPalette.js.twig",
