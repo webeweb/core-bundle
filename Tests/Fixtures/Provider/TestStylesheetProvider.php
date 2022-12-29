2bc96191e97ee0993953556ebf5f579e202be3b0
webeweb <support@webeweb.fr>
2022-12-29 15:29:02 +0100

    Add Test stylesheet provider

---
+++ Tests/Fixtures/Provider/TestStylesheetProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CoreBundle\Tests\Fixtures\Provider;
+
+use WBW\Library\Symfony\Provider\StylesheetProviderInterface;
+
+/**
+ * Test stylesheet provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CoreBundle\Tests\Fixtures\Provider
+ */
+class TestStylesheetProvider implements StylesheetProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @ver string
+     */
+    const SERVICE_NAME = "wbw.core.tests.fixtures.provider.test_stylesheet";
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStylesheets(): array {
+
+        return [
+            "WBWCoreTest" => "@WBWCore/assets/WBWCoreTest.css.twig",
+        ];
+    }
+}
