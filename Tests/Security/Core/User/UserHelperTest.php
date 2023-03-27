15bc6d8c7c9c06a4af5af04785541d0817e9f492
webeweb <support@webeweb.fr>
2023-03-27 09:58:29 +0200

    Fix User helper unit tests

--- Tests/Security/Core/User/UserHelperTest.php
+++ Tests/Security/Core/User/UserHelperTest.php
@@ -11,7 +11,7 @@
 
 namespace WBW\Bundle\CoreBundle\Tests\Security\Core\User;
 
-use Symfony\Component\Security\Core\User\User;
+use WBW\Bundle\CoreBundle\Model\FakeUser;
 use WBW\Bundle\CoreBundle\Security\Core\User\UserHelper;
 use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
 
@@ -30,26 +30,27 @@ class UserHelperTest extends AbstractTestCase {
      */
     public function testHasRoles(): void {
 
-        $arg = new User("github", "github", ["ROLE_USER"]);
+        // Set a User mock.
+        $user = new FakeUser("github", "github", ["ROLE_USER"]);
 
         $this->assertFalse(UserHelper::hasRoles(null, ""));
         $this->assertFalse(UserHelper::hasRoles(null, null));
         $this->assertFalse(UserHelper::hasRoles("", null));
         $this->assertFalse(UserHelper::hasRoles("", ""));
 
-        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_SUPER_ADMIN"));
-        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_SUPER_ADMIN", false));
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_SUPER_ADMIN"));
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_SUPER_ADMIN", false));
 
-        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_USER"));
-        $this->assertTrue(UserHelper::hasRoles($arg, "ROLE_USER", false));
+        $this->assertTrue(UserHelper::hasRoles($user, "ROLE_USER"));
+        $this->assertTrue(UserHelper::hasRoles($user, "ROLE_USER", false));
 
-        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_GITHUB"));
-        $this->assertFalse(UserHelper::hasRoles($arg, "ROLE_GITHUB", false));
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_GITHUB"));
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_GITHUB", false));
 
-        $this->assertTrue(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_USER"]));
-        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_USER"], false));
+        $this->assertTrue(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_USER"]));
+        $this->assertFalse(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_USER"], false));
 
-        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"]));
-        $this->assertFalse(UserHelper::hasRoles($arg, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"], false));
+        $this->assertFalse(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"]));
+        $this->assertFalse(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"], false));
     }
 }
