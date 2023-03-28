c0b64ad74c402fe07b66922a78a2adfa062efcce
webeweb <support@webeweb.fr>
2023-03-28 15:15:44 +0200

    Improve code quality

--- Translation/TranslatorTrait.php
+++ Translation/TranslatorTrait.php
@@ -33,7 +33,7 @@ trait TranslatorTrait {
      *
      * @return TranslatorInterface|null Returns the translator.
      */
-    public function getTranslator() {
+    public function getTranslator(): TranslatorInterface {
         return $this->translator;
     }
 
